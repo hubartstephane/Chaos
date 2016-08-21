10b4f0582a47afcc42f81ccb7f78ef04ccded990
hubartstephane <hubart.stephane@gmail.com>
2016-08-22 00:22:33 +0200

    Add a comment in MeshPrimitive class

--- libraries/chaos/src/SimpleMesh.cpp
+++ libraries/chaos/src/SimpleMesh.cpp
@@ -12,7 +12,18 @@ namespace chaos
     base_vertex_index(0)
   {
   }
-
+  //
+  // XXX : the vb_offset is not really usefull in mixed BUFFER (one buffer with 2 parts)
+  //       
+  //       x------------------x-------------------x
+  //       | vertex size = 6  |  vertex_size = 18 |
+  //       x------------------x-------------------x
+  //
+  //       because BASE_VERTEX_INDEX is a number of vertices 
+  //        => the real offset depends of the size of ONE vertex
+  //       
+  //      to create a shift, use glVertexArrayVertexBuffer(...offset ...) that helps create a view of buffer
+  //
   void MeshPrimitive::ShiftIndexAndVertexPosition(int vb_offset, int ib_offset)
   {
     if (!indexed)

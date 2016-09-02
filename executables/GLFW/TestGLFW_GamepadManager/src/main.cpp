2dd9176703e5e02928f818e3818987b20e156c61
shubart <hubart.stephane@gmail.com>
2016-09-02 13:50:33 +0200

    testGLFW_GamepadManager : use debug output on screen

--- executables/GLFW/TestGLFW_GamepadManager/src/main.cpp
+++ executables/GLFW/TestGLFW_GamepadManager/src/main.cpp
@@ -5,33 +5,56 @@
 #include <chaos/MyGLFWWindow.h> 
 #include <chaos/WinTools.h> 
 #include <chaos/Application.h>
+#include <chaos/GLDebugOnScreenDisplay.h>
+#include <chaos/StringTools.h>
 
 class MyGamepadManager : public chaos::MyGLFWGamepadManager
 {
+public:
+
+  MyGamepadManager():
+    debug_display(nullptr){}
+
+  void SetDebugDisplay(chaos::GLDebugOnScreenDisplay * in_debug_display)
+  {
+    debug_display = in_debug_display;
+  }
+
 protected:
 
   virtual bool OnGamepadConnected(chaos::MyGLFWGamepad * gamepad) override
   {
     if (gamepad->IsButtonPressed(chaos::MyGLFWGamepad::XBOX_BUTTON_START))
     {
-      std::cout << "OnGamepadConnected : " << gamepad->GetGamepadIndex() << std::endl;
+      if (debug_display != nullptr)
+        debug_display->AddLine(chaos::StringTools::Printf("OnGamepadConnected %d : accepted ", gamepad->GetGamepadIndex()).c_str(), 1.0f);
       return true;
     }    
-    return false;
+    else
+    {
+      if (debug_display != nullptr)
+        debug_display->AddLine(chaos::StringTools::Printf("OnGamepadConnected %d : refused (PRESS START)", gamepad->GetGamepadIndex()).c_str(), 1.0f);    
+      return false;
+    }    
   }
 
   virtual bool OnGamepadDisconnected(chaos::MyGLFWGamepad * gamepad) override
   {
-    std::cout << "OnGamepadDisconnected : " << gamepad->GetGamepadIndex() << std::endl;
+    if (debug_display != nullptr)
+      debug_display->AddLine(chaos::StringTools::Printf("OnGamepadDisconnected %d", gamepad->GetGamepadIndex()).c_str(), 1.0f);
     return true;
   }
+
+protected:
+
+  chaos::GLDebugOnScreenDisplay * debug_display;
 };
 
 class MyGLFWWindowGamepadTest : public chaos::MyGLFWWindow
 {
 public:
 
-  MyGLFWWindowGamepadTest() : main_gamepad(nullptr){}
+  MyGLFWWindowGamepadTest(){}
 
 protected:
 
@@ -42,11 +65,40 @@ protected:
 
     float far_plane = 1000.0f;
     glClearBufferfi(GL_DEPTH_STENCIL, 0, far_plane, 0);
+
+    debug_display.Display(width, height);    
+
     return true;
+  }
+
+  virtual void Finalize() override
+  {
+    debug_display.Finalize();
   }
 
   virtual bool Initialize() override
   {
+    chaos::Application * application = chaos::Application::GetInstance();
+    if (application == nullptr)
+      return false;
+
+    boost::filesystem::path resources_path = application->GetResourcesPath();
+    boost::filesystem::path image_path     = resources_path / "font.png";
+
+    chaos::GLDebugOnScreenDisplay::Params debug_params;
+    debug_params.texture_path               = image_path;
+    debug_params.font_characters            = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+    debug_params.font_characters_per_line   = 10;
+    debug_params.font_characters_line_count = 10;
+    debug_params.character_width            = 20;
+    debug_params.spacing                    = glm::ivec2( 0, 0);
+    debug_params.crop_texture               = glm::ivec2(15, 7);
+
+    if (!debug_display.Initialize(debug_params))
+      return false;
+
+    gamepad_manager.SetDebugDisplay(&debug_display);
+
     main_gamepad = gamepad_manager.AllocateGamepad();
     return true;
   }
@@ -61,20 +113,21 @@ protected:
 
   virtual bool Tick(double delta_time) override
   {
+    debug_display.Tick(delta_time);
     gamepad_manager.Tick(0.0f);
 
     if (main_gamepad->IsPresent())
     {
       if (main_gamepad->IsAnyAction())
-        std::cout << "IsAnyAction()" << std::endl;
+        debug_display.AddLine("IsAnyAction()", 1.0f);
 
       glm::vec2 l = main_gamepad->GetXBOXStickDirection(chaos::MyGLFWGamepad::XBOX_LEFT_AXIS);
       if (l.x != 0.0f || l.y != 0.0f)
-        std::cout << "LEFT x : " << l.x << "    y : " << l.y << std::endl;
+        debug_display.AddLine(chaos::StringTools::Printf("LEFT x : %0.3f   y : %0.3f", l.x, l.y).c_str(), 1.0f);
 
       glm::vec2 r = main_gamepad->GetXBOXStickDirection(chaos::MyGLFWGamepad::XBOX_RIGHT_AXIS);
       if (r.x != 0.0f || r.y != 0.0f)
-        std::cout << "RIGHT x : " << r.x << "    y : " << r.y << std::endl;
+        debug_display.AddLine(chaos::StringTools::Printf("RIGHT x : %0.3f  y : %0.3f", r.x, r.y).c_str(), 1.0f);
     }
 
     return true; // no redraw
@@ -84,6 +137,8 @@ protected:
 protected:
 
   MyGamepadManager gamepad_manager;
+
+  chaos::GLDebugOnScreenDisplay debug_display;
 
   chaos::MyGLFWGamepad * main_gamepad;
 };
@@ -98,8 +153,8 @@ int _tmain(int argc, char ** argv, char ** env)
 
   chaos::MyGLFWSingleWindowApplicationParams params;
   params.monitor       = nullptr;
-  params.width         = 500;
-  params.height        = 500;
+  params.width         = 700;
+  params.height        = 700;
   params.monitor_index = 0;
   chaos::MyGLFWWindow::RunSingleWindowApplication<MyGLFWWindowGamepadTest>(params);
 
--- executables/GLFW/TestGLFW_GamepadManager/subpremake5.lua
+++ executables/GLFW/TestGLFW_GamepadManager/subpremake5.lua
@@ -4,6 +4,7 @@
 
   WindowedApp()
   DependOnLib("CHAOS")
+  DeclareResource("resources")      
     
 
                        

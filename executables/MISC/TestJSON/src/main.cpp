d998496de203808ae47a44dff56a8e41fa42de0d
hubartstephane <hubart.stephane@gmail.com>
2016-09-10 00:02:30 +0200

    Test JSON in progress : write test outputs in a directory

--- executables/MISC/TestJSON/src/main.cpp
+++ executables/MISC/TestJSON/src/main.cpp
@@ -7,36 +7,59 @@
 
 // extract tests from : https://github.com/nlohmann/json
 
-void Test1(boost::filesystem::path const & p)
+void Test1(boost::filesystem::path const & filename, boost::filesystem::path const & dst_dir)
 {
-  chaos::Buffer<char> buf = chaos::FileTools::LoadFile(p, true);
+  chaos::Buffer<char> buf = chaos::FileTools::LoadFile(filename, true);
   if (buf != nullptr)
   {
+    std::ofstream file;
+
+    file.open((dst_dir / "Test1.txt").string().c_str(), std::ofstream::out);
+    if (file)
+    {
+      try
+      {
+        nlohmann::json j = nlohmann::json::parse(buf.data);
+
+        
 
 
+        for (nlohmann::json::iterator it = j.begin(); it != j.end(); ++it) 
+        {
+          file << *it << '\n';
+        }
+
+        float p = j["pi"];
+
+
+        p = p;
+      }
+      catch (std::exception & e)
+      {
+        char const * error = e.what();
+        file << "EXCEPTION : " << e.what() << std::endl;
+      }
+    }   
   }
 }
 
-void Test2()
+void Test2(boost::filesystem::path const & dst_dir)
 {
   nlohmann::json j;
 
-  j["pi"] = 3.141;
-
-  j["happy"] = true;
-
-  j["name"] = "Niels";
-
-  j["nothing"] = nullptr;
-
+  j["pi"]      = 3.141;
+  j["happy"]   = true;
+  j["name"]    = "Niels";
+  j["nothing"] = nullptr;  
+  j["list"]    = { 1, 0, 2 };
+  j["object"]  = { { "currency", "USD" },{ "value", 42.99 } };
   j["answer"]["everything"] = 42;
 
-  j["list"] = { 1, 0, 2 };
+  std::ofstream file;
 
-  j["object"] = { { "currency", "USD" },{ "value", 42.99 } };
-
-  std::string s = j.dump();
-  chaos::LogTools::Log("%s", s.c_str());
+  file.open((dst_dir / "Test2.txt").string().c_str(), std::ofstream::out);
+  if (file)
+    file << j.dump(4); // 4 = indent
 }
 
 
@@ -48,11 +71,14 @@ int _tmain(int argc, char ** argv, char ** env)
 
   boost::filesystem::path const & p = chaos::Application::GetInstance()->GetResourcesPath();
 
-  Test1(p / "test.json");
-  Test2();
+  boost::filesystem::path dst_p;
+  if (chaos::FileTools::CreateTemporaryDirectory("TestJSON", dst_p))
+  {
+    Test1(p / "test.json", dst_p);
+    Test2(dst_p);
 
-
-
+    chaos::WinTools::ShowFile(dst_p.string().c_str());
+  }
 
   chaos::WinTools::PressToContinue();
 

639fda5e993ef35f015f48db69fb79388a6107c1
hubartstephane <hubart.stephane@gmail.com>
2016-09-09 23:29:06 +0200

    TestJSON in progress

--- executables/MISC/TestJSON/src/main.cpp
+++ executables/MISC/TestJSON/src/main.cpp
@@ -1,9 +1,43 @@
 
-#include <chaos/LuaTools.h>
 #include <chaos/WinTools.h>
 #include <chaos/StringTools.h>
 #include <chaos/FileTools.h>
 #include <chaos/Application.h>
+#include <chaos/LogTools.h>
+
+// extract tests from : https://github.com/nlohmann/json
+
+void Test1(boost::filesystem::path const & p)
+{
+  chaos::Buffer<char> buf = chaos::FileTools::LoadFile(p, true);
+  if (buf != nullptr)
+  {
+
+
+  }
+}
+
+void Test2()
+{
+  nlohmann::json j;
+
+  j["pi"] = 3.141;
+
+  j["happy"] = true;
+
+  j["name"] = "Niels";
+
+  j["nothing"] = nullptr;
+
+  j["answer"]["everything"] = 42;
+
+  j["list"] = { 1, 0, 2 };
+
+  j["object"] = { { "currency", "USD" },{ "value", 42.99 } };
+
+  std::string s = j.dump();
+  chaos::LogTools::Log("%s", s.c_str());
+}
 
 
 int _tmain(int argc, char ** argv, char ** env)
@@ -14,11 +48,10 @@ int _tmain(int argc, char ** argv, char ** env)
 
   boost::filesystem::path const & p = chaos::Application::GetInstance()->GetResourcesPath();
 
+  Test1(p / "test.json");
+  Test2();
 
-  nlohmann::json js;
 
-
- 
 
 
   chaos::WinTools::PressToContinue();

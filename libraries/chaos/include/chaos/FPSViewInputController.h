e1c223785429b9d1a314e59f35ca9c60f3f25a0e
hubartstephane <hubart.stephane@gmail.com>
2016-12-30 00:12:29 +0100

    FPSViewInputController : 2 modes depending on the fact that mouse must be clicked to rotate or not

--- libraries/chaos/include/chaos/FPSViewInputController.h
+++ libraries/chaos/include/chaos/FPSViewInputController.h
@@ -6,60 +6,64 @@
 namespace chaos
 {
 
-	/**
-	* FPSViewMovementSpeed : a class that describes displacement speed for the FPS view
-	*/
+  /**
+  * FPSViewMovementSpeed : a class that describes displacement speed for the FPS view
+  */
 
-	class FPSViewMovementSpeed
-	{		
-	public:
+  class FPSViewMovementSpeed
+  {
+  public:
 
-		/** some self descriptive speed */
-		float strafe{20.0f};
-		/** some self descriptive speed */
-		float forward{20.0f};
-		/** some self descriptive speed */
-		float back{20.0f};
-		/** some self descriptive speed */
-		float up{20.0f};
-		/** some self descriptive speed */
-		float down{20.0f};	
-	};
+    /** some self descriptive speed */
+    float strafe{ 20.0f };
+    /** some self descriptive speed */
+    float forward{ 20.0f };
+    /** some self descriptive speed */
+    float back{ 20.0f };
+    /** some self descriptive speed */
+    float up{ 20.0f };
+    /** some self descriptive speed */
+    float down{ 20.0f };
+  };
 
-	/**
-	* FPSViewInputController : an utility class to simply handle a FPS camera in a GLFW application. Handle keys ...
-	*/
+  /**
+  * FPSViewInputController : an utility class to simply handle a FPS camera in a GLFW application. Handle keys ...
+  */
 
-	class FPSViewInputController
-	{
-	public:
+  class FPSViewInputController
+  {
+  public:
 
-		/** the tick method */
-		virtual void Tick(GLFWwindow * glfw_window, double delta_time);
+    /** the tick method */
+    virtual void Tick(GLFWwindow * glfw_window, double delta_time);
 
-		/** matrix getter */
-		inline glm::mat4 GlobalToLocal() const { return fps_controller.GlobalToLocal(); }
-		/** matrix getter */
-		inline glm::mat4 LocalToGlobal() const { return fps_controller.LocalToGlobal(); }
+    /** matrix getter */
+    inline glm::mat4 GlobalToLocal() const { return fps_controller.GlobalToLocal(); }
+    /** matrix getter */
+    inline glm::mat4 LocalToGlobal() const { return fps_controller.LocalToGlobal(); }
 
-	protected:
+  protected:
 
-		/** whether the mouse has been captured */
-		bool mouse_captured{false};
-		/** position of the mouse once captured */
-		double previous_mouse_x{0.0};
-		/** position of the mouse once captured */
-		double previous_mouse_y{0.0};
+    static double const INVALID_MOUSE_VALUE;
 
-	public:
+    /** whether the mouse has been captured */
+    bool mouse_captured{ false };
+    /** position of the mouse once captured */
+    double previous_mouse_x{ INVALID_MOUSE_VALUE };
+    /** position of the mouse once captured */
+    double previous_mouse_y{ INVALID_MOUSE_VALUE };
 
-		/** the fps matrix handler */
-		FPSViewController fps_controller;
-		/** the speed for the displacement */
-		FPSViewMovementSpeed movement_speed;
-		/** mouse speed */
-		float mouse_sensibility{1.0f};
-	};
+  public:
+
+    /** the fps matrix handler */
+    FPSViewController fps_controller;
+    /** the speed for the displacement */
+    FPSViewMovementSpeed movement_speed;
+    /** mouse speed */
+    float mouse_sensibility{ 1.0f };
+    /** whether we need to capture to move the camera */
+    bool must_click_to_rotate{ true };
+  };
 
 }; // namespace chaos
 
--- libraries/chaos/src/FPSViewInputController.cpp
+++ libraries/chaos/src/FPSViewInputController.cpp
@@ -4,53 +4,56 @@
 
 namespace chaos
 {
-	void FPSViewInputController::Tick(GLFWwindow * glfw_window, double delta_time)
-	{
-		// handle mouse movement
-		if (mouse_captured)
-		{
-			if (glfwGetMouseButton(glfw_window, 0) == GLFW_RELEASE)
-				mouse_captured = false;
-			else
-			{
-				double new_mouse_x = 0.0;
-				double new_mouse_y = 0.0;
-				glfwGetCursorPos(glfw_window, &new_mouse_x, &new_mouse_y);
-			
-				if (new_mouse_x != previous_mouse_x)
-					fps_controller.IncrementYaw(-(float)((new_mouse_x - previous_mouse_x) * mouse_sensibility));
-				if (new_mouse_y != previous_mouse_y)
-					fps_controller.IncrementPitch(-(float)((new_mouse_y - previous_mouse_y) * mouse_sensibility));
+  double const FPSViewInputController::INVALID_MOUSE_VALUE = std::numeric_limits<double>::max();
 
-				previous_mouse_x = new_mouse_x;
-				previous_mouse_y = new_mouse_y;
-			}
-		}
-		else
-		{
-			if (glfwGetMouseButton(glfw_window, 0) == GLFW_PRESS)
-			{
-				mouse_captured = true;
-				glfwGetCursorPos(glfw_window, &previous_mouse_x, &previous_mouse_y);				
-			}						
-		}
+  void FPSViewInputController::Tick(GLFWwindow * glfw_window, double delta_time)
+  {
+    // handle mouse movement
+    if (mouse_captured || !must_click_to_rotate)
+    {
+      if (must_click_to_rotate && glfwGetMouseButton(glfw_window, 0) == GLFW_RELEASE)
+        mouse_captured = false;
+      else
+      {
+        double new_mouse_x = 0.0;
+        double new_mouse_y = 0.0;
+        glfwGetCursorPos(glfw_window, &new_mouse_x, &new_mouse_y);
 
-		// handles the keys for displacement
-		if (glfwGetKey(glfw_window, GLFW_KEY_LEFT) == GLFW_PRESS)
-			fps_controller.StrafeLeft(movement_speed.strafe * (float)delta_time);
-		if (glfwGetKey(glfw_window, GLFW_KEY_RIGHT) == GLFW_PRESS)
-			fps_controller.StrafeRight(movement_speed.strafe * (float)delta_time);
-		if (glfwGetKey(glfw_window, GLFW_KEY_UP) == GLFW_PRESS)
-			fps_controller.GoForward(movement_speed.forward * (float)delta_time);
-		if (glfwGetKey(glfw_window, GLFW_KEY_DOWN) == GLFW_PRESS)
-			fps_controller.GoBackward(movement_speed.back * (float)delta_time);
-		if (glfwGetKey(glfw_window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS)
-			fps_controller.position.y -= movement_speed.down * (float)delta_time;
-		if (glfwGetKey(glfw_window, GLFW_KEY_PAGE_UP) == GLFW_PRESS)
-			fps_controller.position.y += movement_speed.up * (float)delta_time;
-	}
+        if (previous_mouse_x != INVALID_MOUSE_VALUE && previous_mouse_y != INVALID_MOUSE_VALUE) // only want to rotate if we have a valid mouse position
+        {
+          if (new_mouse_x != previous_mouse_x)
+            fps_controller.IncrementYaw(-(float)((new_mouse_x - previous_mouse_x) * mouse_sensibility));
+          if (new_mouse_y != previous_mouse_y)
+            fps_controller.IncrementPitch(-(float)((new_mouse_y - previous_mouse_y) * mouse_sensibility));
+        }
+        previous_mouse_x = new_mouse_x;
+        previous_mouse_y = new_mouse_y;
+      }
+    }
+    else
+    {
+      if (glfwGetMouseButton(glfw_window, 0) == GLFW_PRESS)
+      {
+        mouse_captured = true;
+        previous_mouse_x = INVALID_MOUSE_VALUE;
+        previous_mouse_y = INVALID_MOUSE_VALUE;
+        glfwGetCursorPos(glfw_window, &previous_mouse_x, &previous_mouse_y);
+      }
+    }
 
-
-
+    // handles the keys for displacement
+    if (glfwGetKey(glfw_window, GLFW_KEY_LEFT) == GLFW_PRESS)
+      fps_controller.StrafeLeft(movement_speed.strafe * (float)delta_time);
+    if (glfwGetKey(glfw_window, GLFW_KEY_RIGHT) == GLFW_PRESS)
+      fps_controller.StrafeRight(movement_speed.strafe * (float)delta_time);
+    if (glfwGetKey(glfw_window, GLFW_KEY_UP) == GLFW_PRESS)
+      fps_controller.GoForward(movement_speed.forward * (float)delta_time);
+    if (glfwGetKey(glfw_window, GLFW_KEY_DOWN) == GLFW_PRESS)
+      fps_controller.GoBackward(movement_speed.back * (float)delta_time);
+    if (glfwGetKey(glfw_window, GLFW_KEY_PAGE_DOWN) == GLFW_PRESS)
+      fps_controller.position.y -= movement_speed.down * (float)delta_time;
+    if (glfwGetKey(glfw_window, GLFW_KEY_PAGE_UP) == GLFW_PRESS)
+      fps_controller.position.y += movement_speed.up * (float)delta_time;
+  }
 
 }; // namespace chaos

16558a554da28c8e127532101bf49253aa06470a
shubart <hubart.stephane@gmail.com>
2017-01-04 19:01:47 +0100

    Endian code

--- executables/SOUND/Test_MIDI_readFile/src/main.cpp
+++ executables/SOUND/Test_MIDI_readFile/src/main.cpp
@@ -8,6 +8,162 @@
 #include <chaos/IrrklangTools.h>
 #include <chaos/MIDITools.h>
 
+//
+// Little Endian :
+// 
+//    lsb        msb
+//   --+----------+-->
+//
+// Big Endian :
+//
+//    msb        lsb
+//   --+----------+-->
+//
+// 0x86   : Little Endian
+// TCP/IP : Big Endian
+// MIDI File : BigEndian
+//
+
+class EndianTools
+{
+public:
+
+	/** whether the host is Little Endian */
+	static bool IsHostLittleEndian()
+	{
+		uint16_t const tmp = 0x00FF;
+
+		unsigned char lsb = ((unsigned char const *)&tmp)[0];
+
+		return (lsb == 0xFF);	
+	}
+
+	/** whether the host is Big Endian */
+	static bool IsHostBigEndian(){ return !IsHostLittleEndian();}
+
+	/** convert endianness */
+	template<typename T>
+	static T HostToLittleEndian(T src)
+	{
+		if (IsHostBigEndian())
+			return EndianSwap(src);
+		return src;	
+	}
+
+	/** convert endianness */
+	template<typename T>
+	static T LittleEndianToHost(T src)
+	{
+		return HostToLittleEndian(src);
+	}
+
+	/** convert endianness */
+	template<typename T>
+	static T HostToBigEndian(T src)
+	{
+		if (IsHostLittleEndian())
+			return EndianSwap(src);
+		return src;	
+	}
+	/** convert endianness */
+	template<typename T>
+	static T BigEndianToHost(T src)
+	{
+		return HostToBigEndian(src);
+	}
+
+	static uint16_t EndianSwap(uint16_t src)
+	{
+		uint16_t result = 0;
+		((unsigned char *)&result)[0] = ((unsigned char const *)&src)[1];
+		((unsigned char *)&result)[1] = ((unsigned char const *)&src)[0];	
+		return result;
+	}
+
+	static int16_t EndianSwap(int16_t src)
+	{
+		int16_t result = 0;
+		((unsigned char *)&result)[0] = ((unsigned char const *)&src)[1];
+		((unsigned char *)&result)[1] = ((unsigned char const *)&src)[0];	
+		return result;
+	}
+
+	static uint32_t EndianSwap(uint32_t src)
+	{
+		uint32_t result = 0;
+		((unsigned char *)&result)[0] = ((unsigned char const *)&src)[3];
+		((unsigned char *)&result)[1] = ((unsigned char const *)&src)[2];
+		((unsigned char *)&result)[2] = ((unsigned char const *)&src)[1];
+		((unsigned char *)&result)[3] = ((unsigned char const *)&src)[0];	
+		return result;
+	}
+
+	static int32_t EndianSwap(int32_t src)
+	{
+		int32_t result = 0;
+		((unsigned char *)&result)[0] = ((unsigned char const *)&src)[3];
+		((unsigned char *)&result)[1] = ((unsigned char const *)&src)[2];
+		((unsigned char *)&result)[2] = ((unsigned char const *)&src)[1];
+		((unsigned char *)&result)[3] = ((unsigned char const *)&src)[0];	
+		return result;
+	}
+
+	static uint64_t EndianSwap(uint64_t src)
+	{
+		uint64_t result = 0;
+		((unsigned char *)&result)[0] = ((unsigned char const *)&src)[7];
+		((unsigned char *)&result)[1] = ((unsigned char const *)&src)[6];
+		((unsigned char *)&result)[2] = ((unsigned char const *)&src)[5];
+		((unsigned char *)&result)[3] = ((unsigned char const *)&src)[4];	
+		((unsigned char *)&result)[4] = ((unsigned char const *)&src)[3];
+		((unsigned char *)&result)[5] = ((unsigned char const *)&src)[2];
+		((unsigned char *)&result)[6] = ((unsigned char const *)&src)[1];
+		((unsigned char *)&result)[7] = ((unsigned char const *)&src)[0];
+		return result;
+	}
+
+	static int64_t EndianSwap(int64_t src)
+	{
+		int64_t result = 0;
+		((unsigned char *)&result)[0] = ((unsigned char const *)&src)[7];
+		((unsigned char *)&result)[1] = ((unsigned char const *)&src)[6];
+		((unsigned char *)&result)[2] = ((unsigned char const *)&src)[5];
+		((unsigned char *)&result)[3] = ((unsigned char const *)&src)[4];	
+		((unsigned char *)&result)[4] = ((unsigned char const *)&src)[3];
+		((unsigned char *)&result)[5] = ((unsigned char const *)&src)[2];
+		((unsigned char *)&result)[6] = ((unsigned char const *)&src)[1];
+		((unsigned char *)&result)[7] = ((unsigned char const *)&src)[0];
+		return result;
+	}
+
+	static float EndianSwap(float src)
+	{
+		float result = 0;
+		((unsigned char *)&result)[0] = ((unsigned char const *)&src)[3];
+		((unsigned char *)&result)[1] = ((unsigned char const *)&src)[2];
+		((unsigned char *)&result)[2] = ((unsigned char const *)&src)[1];
+		((unsigned char *)&result)[3] = ((unsigned char const *)&src)[0];	
+		return result;
+	}
+
+	static double EndianSwap(double src)
+	{
+		double result = 0;
+		((unsigned char *)&result)[0] = ((unsigned char const *)&src)[7];
+		((unsigned char *)&result)[1] = ((unsigned char const *)&src)[6];
+		((unsigned char *)&result)[2] = ((unsigned char const *)&src)[5];
+		((unsigned char *)&result)[3] = ((unsigned char const *)&src)[4];	
+		((unsigned char *)&result)[4] = ((unsigned char const *)&src)[3];
+		((unsigned char *)&result)[5] = ((unsigned char const *)&src)[2];
+		((unsigned char *)&result)[6] = ((unsigned char const *)&src)[1];
+		((unsigned char *)&result)[7] = ((unsigned char const *)&src)[0];
+		return result;
+	}
+		
+
+};
+
+
 class BufferReader
 {
 public:
@@ -81,8 +237,15 @@ protected:
 	MidiChunk ReadHeaderChunk(BufferReader & reader);
 };
 
+
+
 MidiChunk MidiLoader::ReadChunk(BufferReader & reader)
 {
+//	int a = BIG_ENDIAN;
+//		int b = LITTLE_ENDIAN;
+//		int c = BYTE_ORDER;
+
+
 	MidiChunk result;
 	if (reader.IsEnoughData(8))
 	{
@@ -91,14 +254,15 @@ MidiChunk MidiLoader::ReadChunk(BufferReader & reader)
 		// read the size of the data
 		uint32_t data_size;
 		reader.Read(data_size);
+		data_size = EndianTools::BigEndianToHost(data_size);
 		
 		if (reader.IsEnoughData(data_size))
 		{
+			result.bufsize = data_size;
+			result.data    = reader.GetCurrentPiosition();
 		
 			data_size = data_size;
-		}
-		
-	
+		}		
 	}
 	return result;
 }
@@ -193,6 +357,8 @@ protected:
 
 int _tmain(int argc, char ** argv, char ** env)
 {
+	bool b1 = EndianTools::IsHostLittleEndian();
+
 	chaos::Application::Initialize<chaos::Application>(argc, argv, env);
 
 	chaos::WinTools::AllocConsoleAndRedirectStdOutput();

2aaaec7a4e0bf0a63926d82591581d0c4c59f62b
hubartstephane <hubart.stephane@gmail.com>
2017-01-14 00:05:00 +0100

    CutWord in progress

--- executables/MISC/TestCutWord/src/main.cpp
+++ executables/MISC/TestCutWord/src/main.cpp
@@ -3,52 +3,140 @@
 #include <chaos/WinTools.h>
 #include <chaos/StringTools.h>
 #include <chaos/FileTools.h>
+#include <chaos/LogTools.h>
 #include <chaos/Application.h>
 
 
 bool IsVowel(char c)
 {
-	char const * vowel = "aeiouy";
+	char const * vowel = "aeiouyâêîôûäëïöüÿéèà";
 	return (strchr(vowel, c) != nullptr);
 }
 
 bool IsConsonant(char c)
 {
-	char const * consonant = "bcdfghjqlmnpqrstvwxz";
+	char const * consonant = "bcdfghjklmnpqrstvwxz";
 	return (strchr(consonant, c) != nullptr);
+}
+
+
+
+#if 0
+
+
+an
+amiante
+#endif
+
+#if 0
+
+
+PREFIX           MIDDLE           SUFFIX
+.MOT = (CONSONNE*.VOYELLE).(CONSONNE.VOYELLE)*.(CONSONNE)*
+
+-le suffix est toujours collé au token d'avant
+
+plain | te plai | ne
+
+WORD = (GROUPE CONSONNE).(GROUPE VOYELLE))
+
+GROUPE VOYELLE = VOYELLE*
+(aeiouy)*
+an->si n est pas suivie d une voyelle : a | na | na | s
+am ->
+en
+em
+ain
+aim
+
+b
+b o
+b o n
+
+#endif
+
+int const TYPE_CONSONANT = 0;
+int const TYPE_VOWEL = 1;
+
+struct Token
+{
+	Token() = default;
+
+	Token(int s, int e, int t) : start(s), end(e), type(t) {}
+
+	int start{ 0 };
+	int end{ 0 };
+	int type{ 0 };
+};
+
+
+
+
+
+std::vector<Token> CutStringPerTypes(char const * str)
+{
+	std::vector<Token> result;
+
+	int i = 0;
+	int j = 0;
+	while (str[i] != 0)
+	{
+		// test for vowels
+		j = i;
+		while (str[j] != 0 && IsVowel(str[j]))
+			++j;
+		if (i != j)
+		{
+			result.emplace_back(i, j - 1, TYPE_VOWEL);
+			i = j;
+		}
+		// test for consonant
+		j = i;
+		while (str[j] != 0 && IsConsonant(str[j]))
+			++j;
+		if (i != j)
+		{
+			result.emplace_back(i, j - 1, TYPE_CONSONANT);
+			i = j;
+		}
+	}
+	return result;
+}
+
+std::string GetDebugCutResultString(char const * str, std::vector<Token> const & tokens)
+{
+	std::string result;
+	result.reserve(strlen(str) + tokens.size());
+
+	for (size_t i = 0; i < tokens.size(); ++i)
+	{
+		Token const & token = tokens[i];
+		for (int j = token.start; j <= token.end; ++j)
+			result += str[j];
+		if (i != tokens.size() - 1)
+			result += "|";
+	}
+	return result;
+}
+
+
+std::vector<Token> GenerateTokens(char const * str)
+{
+	std::vector<Token> result = CutStringPerTypes(str);
+
+
+	return result;
 }
 
 void TestCutWord(char const * str)
 {
-	
-	
-	
-	
-	for (int i = 0; str[i] != 0; ++i)
-	{
-		bool is_vowel = IsVowel(str[i]);
-		if (is_vowel)
-		{
-			int j = i + 1;
-			while ((str[j] != 0) && IsVowel(str[j]))
-				++j;
-		}
+	std::vector<Token> tokens = GenerateTokens(str);
 
-		bool is_consonant = IsConsonant(str[i]);
-		if (is_consonant)
-		{
-			int j = i + 1;
-			while ((str[j] != 0) && IsConsonant(str[j]) && (str[j] != str))
-				++j;
-		}
+	std::string debug_string = GetDebugCutResultString(str, tokens);
 
-		
-
-
-	}
-
-
+	chaos::LogTools::Log("TestCutWord[%s] = [%s]", str, debug_string.c_str());
 }
+
 
 
 int _tmain(int argc, char ** argv, char ** env)
@@ -61,7 +149,9 @@ int _tmain(int argc, char ** argv, char ** env)
 	TestCutWord("peuple");
 	TestCutWord("trottinette");
 	TestCutWord("autour");
-	TestCutWord("ancien !!!");
+	TestCutWord("ancien");
+
+
 
   chaos::WinTools::PressToContinue();
 

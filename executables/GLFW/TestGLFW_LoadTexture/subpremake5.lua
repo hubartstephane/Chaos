49a0e445c46b8f77cb25c5da14dec2b7f545a5d5
shubart <hubart.stephane@gmail.com>
2017-02-15 17:14:03 +0100

    new TestGLFW_LoadTexture project

---
+++ executables/GLFW/TestGLFW_LoadTexture/subpremake5.lua
@@ -0,0 +1,10 @@
+-- =============================================================================
+-- ROOT_PATH/executables/GLFW/TestGLFW_LoadTexture
+-- =============================================================================
+
+  WindowedApp()
+  DependOnLib("CHAOS")
+  DeclareResource("resources")    
+    
+
+                       

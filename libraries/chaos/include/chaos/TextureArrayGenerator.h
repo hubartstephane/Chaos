5c2547f185b5e32f05defc0d4d890785810dfd89
hubartstephane <hubart.stephane@gmail.com>
2017-03-07 00:43:07 +0100

    TextureArrayGenerator in progress

--- libraries/chaos/include/chaos/TextureArrayGenerator.h
+++ libraries/chaos/include/chaos/TextureArrayGenerator.h
@@ -8,209 +8,185 @@
 namespace chaos
 {
 
-	/**
-	* ImageSliceRegisterEntry : an entry in the register of slices
-	*/
+  /**
+  * ImageSliceRegisterEntry : an entry in the register of slices
+  */
 
-	class ImageSliceRegisterEntry
-	{
-	public:
+  class ImageSliceRegisterEntry
+  {
+  public:
 
-		/** the image description */
-		ImageDescription description;
-		/** a user data that may be used later for memory releasing */
-		void * user_data;
-	};
+    /** the image description */
+    ImageDescription description;
+    /** a user data that may be used later for memory releasing */
+    void * user_data{ nullptr };
+  };
 
-	/**
-	* ImageSliceRegister : utility function that serves to register a slice into a vector
-	*/
+  /**
+  * ImageSliceRegister : utility function that serves to register a slice into a vector
+  */
 
-	class ImageSliceRegister
-	{
-		friend class TextureArrayGenerator;
+  class ImageSliceRegister
+  {
+    friend class TextureArrayGenerator;
 
-	public:
+  public:
 
-		/** the method to insert one slice */
-		bool InsertSlice(ImageDescription & description, void * user_data = nullptr);
-		/** gets the number of slices inserted */
-		size_t size() const { return slices.size();}
+    /** the method to insert one slice */
+    bool InsertSlice(ImageDescription & description, void * user_data = nullptr);
+    /** gets the number of slices inserted */
+    size_t size() const { return slices.size(); }
 
-	protected:
+  protected:
 
-		/** test whether the description is valid */
-		bool IsImageSliceValid(ImageDescription const & description) const;
+    /** test whether the description is valid */
+    bool IsImageSliceValid(ImageDescription const & description) const;
 
-		/** the array that contains all slices */
-		std::vector<ImageSliceRegisterEntry> slices;
-	};
+    /** the array that contains all slices */
+    std::vector<ImageSliceRegisterEntry> slices;
+  };
 
-	/**
-	* ImageSliceGeneratorProxyBase : class that deserve registration of several slices
-	*/
+  /**
+  * ImageSliceGeneratorProxyBase : class that deserve registration of several slices
+  */
 
-	class ImageSliceGeneratorProxyBase
-	{
-	public:
+  class ImageSliceGeneratorProxyBase
+  {
+  public:
 
-		/** destructor */
-		virtual ~ImageSliceGeneratorProxyBase() = default;
-		/** the method to override to add all slice we want */
-		virtual void AddSlices(ImageSliceRegister & slice_register){}
-		/** the method to override to release all slices */
-		virtual void ReleaseSlices(ImageSliceRegisterEntry * slices, size_t count){}
-	};
+    /** destructor */
+    virtual ~ImageSliceGeneratorProxyBase() = default;
+    /** the method to override to add all slice we want */
+    virtual void AddSlices(ImageSliceRegister & slice_register) {}
+    /** the method to override to release all slices */
+    virtual void ReleaseSlices(ImageSliceRegisterEntry * slices, size_t count) {}
+  };
 
-	/**
-	* ImageSliceGeneratorBase : used to generate proxy for TextureArrayGeneration
-	*/
+  /**
+  * ImageSliceGeneratorBase : used to generate proxy for TextureArrayGeneration
+  */
 
-	class ImageSliceGeneratorBase
-	{
-	public:
+  class ImageSliceGeneratorBase
+  {
+  public:
 
-		/** destructor */
-		virtual ~ImageSliceGeneratorBase() = default;
-		/** proxy generation method */
-		virtual ImageSliceGeneratorProxyBase * CreateProxy() const = 0;
-	};
+    /** destructor */
+    virtual ~ImageSliceGeneratorBase() = default;
+    /** proxy generation method */
+    virtual ImageSliceGeneratorProxyBase * CreateProxy() const = 0;
+  };
 
-	/**
-	* ImageSliceGeneratorProxy : a slice generator proxy from a image 
-	*/
+  /**
+  * ImageSliceGeneratorProxy : a slice generator proxy from a image
+  */
 
-	class ImageSliceGeneratorProxy : public ImageSliceGeneratorProxyBase
-	{
-	public:
+  class ImageSliceGeneratorProxy : public ImageSliceGeneratorProxyBase
+  {
+  public:
 
-		/** constructor */
-		ImageSliceGeneratorProxy(FIBITMAP * in_image, bool in_release_image) :
-			image(in_image),
-			release_image(in_release_image) {}
+    /** constructor */
+    ImageSliceGeneratorProxy(FIBITMAP * in_image, bool in_release_image) :
+      image(in_image),
+      release_image(in_release_image) {}
 
-		/** destructor */
-		~ImageSliceGeneratorProxy();
-		/** the method to override to add all slice we want */
-		virtual void AddSlices(ImageSliceRegister & slice_register);
-		/** the method to override to release all slices */
-		virtual void ReleaseSlices(ImageSliceRegisterEntry * slices, size_t count);
+    /** destructor */
+    ~ImageSliceGeneratorProxy();
+    /** the method to override to add all slice we want */
+    virtual void AddSlices(ImageSliceRegister & slice_register);
+    /** the method to override to release all slices */
+    virtual void ReleaseSlices(ImageSliceRegisterEntry * slices, size_t count);
 
-	protected:
+  protected:
 
-		/** the image */
-		FIBITMAP * image;
-		/** whether the image has to be released */
-		bool release_image;
-	};
+    /** the image */
+    FIBITMAP * image{ nullptr };
+    /** whether the image has to be released */
+    bool release_image{ false };
+  };
 
-	/**
-	* ImageSliceGenerator : a slice generator from a image filename
-	*/
+  /**
+  * ImageSliceGenerator : a slice generator from a image filename
+  */
 
-	class ImageSliceGenerator : public ImageSliceGeneratorBase
-	{
-	public:
+  class ImageSliceGenerator : public ImageSliceGeneratorBase
+  {
+  public:
 
-		/** constructor */
-		ImageSliceGenerator(boost::filesystem::path const & in_image_path):
-			image_path(in_image_path), 
-			image(nullptr), 
-			release_image(false){} // release_image is unused with that constructor : the proxy will destroy the image loaded
+    /** constructor */
+    ImageSliceGenerator(boost::filesystem::path const & in_image_path) :
+      image_path(in_image_path),
+      image(nullptr),
+      release_image(false) {} // release_image is unused with that constructor : the proxy will destroy the image loaded
 
-		ImageSliceGenerator(FIBITMAP * in_image, bool in_release_image):
-			image(in_image),
-			release_image(in_release_image) 
-		{
-			assert(image != nullptr);
-		}
+    ImageSliceGenerator(FIBITMAP * in_image, bool in_release_image) :
+      image(in_image),
+      release_image(in_release_image)
+    {
+      assert(image != nullptr);
+    }
 
-		/** proxy generation method */
-		virtual ImageSliceGeneratorProxyBase * CreateProxy() const;
+    /** proxy generation method */
+    virtual ImageSliceGeneratorProxyBase * CreateProxy() const;
 
-	protected:
+  protected:
 
-		/** path of the resource file */
-		boost::filesystem::path image_path;
+    /** path of the resource file */
+    boost::filesystem::path image_path;
 
-		/** the image */
-		FIBITMAP * image;
-		/** whether the image has to be released */
-		bool release_image;
-	};
+    /** the image */
+    FIBITMAP * image{ nullptr };
+    /** whether the image has to be released */
+    bool release_image{ false };
+  };
 
+  /**
+  * TextureArrayGenerator : an helper class that is used to generate texture array    GL_TEXTURE_1D_ARRAY,    GL_TEXTURE_2D_ARRAY or    GL_TEXTURE_CUBE_ARRAY
+  */
 
+  class TextureArrayGenerator
+  {
+  public:
 
+    class SliceInfo
+    {
+    public:
 
-	/**
-	* TextureArrayGeneratorParams : parameters for merging some slices of different format
-	*/
+      /** first slice allocated for the generator */
+      int first_slice{ 0 };
+      /** number of slice used by the generator */
+      int slice_count{ 0 };
+    };
 
-	class TextureArrayGeneratorParams
-	{
-	public:
+    class GeneratorEntry
+    {
+    public:
+      /** the proxy that will request some slices */
+      ImageSliceGeneratorProxyBase * proxy{ nullptr };
+      /** optional where the slices are allocated */
+      SliceInfo * slice_info{ nullptr };
+    };
 
-		/** whether grayscale sliced must be converted into color slice */
-		bool accept_grayscaled{true};
-		/** whether float images must be converted into unsigned char slice */
-		bool accept_float{true};
-		/** the wanted component count */
-		int component_count{0};
-		/** the wanted component type */
-		int component_type{0};		
-	};
+    /** constructor */
+    TextureArrayGenerator() = default;
+    /** destructor */
+    virtual ~TextureArrayGenerator();
 
+    /** the insertion method (returns the slice considered) */
+    bool AddGenerator(ImageSliceGenerator const & generator, SliceInfo * slice_info = nullptr);
+    /** clean all generators */
+    void Clean();
+    /** generate the texture array */
+    boost::intrusive_ptr<Texture> GenerateTexture(PixelFormatMergeParams const & merge_params = PixelFormatMergeParams(), GenTextureParameters const & parameters = GenTextureParameters());
 
+  protected:
 
+    /** internal method to generate the texture array */
+    boost::intrusive_ptr<Texture> GenerateTexture(ImageSliceRegister & slice_register, PixelFormat const & pixel_format, int width, int height, GenTextureParameters const & parameters) const;
 
-	/**
-	* TextureArrayGenerator : an helper class that is used to generate texture array    GL_TEXTURE_1D_ARRAY,    GL_TEXTURE_2D_ARRAY or    GL_TEXTURE_CUBE_ARRAY
-	*/
+  protected:
 
-	class TextureArrayGenerator
-	{
-	public:
-
-		class SliceInfo
-		{
-		public:
-
-			/** first slice allocated for the generator */
-			int first_slice{0};
-			/** number of slice used by the generator */
-			int slice_count{0};
-		};
-
-		class GeneratorEntry
-		{
-		public:
-			/** the proxy that will request some slices */
-			ImageSliceGeneratorProxyBase * proxy{nullptr};
-			/** optional where the slices are allocated */
-			SliceInfo * slice_info{nullptr};
-		};
-
-		/** constructor */
-		TextureArrayGenerator() = default;
-		/** destructor */
-		virtual ~TextureArrayGenerator();
-
-		/** the insertion method (returns the slice considered) */
-		bool AddGenerator(ImageSliceGenerator const & generator, SliceInfo * slice_info = nullptr);
-		/** clean all generators */
-		void Clean();
-		/** generate the texture array */
-		boost::intrusive_ptr<Texture> GenerateTexture(GenTextureParameters const & parameters = GenTextureParameters());
-
-	protected:
-
-		/** internal method to generate the texture array */
-		boost::intrusive_ptr<Texture> GenerateTexture(ImageSliceRegister & slice_register, int bpp, int width, int height, GenTextureParameters const & parameters) const;
-
-	protected:
-
-		/** the registered element to generate */
-		std::vector<GeneratorEntry> generators;
-	};
+    /** the registered element to generate */
+    std::vector<GeneratorEntry> generators;
+  };
 
 };
--- libraries/chaos/src/TextureArrayGenerator.cpp
+++ libraries/chaos/src/TextureArrayGenerator.cpp
@@ -3,218 +3,229 @@
 
 namespace chaos
 {
-	// ========================================================================
-	// ImageSliceRegister functions
-	// ========================================================================
+  // ========================================================================
+  // ImageSliceRegister functions
+  // ========================================================================
 
-	bool ImageSliceRegister::InsertSlice(ImageDescription & description, void * user_data)
-	{
-		// test whether the slice format is valid
-		if (!IsImageSliceValid(description))
-			return false;      
+  bool ImageSliceRegister::InsertSlice(ImageDescription & description, void * user_data)
+  {
+    // test whether the slice format is valid
+    if (!IsImageSliceValid(description))
+      return false;
 
-		// insert the slice into the register
-		ImageSliceRegisterEntry entry;
-		entry.description = description;
-		entry.user_data   = user_data;
-		slices.push_back(entry);
-		return true;
-	}
+    // insert the slice into the register
+    ImageSliceRegisterEntry entry;
+    entry.description = description;
+    entry.user_data = user_data;
+    slices.push_back(entry);
+    return true;
+  }
 
-	bool ImageSliceRegister::IsImageSliceValid(ImageDescription const & description) const
-	{
-		if (!description.IsValid())
-			return false;
-		if (description.IsEmpty())
-			return false;
-		if (description.pixel_format.component_type != PixelFormat::TYPE_UNSIGNED_CHAR)
-			return false;
-		return true;
-	}
+  bool ImageSliceRegister::IsImageSliceValid(ImageDescription const & description) const
+  {
+    if (!description.IsValid())
+      return false;
+    if (description.IsEmpty())
+      return false;
+    return true;
+  }
 
-	// ========================================================================
-	// ImageSliceGenerator functions
-	// ========================================================================
+  // ========================================================================
+  // ImageSliceGenerator functions
+  // ========================================================================
 
-	ImageSliceGeneratorProxyBase * ImageSliceGenerator::CreateProxy() const
-	{
-		if (image != nullptr)
-			return new ImageSliceGeneratorProxy(image, release_image);
-		else
-		{
-			FIBITMAP * image = ImageTools::LoadImageFromFile(image_path.string().c_str());
-			if (image != nullptr)
-				return new ImageSliceGeneratorProxy(image, true);
-		}
-		return nullptr;
-	}
+  ImageSliceGeneratorProxyBase * ImageSliceGenerator::CreateProxy() const
+  {
+    if (image != nullptr)
+      return new ImageSliceGeneratorProxy(image, release_image);
+    else
+    {
+      FIBITMAP * image = ImageTools::LoadImageFromFile(image_path.string().c_str());
+      if (image != nullptr)
+        return new ImageSliceGeneratorProxy(image, true);
+    }
+    return nullptr;
+  }
 
-	// ========================================================================
-	// ImageSliceGeneratorProxy functions
-	// ========================================================================
+  // ========================================================================
+  // ImageSliceGeneratorProxy functions
+  // ========================================================================
 
-	ImageSliceGeneratorProxy::~ImageSliceGeneratorProxy()
-	{
-		if (release_image)
-		{
-			if (image != nullptr)
-			{
-				FreeImage_Unload(image);
-				image = nullptr;
-			}
-		}
-	}
+  ImageSliceGeneratorProxy::~ImageSliceGeneratorProxy()
+  {
+    if (release_image)
+    {
+      if (image != nullptr)
+      {
+        FreeImage_Unload(image);
+        image = nullptr;
+      }
+    }
+  }
 
-	void ImageSliceGeneratorProxy::AddSlices(ImageSliceRegister & slice_register)
-	{
-		if (image != nullptr)
-			slice_register.InsertSlice(ImageTools::GetImageDescription(image), image);
-	}
+  void ImageSliceGeneratorProxy::AddSlices(ImageSliceRegister & slice_register)
+  {
+    if (image != nullptr)
+      slice_register.InsertSlice(ImageTools::GetImageDescription(image), image);
+  }
 
-	void ImageSliceGeneratorProxy::ReleaseSlices(ImageSliceRegisterEntry * slices, size_t count)
-	{
-		// no releasing here. wait until proxy destructor is called.
-		// because if user wants to call 
-		//   - generator.GenerateTexture();   twice in a row, we still expect to generate a texture array twice (an effective clean would break that)
-	}
+  void ImageSliceGeneratorProxy::ReleaseSlices(ImageSliceRegisterEntry * slices, size_t count)
+  {
+    // no releasing here. wait until proxy destructor is called.
+    // because if user wants to call 
+    //   - generator.GenerateTexture();   twice in a row, we still expect to generate a texture array twice (an effective clean would break that)
+  }
 
-	// ========================================================================
-	// TextureArrayGenerator functions
-	// ========================================================================
+  // ========================================================================
+  // TextureArrayGenerator functions
+  // ========================================================================
 
-	TextureArrayGenerator::~TextureArrayGenerator()
-	{
-		Clean();
-	}
+  TextureArrayGenerator::~TextureArrayGenerator()
+  {
+    Clean();
+  }
 
-	bool TextureArrayGenerator::AddGenerator(ImageSliceGenerator const & generator, SliceInfo * slice_info)
-	{
-		// creates the proxy
-		ImageSliceGeneratorProxyBase * proxy = generator.CreateProxy();
-		if (proxy == nullptr)
-			return false;
-		// insert it into the list
-		GeneratorEntry entry;
-		entry.proxy      = proxy;
-		entry.slice_info = slice_info;
-		generators.push_back(entry);
-		return true;
-	}
+  bool TextureArrayGenerator::AddGenerator(ImageSliceGenerator const & generator, SliceInfo * slice_info)
+  {
+    // creates the proxy
+    ImageSliceGeneratorProxyBase * proxy = generator.CreateProxy();
+    if (proxy == nullptr)
+      return false;
+    // insert it into the list
+    GeneratorEntry entry;
+    entry.proxy = proxy;
+    entry.slice_info = slice_info;
+    generators.push_back(entry);
+    return true;
+  }
 
-	void TextureArrayGenerator::Clean()
-	{
-		for (auto it : generators)
-			delete (it.proxy);
-		generators.clear(); // destroy the intrusive_ptr
-	}
+  void TextureArrayGenerator::Clean()
+  {
+    for (auto it : generators)
+      delete (it.proxy);
+    generators.clear(); // destroy the intrusive_ptr
+  }
 
-	boost::intrusive_ptr<Texture> TextureArrayGenerator::GenerateTexture(GenTextureParameters const & parameters)
-	{
-		ImageSliceRegister  slice_register;
-		std::vector<size_t> slice_counts;
-		// insert all slices
-		for (size_t i = 0 ; i < generators.size() ; ++i)            
-		{
-			GeneratorEntry entry = generators[i];
+  boost::intrusive_ptr<Texture> TextureArrayGenerator::GenerateTexture(PixelFormatMergeParams const & merge_params, GenTextureParameters const & parameters)
+  {
+    ImageSliceRegister  slice_register;
+    std::vector<size_t> slice_counts;
+    // insert all slices
+    for (size_t i = 0; i < generators.size(); ++i)
+    {
+      GeneratorEntry entry = generators[i];
 
-			// fill the slice register. keep trace of how many insertion
-			size_t c1 = slice_register.size();
-			entry.proxy->AddSlices(slice_register);
-			size_t c2 = slice_register.size();
+      // fill the slice register. keep trace of how many insertion
+      size_t c1 = slice_register.size();
+      entry.proxy->AddSlices(slice_register);
+      size_t c2 = slice_register.size();
 
-			// return back the slices values
-			if (entry.slice_info != nullptr)
-			{
-				entry.slice_info->first_slice = (int)c1;
-				entry.slice_info->slice_count = (int)(c2 - c1);
-			}
+      // return back the slices values
+      if (entry.slice_info != nullptr)
+      {
+        entry.slice_info->first_slice = (int)c1;
+        entry.slice_info->slice_count = (int)(c2 - c1);
+      }
 
-			slice_counts.push_back(c2 - c1); // insert the count of slices inserted for that generator
-		}
+      slice_counts.push_back(c2 - c1); // insert the count of slices inserted for that generator
+    }
 
-		// no slice, no texture
-		if (slice_register.size() == 0)
-			return nullptr;
+    // no slice, no texture
+    if (slice_register.size() == 0)
+      return nullptr;
 
-		// search max size, max bpp, and final target (GL_TEXTURE_1D_ARRAY or GL_TEXTURE_2D_ARRAY)
-		int width  = 0;
-		int height = 0;
-		int bpp = 0;
-		int component_count = 0;
-		int component_type  = 0;
-		for (ImageSliceRegisterEntry const & entry : slice_register.slices)
-		{
-			width  = max(width,  entry.description.width);
-			height = max(height, entry.description.height);
-			bpp    = max(bpp,    entry.description.pixel_format.GetBPP());
-		}
+    // search max size, max bpp, and final target (GL_TEXTURE_1D_ARRAY or GL_TEXTURE_2D_ARRAY)
+    PixelFormatMerger pixel_format_merger(merge_params);
 
-		// create the texture and fill the slices
-		boost::intrusive_ptr<Texture> result = GenerateTexture(slice_register, bpp, width, height, parameters);
 
-		// release slices
-		size_t start = 0;
-		for (size_t i = 0; i < generators.size(); ++i)
-		{
-			GeneratorEntry entry = generators[i];
+    int width = 0;
+    int height = 0;
+    int component_count = 0;
+    int component_type = 0;
+    for (ImageSliceRegisterEntry const & entry : slice_register.slices)
+    {
+      width = max(width, entry.description.width);
+      height = max(height, entry.description.height);
+      pixel_format_merger.Merge(entry.description.pixel_format);
+    }
 
-			size_t count = slice_counts[i];
-			entry.proxy->ReleaseSlices(&slice_register.slices[start], count); // each proxy is responsible for releasing its own slices
-			start += count;
-		}
+    // test whether the final size is valid
+    if (width <= 0 || height <= 0)
+      return nullptr;
 
-		// clean the generator
-		Clean();
+    PixelFormat pixel_format = pixel_format_merger.GetResult();
+    if (!pixel_format.IsValid())
+      return nullptr;
 
-		return result;
-	}
+    // create the texture and fill the slices
+    boost::intrusive_ptr<Texture> result = GenerateTexture(slice_register, pixel_format, width, height, parameters);
 
-	boost::intrusive_ptr<Texture> TextureArrayGenerator::GenerateTexture(ImageSliceRegister & slice_register, int bpp, int width, int height, GenTextureParameters const & parameters) const
-	{
-		// compute the 'flat' texture target
-		GLenum flat_target = GLTextureTools::GetTextureTargetFromSize(width, height, false);
-		// convert to 'array' target 
-		GLenum array_target = GLTextureTools::ToArrayTextureType(flat_target);
-		assert(array_target != GL_NONE);
+    // release slices
+    size_t start = 0;
+    for (size_t i = 0; i < generators.size(); ++i)
+    {
+      GeneratorEntry entry = generators[i];
 
-		GenTextureResult result;
-		glCreateTextures(array_target, 1, &result.texture_id);
-		if (result.texture_id > 0)
-		{
-			// choose format and internal format (beware FreeImage is BGR/BGRA)
-			GLenum internal_format = GLTextureTools::GetTextureFormatsFromBPP(bpp).internal_format;
-			assert(internal_format != GL_NONE);
+      size_t count = slice_counts[i];
+      entry.proxy->ReleaseSlices(&slice_register.slices[start], count); // each proxy is responsible for releasing its own slices
+      start += count;
+    }
 
-			glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+    // clean the generator
+    Clean();
 
-			int level_count = GLTextureTools::GetMipmapLevelCount(width, height);
-			glTextureStorage3D(result.texture_id, level_count, internal_format, width, height, slice_register.size());
+    return result;
+  }
 
-			for (size_t i = 0; i < slice_register.size(); ++i)
-			{
-				ImageDescription desc = slice_register.slices[i].description;
+  boost::intrusive_ptr<Texture> TextureArrayGenerator::GenerateTexture(ImageSliceRegister & slice_register, PixelFormat const & pixel_format, int width, int height, GenTextureParameters const & parameters) const
+  {
+#if 0
 
-				int bpp  = desc.pixel_format.GetBPP();
-				int type = desc.pixel_format.component_type == (PixelFormat::TYPE_UNSIGNED_CHAR)? GL_UNSIGNED_BYTE : GL_FLOAT;
+    // compute the 'flat' texture target
+    GLenum flat_target = GLTextureTools::GetTextureTargetFromSize(width, height, false);
+    // convert to 'array' target 
+    GLenum array_target = GLTextureTools::ToArrayTextureType(flat_target);
+    assert(array_target != GL_NONE);
 
-				glPixelStorei(GL_UNPACK_ROW_LENGTH, 8 * desc.pitch_size / bpp);
+    GenTextureResult result;
+    glCreateTextures(array_target, 1, &result.texture_id);
+    if (result.texture_id > 0)
+    {
+      // choose format and internal format (beware FreeImage is BGR/BGRA)
+      GLenum internal_format = GLTextureTools::GetTextureFormatsFromBPP(bpp).internal_format;
+      assert(internal_format != GL_NONE);
 
-				GLenum current_format = GLTextureTools::GetTextureFormatsFromBPP(bpp).format;
-				assert(current_format != GL_NONE);
-				glTextureSubImage3D(result.texture_id, 0, 0, 0, i, desc.width, desc.height, 1, current_format, type, desc.data);
-			}
+      glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 
-			result.texture_description.type   = GL_TEXTURE_2D_ARRAY;
-			result.texture_description.width  = width;
-			result.texture_description.height = height;
-			result.texture_description.depth  = slice_register.size();
-			result.texture_description.internal_format = internal_format;
+      int level_count = GLTextureTools::GetMipmapLevelCount(width, height);
+      glTextureStorage3D(result.texture_id, level_count, internal_format, width, height, slice_register.size());
 
-			GLTextureTools::GenTextureApplyParameters(result, parameters);
+      for (size_t i = 0; i < slice_register.size(); ++i)
+      {
+        ImageDescription desc = slice_register.slices[i].description;
 
-			return new Texture(result.texture_id, result.texture_description);
-		}
-		return nullptr;
-	}
+        int bpp = desc.pixel_format.GetBPP();
+        int type = desc.pixel_format.component_type == (PixelFormat::TYPE_UNSIGNED_CHAR) ? GL_UNSIGNED_BYTE : GL_FLOAT;
+
+        glPixelStorei(GL_UNPACK_ROW_LENGTH, 8 * desc.pitch_size / bpp);
+
+        GLenum current_format = GLTextureTools::GetTextureFormatsFromBPP(bpp).format;
+        assert(current_format != GL_NONE);
+        glTextureSubImage3D(result.texture_id, 0, 0, 0, i, desc.width, desc.height, 1, current_format, type, desc.data);
+      }
+
+      result.texture_description.type = GL_TEXTURE_2D_ARRAY;
+      result.texture_description.width = width;
+      result.texture_description.height = height;
+      result.texture_description.depth = slice_register.size();
+      result.texture_description.internal_format = internal_format;
+
+      GLTextureTools::GenTextureApplyParameters(result, parameters);
+
+      return new Texture(result.texture_id, result.texture_description);
+    }
+#endif
+    return nullptr;
+  }
 
 };

3d80d55432026cbc7d9751b47697e8460ea8d430
hubartstephane <hubart.stephane@gmail.com>
2017-10-22 10:36:13 +0200

    create (empty) sound manager files (for true now)

---
+++ libraries/chaos/include/chaos/SoundManager.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <chaos/StandardHeaders.h>
+
+namespace chaos
+{
+
+}; // namespace chaos
---
+++ libraries/chaos/src/SoundManager.cpp
@@ -0,0 +1,6 @@
+#include <chaos/SoundManager.h>
+
+namespace chaos
+{
+
+}; // namespace chaos

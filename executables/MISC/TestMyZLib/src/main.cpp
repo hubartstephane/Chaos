53d237e188f017cf48508a25b9f201e0a0243e88
shubart <hubart.stephane@gmail.com>
2017-11-03 16:26:26 +0100

    ZLIB compression test

--- executables/MISC/TestMyZLib/src/main.cpp
+++ executables/MISC/TestMyZLib/src/main.cpp
@@ -1,19 +1,55 @@
 #include <chaos/WinTools.h>
 #include <chaos/StringTools.h>
 #include <chaos/WinTools.h>
+#include <chaos/MathTools.h>
 #include <chaos/MyZLib.h>
 
+bool AreBuffersEquals(chaos::Buffer<char> const & b1, chaos::Buffer<char> const & b2)
+{
+	if (b1.bufsize != b2.bufsize)
+		return false;
+
+	size_t count = b1.bufsize;
+	for (size_t i = 0 ; i < count ; ++i)
+		if (b1.data[i] != b2.data[i])
+			return false;
+
+	return true;
+}
+
+chaos::Buffer<char> GenerateRandomBuffer()
+{
+	size_t size = (size_t)(5000.0f + 5000.0f * chaos::MathTools::RandFloat());
+
+	chaos::Buffer<char> result = chaos::SharedBufferPolicy<char>::NewBuffer(256 + size);
+	if (result.data != nullptr)
+	{
+		for (size_t i = 0 ; i < 256 ; ++i)
+			result.data[i] = (char)(i); // ensure all numbers appear at least once
+		for (size_t i = 256 ; i < 256 + size ; ++i)
+			result.data[i] = (char)(rand() % 32); // % 32 just to make sur some characters do not happen, and so is the compression better				
+	}
+	return result;
+}
 
 int _tmain(int argc, char ** argv, char ** env)
 {
-  chaos::WinTools::AllocConsoleAndRedirectStdOutput();
+	chaos::WinTools::AllocConsoleAndRedirectStdOutput();
+	
+	chaos::MathTools::ResetRandSeed();
+
+	chaos::Buffer<char> initial_buffer = GenerateRandomBuffer();
+
+	chaos::Buffer<char> compressed = chaos::MyZLib().Encode(initial_buffer);
+
+	chaos::Buffer<char> uncompressed = chaos::MyZLib().Decode(compressed);
+
+	bool success = AreBuffersEquals(initial_buffer, uncompressed);
 
 
+	chaos::WinTools::PressToContinue();
 
-
-  chaos::WinTools::PressToContinue();
-
-  return 0;
+	return 0;
 }
 
 
--- libraries/chaos/src/MyZlib.cpp
+++ libraries/chaos/src/MyZlib.cpp
@@ -1,11 +1,50 @@
 #include <chaos/MyZLib.h>
+#include <chaos/SparseWriteBuffer.h>
 
 namespace chaos
 {
 	Buffer<char> MyZLib::Encode(Buffer<char> const & src)
 	{
-		Buffer<char> result;
+		static const int CHUNK_SIZE = 1024 * 64;
+		
+		SparseWriteBuffer<> writer(CHUNK_SIZE);
 
+		z_stream strm;
+		strm.zalloc = Z_NULL;
+		strm.zfree = Z_NULL;
+		strm.opaque = Z_NULL;
+		strm.avail_in = 0;
+		strm.next_in = Z_NULL;
+
+		if (deflateInit(&strm, Z_DEFAULT_COMPRESSION) == Z_OK)
+		{		
+			unsigned char chunk[CHUNK_SIZE];
+			
+			strm.avail_in = src.bufsize;
+			strm.next_in  = (unsigned char*)src.data; // prepare input
+
+			do
+			{
+				strm.avail_out = CHUNK_SIZE;
+				strm.next_out  = chunk;
+
+				if (deflate(&strm, Z_FINISH) == Z_STREAM_ERROR) // the whole buffer is given so => Z_FINISH
+				{
+					deflateEnd(&strm);
+					return Buffer<char>();
+				}
+
+				int data_to_copy = CHUNK_SIZE - strm.avail_out;
+				if (data_to_copy > 0)
+					writer.Write(chunk, data_to_copy);
+
+			} while (strm.avail_out == 0); // all data in inputs consummed ?
+		
+			deflateEnd(&strm);
+		}
+
+		Buffer<char> result = SharedBufferPolicy<char>::NewBuffer(writer.GetWrittenSize());
+		writer.CopyToBuffer(result.data, result.bufsize);
 		return result;
 	}
 

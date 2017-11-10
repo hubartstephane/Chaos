45f25c8b2e1e92b86b8431c1508c851210d191d7
hubartstephane <hubart.stephane@gmail.com>
2017-11-10 05:54:33 +0100

    add some properties in TileMap resource,= (in tileset)

--- executables/MISC/TestTiledMapReader/resources/tile set 1.tsx
+++ executables/MISC/TestTiledMapReader/resources/tile set 1.tsx
@@ -1,4 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="mon tileset 1" tilewidth="32" tileheight="32" tilecount="49" columns="7">
  <image source="brick.jpg" width="225" height="225"/>
+ <tile id="19">
+  <objectgroup draworder="index">
+   <properties>
+    <property name="prop 1" type="int" value="5"/>
+   </properties>
+  </objectgroup>
+ </tile>
+ <tile id="33">
+  <objectgroup draworder="index">
+   <object id="1" x="-32" y="-68" width="88" height="93"/>
+  </objectgroup>
+ </tile>
 </tileset>

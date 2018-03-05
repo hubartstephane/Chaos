9a3d8eb339d6e15f991231397f0d61c09076ae2d
shubart <hubart.stephane@gmail.com>
2018-03-05 16:44:37 +0100

    Reuse SoundTest refactor files to prepare a SpriteManager refactor

--- executables/SOUND/Test_Sound_Refactor/src/SoundTest.cpp
+++ executables/SOUND/Test_Sound_Refactor/src/SoundTest.cpp
@@ -1,1 +1,24 @@
 #include "SoundTest.h"
+
+#include <chaos/ReferencedObject.h>
+#include <chaos/RenderMaterial.h>
+
+class SpriteLayer
+{
+public:
+
+protected:
+
+	/** the order of the layer in the manager */
+	int order = 0;
+	/** the material used to render the layer */
+	boost::intrusive_ptr<chaos::RenderMaterial> render_material;
+};
+
+
+
+
+class SpriteManager : public chaos::ReferencedObject
+{
+
+};

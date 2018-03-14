bf069ffface809b59f877a84a09b7d00e3990deb
hubartstephane <hubart.stephane@gmail.com>
2018-03-14 23:59:37 +0100

    FilePathParam : resolve path at construction. Do not use pointer on volatile data anymore

--- libraries/chaos/include/chaos/FilePath.h
+++ libraries/chaos/include/chaos/FilePath.h
@@ -6,46 +6,41 @@ namespace chaos
 {
 
 	// ==============================================================
-	// FILE PATH
+	// FILE PATH PARAM
 	// ==============================================================
 
-	// XXX : this is a temporary object. It is used to give parameters to Load/Save functions and retrieve the resolved filename
-  //       it internally use pointer on temp objet. Do not concerve it
+	// XXX : An object that is used to convert common objects (C string, C++ string, boost path) into a boost path object
+  //       (apply a reference path if needed)
 
 	class FilePathParam
 	{
 	public:
 
     /** constructor */
-    FilePathParam(char const * in_basic_path);
+    FilePathParam(char const * basic_path);
     /** constructor */
-    FilePathParam(std::string const & in_string_path);
+    FilePathParam(std::string const & string_path);
     /** constructor */
-    FilePathParam(boost::filesystem::path const & in_filesystem_path);
+    FilePathParam(boost::filesystem::path const & filesystem_path);
 		/** constructor */
-		FilePathParam(char const * in_basic_path, boost::filesystem::path const & in_reference_path);
+		FilePathParam(char const * basic_path, boost::filesystem::path const & reference_path);
 		/** constructor */
-		FilePathParam(std::string const & in_string_path, boost::filesystem::path const & in_reference_path);
+		FilePathParam(std::string const & string_path, boost::filesystem::path const & reference_path);
 		/** constructor */
-		FilePathParam(boost::filesystem::path const & in_filesystem_path, boost::filesystem::path const & in_reference_path);
+		FilePathParam(boost::filesystem::path const & filesystem_path, boost::filesystem::path const & reference_path);
 
     /** resolve if necessary and return the path */
     boost::filesystem::path const & GetResolvedPath() const;
 
   protected:
 
-		/** if path was given as a basic C string */
-		char const * basic_path = nullptr;
-		/** if path was given as a std::string */
-		std::string const * string_path = nullptr;
-		/** if path was given as a filesystem */
-		boost::filesystem::path const * filesystem_path = nullptr;
+    /** internal method to compute the resolved path */
+    void ResolvePath(char const * basic_path, std::string const * string_path, boost::filesystem::path const * filesystem_path, boost::filesystem::path const * reference_path);
 
-    /** the reference path */
-    boost::filesystem::path const * reference_path = nullptr;
+  protected:
 
 		/** the path after resolution */
-		mutable boost::filesystem::path resolved_path;
+		boost::filesystem::path resolved_path;
 	};
 
 }; // namespace chaos
--- libraries/chaos/src/FilePath.cpp
+++ libraries/chaos/src/FilePath.cpp
@@ -3,93 +3,82 @@
 namespace chaos
 {
 
-	// ==============================================================
-	// FILE PATH
-	// ==============================================================
+  // ==============================================================
+  // FILE PATH
+  // ==============================================================
 
-  FilePathParam::FilePathParam(char const * in_basic_path) :
-    basic_path(in_basic_path)
+  FilePathParam::FilePathParam(char const * basic_path)
   {
-
+    ResolvePath(basic_path, nullptr, nullptr, nullptr);
   }
 
-  FilePathParam::FilePathParam(std::string const & in_string_path) :
-    string_path(&in_string_path)
+  FilePathParam::FilePathParam(std::string const & string_path)
   {
-
+    ResolvePath(nullptr, &string_path, nullptr, nullptr);
   }
 
-  FilePathParam::FilePathParam(boost::filesystem::path const & in_filesystem_path) :
-    filesystem_path(&in_filesystem_path)
+  FilePathParam::FilePathParam(boost::filesystem::path const & filesystem_path)
   {
+    ResolvePath(nullptr, nullptr, &filesystem_path, nullptr);
   }
 
-	FilePathParam::FilePathParam(char const * in_basic_path, boost::filesystem::path const & in_reference_path):
-		basic_path(in_basic_path),
-		reference_path(&in_reference_path)
-	{
+  FilePathParam::FilePathParam(char const * basic_path, boost::filesystem::path const & reference_path)
+  {
+    ResolvePath(basic_path, nullptr, nullptr, &reference_path);
+  }
 
-	}
+  FilePathParam::FilePathParam(std::string const & string_path, boost::filesystem::path const & reference_path) 
+  {
+    ResolvePath(nullptr, &string_path, nullptr, &reference_path);
+  }
 
-	FilePathParam::FilePathParam(std::string const & in_string_path, boost::filesystem::path const & in_reference_path):
-		string_path(&in_string_path),
-		reference_path(&in_reference_path)
-	{
+  FilePathParam::FilePathParam(boost::filesystem::path const & filesystem_path, boost::filesystem::path const & reference_path)
+  {
+    ResolvePath(nullptr, nullptr, &filesystem_path, &reference_path);
+  }
 
-	}
+  void FilePathParam::ResolvePath(char const * basic_path, std::string const * string_path, boost::filesystem::path const * filesystem_path, boost::filesystem::path const * reference_path)
+  {
+    boost::filesystem::path temp_path; // for conversion to boost object if necessary
 
-	FilePathParam::FilePathParam(boost::filesystem::path const & in_filesystem_path, boost::filesystem::path const & in_reference_path):
-		filesystem_path(&in_filesystem_path),
-		reference_path(&in_reference_path)
-	{
-	}
+    boost::filesystem::path const * p = nullptr; // a pointer on a boost path
+                                                 
+    // ensure we have a boost path object on which we can work
+    if (basic_path != nullptr)
+    {
+      temp_path = basic_path;
+      p = &temp_path;
+    }
+    else if (string_path != nullptr)
+    {
+      temp_path = string_path->c_str();
+      p = &temp_path;
+    }
+    else if (filesystem_path != nullptr)
+    {
+      p = filesystem_path;
+    }
+    else
+      return;
+                            
+    // cannot resolve or it is not necessary
+    if (reference_path == nullptr || reference_path->empty() || p->is_absolute())
+    {
+      if (p == &temp_path)
+        resolved_path = std::move(temp_path); // can steal the temp_path
+      else
+        resolved_path = *p; // must copy the temp_path		
+    }
+    // resolve the path
+    else
+    {
+      resolved_path = (reference_path->parent_path() / *p).normalize(); // resolve
+    }
+  }
 
-	boost::filesystem::path const & FilePathParam::GetResolvedPath() const
-	{
-		if (resolved_path.empty()) // compute the path or use cache result
-		{
-			boost::filesystem::path temp_path; // for conversion to boost object if necessary
-
-			boost::filesystem::path const * p = nullptr; // a pointer on a boost path
-
-			// ensure we have a boost path object on which we can work
-			if (basic_path != nullptr)
-			{
-				temp_path = basic_path;
-				p         = &temp_path;
-			}
-			else if (string_path != nullptr)
-			{
-				temp_path = string_path->c_str();
-				p         = &temp_path;
-			}
-			else if (filesystem_path != nullptr)
-			{
-				p = filesystem_path;		
-			}
-			else
-				return resolved_path; // error 
-
-			// cannot resolve or it is not necessary
-			if (reference_path == nullptr || reference_path->empty() || p->is_absolute())
-			{
-				if (p == &temp_path)
-					resolved_path = std::move(temp_path); // can steal the temp_path
-				else
-					resolved_path = temp_path; // must copy the temp_path		
-			}
-			// resolve the path
-			else
-			{
-				resolved_path = (reference_path->parent_path() / temp_path).normalize(); // resolve
-			}
-		}
-		return resolved_path;
-	}
-
-
-
-
-
+  boost::filesystem::path const & FilePathParam::GetResolvedPath() const
+  {
+    return resolved_path;
+  }
 
 }; // namespace chaos

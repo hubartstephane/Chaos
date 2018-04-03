383b5dcad52f195581d98207ce12b0f74e30bcf1
hubartstephane <hubart.stephane@gmail.com>
2018-04-04 00:25:09 +0200

    Forgotten GPUResource.cpp file

---
+++ libraries/chaos/src/GPUResource.cpp
@@ -0,0 +1,18 @@
+#include <chaos/GPUResource.h>
+
+namespace chaos
+{
+
+  void GPUResourceCallbacks::OnReleased(GPUResource const * object)
+  {
+    assert(object != nullptr);
+  }
+
+  void GPUResourceAutoCallbacks::OnReleased(GPUResource const * object)
+  {
+    assert(object != nullptr);
+    if (released_func)
+      released_func(object);
+  }
+
+}; // namespace chaos

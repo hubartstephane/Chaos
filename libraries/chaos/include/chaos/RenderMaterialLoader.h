88fd02933c3d2380d56f0e89216a23b52c82c382
hubartstephane <hubart.stephane@gmail.com>
2018-04-08 22:11:56 +0200

    RenderMaterialLoader almost done. Must finish initialization methods from JSON

--- libraries/chaos/include/chaos/RenderMaterialLoader.h
+++ libraries/chaos/include/chaos/RenderMaterialLoader.h
@@ -11,7 +11,7 @@
 namespace chaos
 {
 
-	class RenderMaterialLoader : public ManagerResourceLoader<EmptyClass, GPUResourceManager>
+	class RenderMaterialLoader : public ManagerResourceLoader<GPUFileResourceFriend, GPUResourceManager>
 	{
 	public:
 
@@ -32,43 +32,5 @@ namespace chaos
 		/** search whether the name is already in used in the manager */
 		virtual bool IsNameAlreadyUsedInManager(std::string const & in_name) const override;
 	};
-
-#if 0
-
-	/**
-	* RenderMaterialLoader
-	*/
-
-	class RenderMaterialLoader : protected GPUFileResourceFriend
-	{
-	public:
-
-		/** constructor */
-		RenderMaterialLoader(GPUResourceManager * in_resource_manager);
-
-		/** destructor */
-		virtual ~RenderMaterialLoader() = default;
-
-		/** Generate a render material from a json content */
-		virtual RenderMaterial * GenRenderMaterialObject(nlohmann::json const & json, boost::filesystem::path const & config_path, std::string & parent_name) const;
-		/** Generate a render material from an file */
-		virtual RenderMaterial * GenRenderMaterialObject(FilePathParam const & path, std::string & parent_name) const;
-
-	protected:
-
-		/** internal generate a render material from a json content */
-		virtual RenderMaterial * DoGenRenderMaterialObject(nlohmann::json const & json, boost::filesystem::path const & config_path, std::string & parent_name) const;
-		/** internal generate a render material from an file */
-		virtual RenderMaterial * DoGenRenderMaterialObject(FilePathParam const & path, std::string & parent_name) const;
-
-	protected:
-
-		/** the manager concerned by the loader */
-		GPUResourceManager * resource_manager = nullptr;
-		/** indicates whether the resource path is already detected */
-		mutable bool material_path_resolved = false;
-	};
-
-#endif
 
 }; // namespace chaos
--- libraries/chaos/src/RenderMaterialLoader.cpp
+++ libraries/chaos/src/RenderMaterialLoader.cpp
@@ -9,32 +9,69 @@ namespace chaos
 
 
 	RenderMaterialLoader::RenderMaterialLoader(GPUResourceManager * in_resource_manager) :
-		ManagerResourceLoader<EmptyClass, GPUResourceManager>(in_resource_manager)
+		ManagerResourceLoader<GPUFileResourceFriend, GPUResourceManager>(in_resource_manager)
 	{
 
 	}
 
 	RenderMaterial * RenderMaterialLoader::GenRenderMaterialObject(nlohmann::json const & json, boost::filesystem::path const & config_path, std::string & parent_name) const
 	{
+		// get the name, ensure no name collision
+		if (!CheckResourceName(json))
+			return nullptr;
+
+		// indirect call
+		std::string path;
+		if (JSONTools::GetAttribute(json, "path", path))
+			return GenRenderMaterialObject(path, parent_name);
+
+		// create a new material
+		RenderMaterial * result = new RenderMaterial;
+		if (result == nullptr)
+			return nullptr;
+
+		// search material parent
+		if (parent_name.empty())
+			JSONTools::GetAttribute(json, "parent_material", parent_name);
+
+		// search program
+		nlohmann::json const * program = JSONTools::GetStructure(json, "program");
+		if (program != nullptr)
+		{
+
+			program = program;
+		}
+
+		// look at textures
+		nlohmann::json const * textures = JSONTools::GetStructure(json, "textures");
+		if (textures != nullptr)
+		{
+
+			textures = textures;
+		}
+
+		// look at uniforms
+		nlohmann::json const * uniforms = JSONTools::GetStructure(json, "uniforms");
+		if (uniforms != nullptr)
+		{
 
 
+			uniforms = uniforms;
+		}
 
+		FinalizeLoadedResource(result);
 
-
-
-
-		return nullptr;
+		return result;
 	}
 
 	RenderMaterial * RenderMaterialLoader::GenRenderMaterialObject(FilePathParam const & path, std::string & parent_name) const
 	{
+		if (!CheckResourcePath(path))
+			return nullptr;
 
-
-
-
-
-
-
+		nlohmann::json json;
+		if (JSONTools::LoadJSONFile(path, json, true))
+			return GenRenderMaterialObject(json, path.GetResolvedPath(), parent_name);
 		return nullptr;
 	}
 
@@ -47,148 +84,5 @@ namespace chaos
 	{
 		return (manager->FindRenderMaterial(in_name.c_str()) != nullptr);
 	}
-
-
-
-
-
-
-
-
-
-
-
-#if 0
-
-	RenderMaterialLoader::RenderMaterialLoader(GPUResourceManager * in_resource_manager) :
-		resource_manager(in_resource_manager)
-	{
-		assert(in_resource_manager != nullptr);
-	}
-
-	RenderMaterial * RenderMaterialLoader::GenRenderMaterialObject(nlohmann::json const & json, boost::filesystem::path const & config_path, std::string & parent_name) const
-	{
-		material_path_resolved = false;
-		return DoGenRenderMaterialObject(json, config_path, parent_name);
-	}
-
-	RenderMaterial * RenderMaterialLoader::GenRenderMaterialObject(FilePathParam const & path, std::string & parent_name) const
-	{
-		if (resource_manager->FindRenderMaterialByPath(path) != nullptr)  // ensure the Manager does not already have a RenderMaterial with the same path
-			return nullptr;
-		material_path_resolved = false;
-		return DoGenRenderMaterialObject(path, parent_name);
-	}
-
-
-
-
-
-
-
-	RenderMaterial * RenderMaterialLoader::DoGenRenderMaterialObject(nlohmann::json const & json, boost::filesystem::path const & config_path, std::string & parent_name) const
-	{
-		// the entry has a reference to another file => recursive call
-		std::string p;
-		if (JSONTools::GetAttribute(json, "path", p))
-		{
-			FilePathParam path(p, config_path);
-			return DoGenRenderMaterialObject(path, parent_name);
-		}
-
-		JSONTools::GetAttribute(json, "parent_material", parent_name);
-
-#if 0
-
-
-
-
-
-
-
-		nlohmann::json const * program = JSONTools::GetStructure(json, "program");
-		if (program != nullptr)
-		{
-
-			program = program;
-		}
-
-		std::string parent_material;
-		if (JSONTools::GetAttribute(json, "parent_material", parent_material))
-		{
-
-			parent_material = parent_material;
-		}
-
-		nlohmann::json const * textures = JSONTools::GetStructure(json, "textures");
-		if (textures != nullptr)
-		{
-
-			textures = textures;
-		}
-
-		nlohmann::json const * uniforms = JSONTools::GetStructure(json, "uniforms");
-		if (uniforms != nullptr)
-		{
-
-
-			uniforms = uniforms;
-		}
-		nlohmann::json const * program = JSONTools::GetStructure(json, "program");
-		if (program != nullptr)
-		{
-
-			program = program;
-		}
-
-		std::string parent_material;
-		if (JSONTools::GetAttribute(json, "parent_material", parent_material))
-		{
-
-			parent_material = parent_material;
-		}
-
-		nlohmann::json const * textures = JSONTools::GetStructure(json, "textures");
-		if (textures != nullptr)
-		{
-
-			textures = textures;
-		}
-
-		nlohmann::json const * uniforms = JSONTools::GetStructure(json, "uniforms");
-		if (uniforms != nullptr)
-		{
-
-
-			uniforms = uniforms;
-		}
-
-
-#endif
-
-
-
-
-		return nullptr;
-	}
-
-	RenderMaterial * RenderMaterialLoader::DoGenRenderMaterialObject(FilePathParam const & path, std::string & parent_name) const
-	{
-		if (resource_manager->FindRenderMaterialByPath(path) != nullptr)  // ensure the Manager does not already have a RenderMaterial with the same path
-			return nullptr;
-
-		RenderMaterial * result = nullptr;
-
-		nlohmann::json json;
-		if (JSONTools::LoadJSONFile(path, json, true))
-		{
-			result = DoGenRenderMaterialObject(json, path.GetResolvedPath(), parent_name);
-			if (result != nullptr)
-				SetResourcePath(result, path.GetResolvedPath()); // update the path
-		}
-		return result;
-	}
-
-#endif
 
 }; // namespace chaos

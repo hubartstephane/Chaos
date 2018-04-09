57b1740813ce77c2414a9d516a628d3c5c121d6e
shubart <hubart.stephane@gmail.com>
2018-04-09 15:24:57 +0200

    New project TestGLFW_RenderMaterial

---
+++ executables/GLFW/TestGLFW_RenderMaterial/subpremake5.lua
@@ -0,0 +1,7 @@
+-- =============================================================================
+-- ROOT_PATH/executables/GLFW/TestGLFW_RenderMaterial
+-- =============================================================================
+
+  WindowedApp()
+  DependOnLib("CHAOS")
+  DeclareResource("resources")    

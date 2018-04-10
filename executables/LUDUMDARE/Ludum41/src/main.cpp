2c31dea4403a477ff421765713c3786a4b03a825
hubartstephane <hubart.stephane@gmail.com>
2018-04-11 00:32:07 +0200

    Prepare an empty Ludum 41 project

---
+++ executables/LUDUMDARE/Ludum41/src/main.cpp
@@ -0,0 +1,134 @@
+#include <chaos/StandardHeaders.h> 
+#include <chaos/CollisionFramework.h> 
+#include <chaos/FileTools.h> 
+#include <chaos/LogTools.h> 
+#include <chaos/GLTools.h> 
+#include <chaos/GLTextureTools.h>
+#include <chaos/MyGLFWGamepadManager.h> 
+#include <chaos/MyGLFWSingleWindowApplication.h> 
+#include <chaos/MyGLFWWindow.h> 
+#include <chaos/WinTools.h> 
+#include <chaos/GPUProgramGenerator.h>
+#include <chaos/Application.h>
+#include <chaos/SimpleMeshGenerator.h>
+#include <chaos/GLDebugOnScreenDisplay.h>
+#include <chaos/SimpleMesh.h>
+#include <chaos/GPUProgramData.h>
+#include <chaos/GPUProgram.h>
+#include <chaos/Texture.h>
+#include <chaos/VertexDeclaration.h>
+#include <chaos/GPUProgramProvider.h>
+#include <chaos/SoundManager.h>
+#include <json.hpp>
+#include <chaos/BoostTools.h>
+#include <chaos/BitmapAtlas.h>
+#include <chaos/BitmapAtlasGenerator.h>
+#include <chaos/TextureArrayAtlas.h>
+#include <chaos/SpriteManager.h>
+#include <chaos/JSONTools.h>
+
+#include "game.h"
+
+// ======================================================================================
+
+class MyGLFWWindowOpenGLTest1 : public chaos::MyGLFW::Window
+{
+
+	static float const VIEWPORT_WANTED_ASPECT;
+
+protected:
+
+	virtual void OnKeyEvent(int key, int scan_code, int action, int modifier) override
+	{
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		{
+			if (modifier & GLFW_MOD_SHIFT)
+			{
+				RequireWindowClosure();
+				return;
+			}						
+		}
+
+		if (game != nullptr)
+			if (game->OnKeyEvent(key, action))
+				return;
+	}
+
+	virtual bool OnDraw(glm::ivec2 size) override
+	{
+		glm::vec4 clear_color(0.0f, 0.0f, 0.0f, 0.0f);
+		glClearBufferfv(GL_COLOR, 0, (GLfloat*)&clear_color);
+
+		float far_plane = 1000.0f;
+		glClearBufferfi(GL_DEPTH_STENCIL, 0, far_plane, 0);
+
+		chaos::GLTools::SetViewportWithAspect(size, VIEWPORT_WANTED_ASPECT);
+
+		glEnable(GL_DEPTH_TEST);
+		glDisable(GL_CULL_FACE);   // when viewer is inside the cube
+
+		float world_x = 1000.0f;
+		float world_y = world_x / VIEWPORT_WANTED_ASPECT;
+
+		if (game != nullptr)
+			game->Display(glm::vec2(world_x, world_y));
+
+		return true;
+	}
+
+	virtual void Finalize() override
+	{
+		game = nullptr;
+	}
+
+	virtual bool InitializeFromConfiguration(nlohmann::json const & config, boost::filesystem::path const & config_path) override
+	{   
+		game = new Game;
+		if (game == nullptr)
+			return false;
+
+		float WORLD_X = 1000.0f;
+		glm::vec2 world_size = glm::vec2(WORLD_X, WORLD_X / VIEWPORT_WANTED_ASPECT);
+		if (!game->Initialize(glfw_window, chaos::JSONTools::GetStructure(config, "game"), config_path, world_size))
+			return false;
+
+		return true;
+	}
+
+	virtual void TweakHints(chaos::MyGLFW::WindowHints & hints, GLFWmonitor * monitor, bool pseudo_fullscreen) const override
+	{
+		chaos::MyGLFW::Window::TweakHints(hints, monitor, pseudo_fullscreen);
+
+#if !_DEBUG
+		hints.toplevel  = 1;
+#endif
+		hints.decorated = 1;
+	}
+
+	virtual bool Tick(double delta_time) override
+	{
+		game->Tick(delta_time);
+
+		return true; // refresh
+	}
+
+protected:
+
+	boost::intrusive_ptr<Game> game;
+};
+
+float const MyGLFWWindowOpenGLTest1::VIEWPORT_WANTED_ASPECT = (16.0f / 9.0f);
+
+int _tmain(int argc, char ** argv, char ** env)
+{
+	chaos::MyGLFW::SingleWindowApplicationParams params;
+	params.monitor = nullptr;
+	params.width = 500;
+	params.height = 500;
+	params.monitor_index = 0;
+	chaos::MyGLFW::RunWindowApplication<MyGLFWWindowOpenGLTest1>(argc, argv, env, params);
+
+	return 0;
+}
+
+

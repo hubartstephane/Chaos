173d7970b59b2e2c88ec83fbd4fb69ba29aa6b87
shubart <hubart.stephane@gmail.com>
2018-04-21 09:16:15 +0200

    Missing InputMode.h

---
+++ libraries/chaos/include/chaos/InputMode.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <chaos/StandardHeaders.h>
+
+namespace chaos
+{
+	class InputMode
+	{
+	public:
+
+		static int const Keyboard = 0;
+		static int const Mouse    = 1;
+		static int const Gamepad  = 2;	
+	};
+};

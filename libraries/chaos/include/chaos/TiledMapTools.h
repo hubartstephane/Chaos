fb1e5269045852ccdb0fc4509f397fe432c83a10
shubart <hubart.stephane@gmail.com>
2018-06-15 16:30:03 +0200

    New TileMapTools.h/cpp (empty) files

---
+++ libraries/chaos/include/chaos/TiledMapTools.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <chaos/StandardHeaders.h>
+#include <chaos/TiledMap.h>
+
+namespace chaos
+{
+	namespace TiledMapTools
+	{
+
+	}; // namespace TiledMapTools
+
+}; // namespace chaos
---
+++ libraries/chaos/src/TiledMapTools.cpp
@@ -0,0 +1,11 @@
+#include <chaos/TiledMapTools.h>
+#include <chaos/TiledMap.h>
+
+namespace chaos
+{
+	namespace TiledMapTools
+	{
+
+	};  // namespace TiledMapTools
+
+}; // namespace chaos

94c0a52ca80a9d35aeecedb68cc1c3ca58dc9b76
shubart <hubart.stephane@gmail.com>
2018-06-29 16:16:40 +0200

    GPURenderBuffer and GPUFrameBuffer in progress

--- libraries/chaos/include/chaos/GPUFramebuffer.h
+++ libraries/chaos/include/chaos/GPUFramebuffer.h
@@ -6,10 +6,97 @@
 namespace chaos
 {
 
+	class GPURenderbuffer : public GPUResource
+	{
+	public:
+
+		/** constructor */
+		GPURenderbuffer(GLuint in_id = 0);
+		/** destructor */
+		virtual ~GPURenderbuffer();
+
+		/** returns true whether the resource is valid */
+		bool IsValid() const { return glIsRenderbuffer(renderbuffer_id) == GL_TRUE; }
+		/** returns the GL name of the resource */
+		GLuint GetResourceID() const { return renderbuffer_id; }
+
+	protected:
+
+		/** cleaning the object */
+		virtual bool DoRelease() override;
+
+	protected:
+
+		/** the resource id */
+		GLuint renderbuffer_id = 0;
+	};
+
+
 	class GPUFramebuffer : public GPUResource
 	{
 	public:
 
+		/** constructor */
+		GPUFramebuffer(GLuint in_id = 0);
+		/** destructor */
+		virtual ~GPUFramebuffer();
+
+		/** returns true whether the resource is valid */
+		bool IsValid() const { return glIsFramebuffer(framebuffer_id) == GL_TRUE; }
+		/** returns the GL name of the resource */
+		GLuint GetResourceID() const { return framebuffer_id; }
+
+	protected:
+
+		/** cleaning the object */
+		virtual bool DoRelease() override;
+
+	protected:
+
+		/** the resource id */
+		GLuint framebuffer_id = 0;
+		/** private and shared resources */
+		std::vector<boost::intrusive_ptr<GPUResource>> resources;
 	};
+
+
+	class GPUFramebufferGenerator
+	{
+	public:
+
+		/** generate the framebuffer */
+		GPUFramebuffer * GenerateFramebuffer();
+
+		/** Clear the generator */
+		void Clear();
+
+
+
+
+
+	};
+
+
+
+
+#if 0
+	GLuint framebuffer = 0;
+	glGenFramebuffers(1, &framebuffer);
+
+	GLuint depth_renderbuffer = 0;
+	glGenRenderbuffers(1, &depth_renderbuffer);
+
+	glNamedRenderbufferStorage(depth_renderbuffer, GL_DEPTH24_STENCIL8, 512, 512);
+
+	GLuint color_renderbuffer = 0;
+	glGenRenderbuffers(1, &color_renderbuffer);
+	glNamedRenderbufferStorage(color_renderbuffer, GL_RGBA, 512, 512);
+
+	glNamedFramebufferRenderbuffer(framebuffer, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, color_renderbuffer);
+	glNamedFramebufferTexture(framebuffer, GL_COLOR_ATTACHMENT0, texture_id, texture_level);
+	glCheckNamedFramebufferStatus(framebuffer, GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE;
+
+#endif
+
 
 }; // namespace chaos
--- libraries/chaos/src/GPUFramebuffer.cpp
+++ libraries/chaos/src/GPUFramebuffer.cpp
@@ -3,4 +3,64 @@
 namespace chaos
 {
 
+	// ==========================================================================
+
+
+	GPURenderbuffer::GPURenderbuffer(GLuint in_id) :
+		renderbuffer_id(in_id)
+	{
+	}
+
+	GPURenderbuffer::~GPURenderbuffer()
+	{
+		DoRelease();
+	}
+
+	bool GPURenderbuffer::DoRelease()
+	{
+		if (renderbuffer_id == 0)
+			return false;
+		glDeleteRenderbuffers(1, &renderbuffer_id);
+		renderbuffer_id = 0;
+		return true;	
+	}
+
+
+	// ==========================================================================
+
+
+
+	GPUFramebuffer::GPUFramebuffer(GLuint in_id) :
+		framebuffer_id(in_id)
+	{
+	}
+
+	GPUFramebuffer::~GPUFramebuffer()
+	{
+		DoRelease();
+	}
+
+	bool GPUFramebuffer::DoRelease()
+	{
+		if (framebuffer_id == 0)
+			return false;
+		glDeleteFramebuffers(1, &framebuffer_id);
+		framebuffer_id = 0;
+		return true;	
+	}
+
+	// ==========================================================================
+
+
+	GPUFramebuffer * GPUFramebufferGenerator::GenerateFramebuffer()
+	{
+		GPUFramebuffer * result = nullptr;
+
+
+
+
+
+		return result;
+	}
+
 }; // namespace chaos

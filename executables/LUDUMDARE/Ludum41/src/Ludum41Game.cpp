b51c4c9872f50dcbdebb6c7721417c184f6b16fa
hubartstephane <hubart.stephane@gmail.com>
2018-08-04 02:20:58 +0200

    Small tweaks in Ludum41 : do not display combo if 1

--- executables/LUDUMDARE/Ludum41/src/Ludum41Game.cpp
+++ executables/LUDUMDARE/Ludum41/src/Ludum41Game.cpp
@@ -14,6 +14,12 @@
 
 void LudumPlayingHUD::SetComboValue(death::Game * game, int new_combo)
 {
+	if (new_combo < 2)
+	{
+		cached_combo_value = new_combo;
+		combo_allocations = nullptr;
+		return;
+	}
 	CacheAndCreateScoreAllocation(game, new_combo, "Combo : %d x", 60.0f, cached_combo_value, combo_allocations);
 }
 

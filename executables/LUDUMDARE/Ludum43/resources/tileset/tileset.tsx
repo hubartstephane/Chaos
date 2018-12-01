e9d32f2737c6b32e93c2c223fb915de8d3c0d65d
shubart <hubart.stephane@gmail.com>
2018-12-01 12:25:21 +0100

    Ludum43 : New layers for Particles and enemies

--- executables/LUDUMDARE/Ludum43/resources/tileset/tileset.tsx
+++ executables/LUDUMDARE/Ludum43/resources/tileset/tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="tileset" tilewidth="400" tileheight="400" tilecount="2" columns="0">
+<tileset name="tileset" tilewidth="400" tileheight="400" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="5">
   <properties>
@@ -12,5 +12,11 @@
    <property name="OBJECT_TYPE" type="int" value="1"/>
   </properties>
   <image width="400" height="400" source="../sprites/planet3.png"/>
+ </tile>
+ <tile id="7">
+  <image width="400" height="400" source="../sprites/enemy.png"/>
+ </tile>
+ <tile id="8">
+  <image width="50" height="50" source="../sprites/Particles.png"/>
  </tile>
 </tileset>

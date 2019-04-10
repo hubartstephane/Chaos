a47e1ae7af4cb09aebd4110923f4a77dbd520a04
shubart <hubart.stephane@gmail.com>
2019-04-10 18:00:23 +0200

    Some tests for MetaProgramming

--- executables/MISC/TestMetaprogramming/src/main.cpp
+++ executables/MISC/TestMetaprogramming/src/main.cpp
@@ -10,172 +10,108 @@
 #include <chaos/NamedObject.h>
 #include <chaos/ClassTools.h>
 
+
+
+
+
+
 #if 0
-/*
 
 
-template<typename T1, typename T2>
-using A = boost::mpl::inherit_linearly<
-boost::mpl::vector<T1, T2>,
-boost::mpl::inherit< 
-boost::mpl::_1, 
-boost::mpl::identity<boost::mpl::_2> 
-> 
+#define CHAOS_GENERATE_HAS_FUNCTION_METACLASS(function_name)\
+namespace details\
+{\
+char has_function_helper_##function_name(...);\
+template<typename T>\
+auto has_function_helper_##function_name(T const & t) -> decltype(&T::function_name);\
+};\
+template<typename T>\
+using has_function_##function_name = boost::mpl::bool_<\
+	sizeof(details::has_function_helper_##function_name(chaos::meta::GenerateFakeInstance<T>())) != 1\
 >;
-
-*/
-
-/***********************************************************************************************/
-
-bool IsTrue(boost::mpl::true_)
-{
-	return true;
-}
-
-bool IsTrue(boost::mpl::false_)
-{
-	return false;
-}
-
-/***********************************************************************************************/
-
-
-using MyTransf = boost::mpl::vector<
-	chaos::add_logger<boost::mpl::_1>,
-	chaos::add_logger<boost::mpl::_1>
->;
-
-
-
-template<typename BEGIN, typename END, typename BASE>
-class MyClassImpl : public MyClassImpl <
-	typename boost::mpl::next<BEGIN>::type,
-	END,
-	typename boost::mpl::apply<
-	typename boost::mpl::deref<BEGIN>::type,
-	BASE
-	>::type
-> {};
-
-
-template<typename IT, typename BASE>
-class MyClassImpl < IT, IT, BASE > : public BASE {};
-
-template<typename SEQ, typename BASE = chaos::EmptyClass>
-using MyClass = MyClassImpl<
-	typename boost::mpl::template begin<SEQ>::type,
-	typename boost::mpl::template end<SEQ>::type,
-	BASE>;
-
-
-class T : public chaos::BaseClass<chaos::logger> {};
-
-//class T : public chaos::add_logger<> {};
-
-
-//class T : public cond_add_logger<has_logger_tag<boost::mpl::_1>, boost::mpl::_2>
-
-//using TAGS = boost::mpl::vector<chaos::logger>;
-
-//class T : public chaos::cond_add_logger<chaos::has_logger_tag<TAGS>, chaos::EmptyClass> {};
-BOOST_DECLARE_HAS_MEMBER(has_xxx, xxx);
-
-class A : public chaos::NamedObject
-{
-
-};
-
-class B
-{
-public:
-
-	typedef int type;
-};
-
-class C
-{
-public:
-
-	void type() {}
-
-};
-
-class D
-{
-public:
-
-	int type;
-
-};
-
-class MyApplication : public chaos::Application
-{
-protected:
-
-	virtual bool Main() override
-	{
-
-		return true;
-	}
-};
-
-int CHAOS_MAIN(int argc, char ** argv, char ** env)
-{
-
-	auto a0 = boost::mpl::aux::has_type<int>::value;
-	auto a1 = boost::mpl::aux::has_type<A>::value;
-	auto a2 = boost::mpl::aux::has_type<B>::value;
-	auto a3 = boost::mpl::aux::has_type<C>::value;
-	auto a4 = boost::mpl::aux::has_type<D>::value;
-
-
-	auto b0 = boost::mpl::aux::has_type<int>::type();
-	auto b1 = boost::mpl::aux::has_type<A>::type();
-	auto b2 = boost::mpl::aux::has_type<B>::type();
-	auto b3 = boost::mpl::aux::has_type<C>::type();
-	auto b4 = boost::mpl::aux::has_type<D>::type();
-
-	auto c0 = chaos::meta::get_type<int>::type();
-	auto c1 = chaos::meta::get_type<A>::type();
-	auto c2 = chaos::meta::get_type<B>::type();
-	auto c3 = chaos::meta::get_type<C>::type();
-	auto c4 = chaos::meta::get_type<D>::type();
-
-
-
-	chaos::RunApplication<MyApplication>(argc, argv, env);
-	return 0;
-}
-
-
 
 #endif
 
 
 
-class A
+namespace details
 {
+	char has_function_helper_xxx(...);
+	template<typename T>
+	auto has_function_helper_xxx(T const & t) -> decltype(&T::xxx);
+};
+
+template<typename T>
+using has_function_xxx = boost::mpl::bool_<
+	sizeof(details::has_function_helper_xxx(chaos::meta::GenerateFakeInstance<T>())) != 1
+>;
+
+namespace details
+{
+	char has_member_helper_xxx(...);
+	template<typename T>
+	auto has_member_helper_xxx(T const & t) -> decltype(&t.xxx);
+};
+
+template<typename T>
+using has_member_xxx = boost::mpl::bool_<
+	sizeof(details::has_member_helper_xxx(chaos::meta::GenerateFakeInstance<T>())) != 1
+>;
+
+
+namespace details
+{
+	template<typename ...PARAMS>
+	char CanCallImplementation(...);
+
+	template<typename T, typename ...PARAMS>
+	auto CanCallImplementation(T & t, PARAMS... params) -> decltype(&t.fff(params...));
+}
+
+
+
+template<typename T, typename ...PARAMS>
+using CanCall = boost::mpl::bool_<
+	sizeof(details::CanCallImplementation(chaos::meta::GenerateFakeInstance<T>())) != 1
+>;
+
+
+
+
+struct A
+{
+	void fff(){}
 
 };
 
-class B : public A
+struct B : public A
 {
 	int xxx;
 
+	void fff(int){}
+
 };
 
-class C
+struct C
 {
 	int xxx() 
 	{
 		return 0;
 	}
 
+	void fff(int, float){}
+
 };
 
-class D
+struct D
 {
 	class xxx {};
+
+};
+
+struct E
+{
+	static int xxx;
 
 };
 
@@ -195,12 +131,43 @@ int CHAOS_MAIN(int argc, char ** argv, char ** env)
 	using X2 = boost::mpl::bool_<has_xxx<B>::value>; // data    => true
 	using X3 = boost::mpl::bool_<has_xxx<C>::value>; // member function => true !!!
 	using X4 = boost::mpl::bool_<has_xxx<D>::value>; // nested class => true !!!
+	using X5 = boost::mpl::bool_<has_xxx<E>::value>; // static data
 
 
 	auto f1 = X1::value;
 	auto f2 = X2::value;
 	auto f3 = X3::value;
 	auto f4 = X4::value;
+	auto f5 = X5::value;
+
+
+
+
+
+
+
+
+	auto b1 = has_function_xxx<A>::value;
+	auto b2 = has_function_xxx<B>::value;
+	auto b3 = has_function_xxx<C>::value;
+	auto b4 = has_function_xxx<D>::value;
+	auto b5 = has_function_xxx<E>::value;
+
+
+	auto c1 = has_member_xxx<A>::value;
+	auto c2 = has_member_xxx<B>::value;
+	auto c3 = has_member_xxx<C>::value;
+	auto c4 = has_member_xxx<D>::value;
+	auto c5 = has_member_xxx<E>::value;
+
+	
+
+	auto d1 = CanCall<A>::value;
+	auto d2 = CanCall<B>::value;
+	auto d3 = CanCall<C>::value;
+	auto d4 = CanCall<D>::value;
+	auto d5 = CanCall<E>::value;
+
 
 	return 0;
 }

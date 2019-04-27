b5fe842585d65a4bc771d3bd0fe4293c89e3f3b5
shubart <hubart.stephane@gmail.com>
2019-04-27 09:27:58 +0200

    LD44 PowerUp files (forgotten until now)

---
+++ executables/LUDUMDARE/Ludum44/src/Ludum44PowerUp.cpp
@@ -0,0 +1,120 @@
+#include "Ludum44PowerUp.h"
+#include "Ludum44Player.h"
+#include "Ludum44Game.h"
+
+// =================================================
+// LudumPowerUp
+// =================================================
+
+
+bool LudumPowerUp::InitializeFromConfiguration(char const * json_name, nlohmann::json const & config, boost::filesystem::path const & config_path)
+{
+	std::string cost_json_name = std::string("cost_") + json_name;
+	chaos::JSONTools::GetAttribute(config, cost_json_name.c_str(), life_cost);
+
+	return true;
+}
+
+bool LudumPowerUp::ApplyPowerUp(LudumGame * game, LudumPlayer * player) const
+{
+	if (!CanPowerUp(game, player)) // a final test
+		return false;
+	player->current_life -= life_cost;
+	if (player->current_life > player->current_max_life)
+		player->current_life = player->current_max_life;
+	return true;
+}
+
+bool LudumPowerUp::CanPowerUp(LudumGame * game, LudumPlayer * player) const
+{
+	if (player->current_max_life > life_cost)
+		return true;
+	return false;
+}
+
+// =================================================
+// LudumSpeedUp
+// =================================================
+
+bool LudumSpeedUp::ApplyPowerUp(LudumGame * game, LudumPlayer * player) const
+{
+	if (!LudumPowerUp::ApplyPowerUp(game, player)) // test if still possible to power up (see super method)
+		return false;
+	player->current_speed += game->player_speed.increment_value;
+	if (player->current_speed > game->player_speed.max_value)
+		player->current_speed = game->player_speed.max_value;
+	return true;
+}
+
+bool LudumSpeedUp::CanPowerUp(LudumGame * game, LudumPlayer * player) const
+{
+	if (!LudumPowerUp::CanPowerUp(game, player))
+		return false;
+	if (player->current_speed >= game->player_speed.max_value)
+		return false;
+	return true;
+}
+
+char const * LudumSpeedUp::GetPowerUpTitle() const
+{
+	return "Speed Up";
+}
+
+
+// =================================================
+// LudumDamageUp
+// =================================================
+
+bool LudumDamageUp::ApplyPowerUp(LudumGame * game, LudumPlayer * player) const
+{
+	if (!LudumPowerUp::ApplyPowerUp(game, player)) // test if still possible to power up (see super method)
+		return false;
+	player->current_damage += game->player_damage.increment_value;
+	if (player->current_damage > game->player_damage.max_value)
+		player->current_damage = game->player_damage.max_value;
+
+	return true;
+}
+
+bool LudumDamageUp::CanPowerUp(LudumGame * game, LudumPlayer * player) const
+{
+	if (!LudumPowerUp::CanPowerUp(game, player))
+		return false;
+	if (player->current_damage >= game->player_damage.max_value)
+		return false;
+	return true;
+}
+
+char const * LudumDamageUp::GetPowerUpTitle() const
+{
+	return "Damage Up";
+}
+
+// =================================================
+// LudumFireRateUp
+// =================================================
+
+bool LudumFireRateUp::ApplyPowerUp(LudumGame * game, LudumPlayer * player) const
+{
+	if (!LudumPowerUp::ApplyPowerUp(game, player)) // test if still possible to power up (see super method)
+		return false;
+	player->current_fire_rate += game->player_fire_rate.increment_value;
+	if (player->current_fire_rate > game->player_fire_rate.max_value)
+		player->current_fire_rate = game->player_fire_rate.max_value;
+	return true;
+}
+
+bool LudumFireRateUp::CanPowerUp(LudumGame * game, LudumPlayer * player) const
+{
+	if (!LudumPowerUp::CanPowerUp(game, player))
+		return false;
+	if (player->current_fire_rate >= game->player_fire_rate.max_value)
+		return false;
+	return true;
+}
+
+char const * LudumFireRateUp::GetPowerUpTitle() const
+{
+	return "Fire Rate Up";
+}
+
---
+++ executables/LUDUMDARE/Ludum44/src/Ludum44PowerUp.h
@@ -0,0 +1,76 @@
+#pragma once
+
+#include "Ludum44Player.h"
+
+#include <chaos/StandardHeaders.h> 
+#include <chaos/ReferencedObject.h>
+
+#include <death/Game.h>
+#include <death/GameFramework.h>
+
+// =================================================
+// LudumPowerUp
+// =================================================
+
+class LudumPowerUp : public chaos::ReferencedObject
+{
+public:
+
+	virtual bool ApplyPowerUp(LudumGame * game, LudumPlayer * player) const;
+
+	virtual bool CanPowerUp(LudumGame * game, LudumPlayer * player) const;
+
+	virtual char const * GetPowerUpTitle() const { return nullptr; }
+
+	virtual bool InitializeFromConfiguration(char const * json_name, nlohmann::json const & config, boost::filesystem::path const & config_path);
+
+protected:
+
+	float life_cost = 1.0f;
+};
+
+// =================================================
+// LudumPowerUp
+// =================================================
+
+class LudumSpeedUp : public LudumPowerUp
+{
+public:
+
+	virtual bool ApplyPowerUp(LudumGame * game, LudumPlayer * player) const override;
+
+	virtual bool CanPowerUp(LudumGame * game, LudumPlayer * player) const override;
+
+	virtual char const * GetPowerUpTitle() const override;
+};
+
+// =================================================
+// LudumDamageUp
+// =================================================
+
+class LudumDamageUp : public LudumPowerUp
+{
+public:
+
+	virtual bool ApplyPowerUp(LudumGame * game, LudumPlayer * player) const override;
+
+	virtual bool CanPowerUp(LudumGame * game, LudumPlayer * player) const override;
+
+	virtual char const * GetPowerUpTitle() const override;
+};
+
+// =================================================
+// LudumFireRateUp
+// =================================================
+
+class LudumFireRateUp : public LudumPowerUp
+{
+public:
+
+	virtual bool ApplyPowerUp(LudumGame * game, LudumPlayer * player) const override;
+
+	virtual bool CanPowerUp(LudumGame * game, LudumPlayer * player) const override;
+
+	virtual char const * GetPowerUpTitle() const override;
+};
+

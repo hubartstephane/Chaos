3d7f6d6c40c6115142bc6d40137b679f23b2774b
shubart <hubart.stephane@gmail.com>
2019-04-29 16:27:11 +0200

    LD44 : First level in progress

--- executables/LUDUMDARE/Ludum44/resources/tileset/tileset.tsx
+++ executables/LUDUMDARE/Ludum44/resources/tileset/tileset.tsx
@@ -1,17 +1,35 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.3" name="tileset" tilewidth="640" tileheight="640" tilecount="2" columns="0">
+<tileset version="1.2" tiledversion="1.2.3" name="tileset" tilewidth="640" tileheight="640" tilecount="9" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="0">
-  <properties>
-   <property name="OBJECT_TYPE" type="int" value="1"/>
-  </properties>
-  <image width="640" height="640" source="../sprites/mars.png"/>
- </tile>
  <tile id="1">
   <properties>
    <property name="KEEP_ASPECT_RATIO" type="bool" value="false"/>
    <property name="OBJECT_TYPE" type="int" value="2"/>
   </properties>
   <image width="64" height="64" source="../sprites/rectangle.png"/>
+ </tile>
+ <tile id="2">
+  <image width="400" height="400" source="../sprites/planet1.png"/>
+ </tile>
+ <tile id="3">
+  <image width="400" height="400" source="../sprites/planet2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="400" height="400" source="../sprites/planet3.png"/>
+ </tile>
+ <tile id="5">
+  <image width="640" height="640" source="../sprites/planet4.png"/>
+ </tile>
+ <tile id="6">
+  <image width="400" height="400" source="../sprites/planet5.png"/>
+ </tile>
+ <tile id="7">
+  <image width="500" height="500" source="../sprites/planet6.png"/>
+ </tile>
+ <tile id="8">
+  <image width="464" height="464" source="../sprites/planet7.png"/>
+ </tile>
+ <tile id="9">
+  <image width="50" height="50" source="../sprites/Particles.png"/>
  </tile>
 </tileset>

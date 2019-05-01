c6abc81352fdddd688f845d21f5f025658c07bde
hubartstephane <hubart.stephane@gmail.com>
2019-05-02 01:44:24 +0200

    Test in LD43 : ConstCast : to fix

--- executables/LUDUMDARE/Ludum43/src/main.cpp
+++ executables/LUDUMDARE/Ludum43/src/main.cpp
@@ -9,25 +9,59 @@
 template<typename T> 
 class Casted
 {
+protected:
+
+	Casted(T * in_ptr) : ptr(in_ptr) {}
+
 public:
 
-	Casted(T * in_ptr) : ptr(in_ptr) 
-	{
-		//assert(in_ptr != nullptr;)
-	}
-
 	template<typename U>
-	operator U * ()
+	operator U * () const
 	{
 		return dynamic_cast<U *>(ptr);
 
 	}
+
+	template<typename T>
+	friend Casted<T> cast(T * ptr) { return Casted<T>(ptr); }
 
 protected:
 
 	T * ptr = nullptr;
 
 };
+
+template<typename T>
+class ConstCasted
+{
+protected:
+
+	ConstCasted(T * in_ptr) : ptr(in_ptr) {}
+
+public:
+	
+	template<typename U>
+	operator U const * () const
+	{
+		return dynamic_cast<const U *>(ptr);
+	}
+
+	template<typename T>
+	friend ConstCasted<T> cast(T const * ptr) { return ConstCasted<T>(ptr); }
+
+protected:
+
+	T const * ptr = nullptr;
+
+};
+
+
+
+
+
+
+
+
 
 class A
 {
@@ -53,27 +87,47 @@ public:
 
 };
 
-template<typename T>
-Casted<T> cast(T * ptr) { return Casted<T>(ptr); }
+
+
+
 
 
 int CHAOS_MAIN(int argc, char ** argv, char ** env)
 {
-	A * a = new A;
-	B * b = new B;
-	C * c = nullptr; // new C;
+	{
+		A * a = new A;
+		B * b = new B;
+		C * c = nullptr; // new C;
 
-	A * a1 = cast(a);
-	B * b1 = cast(a);
-	C * c1 = cast(a);
+		A * a1 = cast(a);
+		B * b1 = cast(a);
+		C * c1 = cast(a);
 
-	A * a2 = cast(b);
-	B * b2 = cast(b);
-	C * c2 = cast(b);
+		A * a2 = cast(b);
+		B * b2 = cast(b);
+		C * c2 = cast(b);
 
-	A * a3 = cast(c);
-	B * b3 = cast(c);
-	C * c3 = cast(c);
+		A * a3 = cast(c);
+		B * b3 = cast(c);
+		C * c3 = cast(c);
+	}
+	{
+		A const * a = new A;
+		B const * b = new B;
+		C const * c = nullptr; // new C;
+
+		A const * a1 = cast(a);
+		B const * b1 = cast(a);
+		C const * c1 = cast(a);
+
+		A const * a2 = cast(b);
+		B const * b2 = cast(b);
+		C const * c2 = cast(b);
+
+		A const * a3 = cast(c);
+		B const * b3 = cast(c);
+		C const * c3 = cast(c);
+	}
 
 
 	chaos::MyGLFW::SingleWindowApplicationParams params;

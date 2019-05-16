a5a710a2fd4f02d6cfda132a349f5307f105fddf
shubart <hubart.stephane@gmail.com>
2019-05-16 17:47:39 +0200

    New GPUFence files (empty)

---
+++ libraries/chaos/include/chaos/GPUFence.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include <chaos/StandardHeaders.h>
+#include <chaos/GPUResource.h>
+
+namespace chaos
+{
+
+	class GPUFence : public GPUResource
+	{
+	public:
+
+#if 0
+
+		/** constructor */
+		GPUFence(GLuint in_id = 0, SurfaceDescription const & in_surface_description = SurfaceDescription());
+		/** destructor */
+		virtual ~GPUFence();
+
+		/** returns true whether the resource is valid */
+		bool IsValid() const { return glIsRenderbuffer(renderbuffer_id) == GL_TRUE; }
+		/** returns the GL name of the resource */
+		GLuint GetResourceID() const { return renderbuffer_id; }
+		/** get the description of the surface */
+		SurfaceDescription const & GetSurfaceDescription() const override { return surface_description; }
+
+	protected:
+
+		/** cleaning the object */
+		virtual bool DoRelease() override;
+
+	protected:
+
+		/** the resource id */
+		GLuint renderbuffer_id = 0;
+
+#endif
+
+	};
+
+}; // namespace chaos
---
+++ libraries/chaos/src/GPUFence.cpp
@@ -0,0 +1,29 @@
+﻿#include <chaos/GPUFence.h>
+
+namespace chaos
+{
+
+#if 0
+
+	GPUFence::GPUFence(GLuint in_id, SurfaceDescription const & in_surface_description) :
+		renderbuffer_id(in_id),
+		surface_description(in_surface_description)
+	{
+	}
+
+	GPUFence::~GPUFence()
+	{
+		DoRelease();
+	}
+
+	bool GPUFence::DoRelease()
+	{
+		if (renderbuffer_id == 0)
+			return false;
+		glDeleteRenderbuffers(1, &renderbuffer_id);
+		renderbuffer_id = 0;
+		return true;	
+	}
+#endif
+
+}; // namespace chaos

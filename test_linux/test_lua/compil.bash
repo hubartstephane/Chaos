78af830f9fcc16cea7d0b6e8e085c24c4561cd95
hubartstephane <hubart.stephane@gmail.com>
2019-07-17 17:05:14 +0200

    Update test_linux compilation script to have a separate build directory

--- test_linux/test_lua/compil.bash
+++ test_linux/test_lua/compil.bash
@@ -3,10 +3,14 @@
 cd $(dirname $0)
 TARGET_NAME=$(basename $(pwd))
 
+TARGET_DIR=../../../build/test_linux/$TARGET_NAME
+mkdir -p $TARGET_DIR
+
+
 LIBS="lua-5.3"
 for LIB in $LIBS; do 
 	CCFLAGS="$CCFLAGS $(pkg-config --libs $LIB)"
 	CCFLAGS="$CCFLAGS $(pkg-config --cflags $LIB)"
 done
 
-gcc $CCFLAGS -o $TARGET_NAME *.cpp
+gcc $CCFLAGS -o $TARGET_DIR/$TARGET_NAME *.cpp

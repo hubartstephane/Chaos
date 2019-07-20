3b1c34d30c046108a7f6944f4df42109b47b231a
hubartstephane <hubart.stephane@gmail.com>
2019-07-20 03:50:55 +0200

    test_lua in progress

--- test_linux/test_lua/compil.bash
+++ test_linux/test_lua/compil.bash
@@ -6,11 +6,21 @@ TARGET_NAME=$(basename $(pwd))
 TARGET_DIR=../../../build/test_linux/$TARGET_NAME
 mkdir -p $TARGET_DIR
 
+#lua5.3-deb-multiarch.h
+LUA_LIB_PATH=./x86_64-linux-gnu/liblua5.3
 
-LIBS="lua-5.3"
+#CCFLAGS="$CCFLAGS -L$LUA_LIB_PATH"
+
+
+CCFLAGS="-ldl"
+
+#LIBS="lua5.3-c++ lua5.3 lua5.3-system"
+
+LIBS="lua5.3"
 for LIB in $LIBS; do 
 	CCFLAGS="$CCFLAGS $(pkg-config --libs $LIB)"
 	CCFLAGS="$CCFLAGS $(pkg-config --cflags $LIB)"
 done
 
+echo gcc $CCFLAGS -o $TARGET_DIR/$TARGET_NAME *.cpp
 gcc $CCFLAGS -o $TARGET_DIR/$TARGET_NAME *.cpp
--- test_linux/test_lua/main.cpp
+++ test_linux/test_lua/main.cpp
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//#define LUA_USE_LINUX
+//#define LUA_LIB
+#include <lua.hpp>
+
 int main(int argc, char **argv)
 {
+	lua_State * state = lua_newstate(nullptr, nullptr);
+	if (state != nullptr)
+	{
+		printf("hello world\n");
 
 
+		lua_close(state);		
+	}
 	return 0;
 }

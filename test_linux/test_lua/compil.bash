6eb502dccb16467ecbe0dde154fd3bdfb43ae2b2
hubartstephane <hubart.stephane@gmail.com>
2019-07-21 20:18:02 +0200

    tweak in test_lua

--- test_linux/test_lua/compil.bash
+++ test_linux/test_lua/compil.bash
@@ -1,5 +1,8 @@
 #! /bin/bash
 
+
+PATH=$PATH:/usr/lib/x86_64-linux-gnu
+
 cd $(dirname $0)
 TARGET_NAME=$(basename $(pwd))
 
@@ -12,15 +15,13 @@ LUA_LIB_PATH=./x86_64-linux-gnu/liblua5.3
 #CCFLAGS="$CCFLAGS -L$LUA_LIB_PATH"
 
 
-CCFLAGS="-ldl"
-
-#LIBS="lua5.3-c++ lua5.3 lua5.3-system"
-
-LIBS="lua5.3"
+CCFLAGS="-std=c++14 -v -march=x86-64"
+LIBS="lua5.3 glfw3"
 for LIB in $LIBS; do 
 	CCFLAGS="$CCFLAGS $(pkg-config --libs $LIB)"
 	CCFLAGS="$CCFLAGS $(pkg-config --cflags $LIB)"
 done
+COMPILER=g++-7
 
-echo gcc $CCFLAGS -o $TARGET_DIR/$TARGET_NAME *.cpp
-gcc $CCFLAGS -o $TARGET_DIR/$TARGET_NAME *.cpp
+echo $COMPILER $CCFLAGS -o $TARGET_DIR/$TARGET_NAME *.cpp
+$COMPILER $CCFLAGS -o $TARGET_DIR/$TARGET_NAME *.cpp
--- test_linux/test_lua/main.cpp
+++ test_linux/test_lua/main.cpp
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//#define GLFW_DLL
+#include <GLFW/glfw3.h>
+
 //#define LUA_USE_LINUX
 //#define LUA_LIB
 #include <lua.hpp>
 
 int main(int argc, char **argv)
 {
+
+
+	glfwInit();
+
+#if 0
 	lua_State * state = lua_newstate(nullptr, nullptr);
 	if (state != nullptr)
 	{
@@ -15,5 +23,6 @@ int main(int argc, char **argv)
 
 		lua_close(state);		
 	}
+#endif
 	return 0;
 }

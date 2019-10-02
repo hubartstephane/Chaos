5b9a08f9e6102c1a76f56e3af75bf87e17287401
hubartstephane <hubart.stephane@gmail.com>
2019-10-02 23:08:58 +0200

    LD45 : clean most resources

--- executables/LUDUMDARE/Ludum45/resources/tilesets/tileset.tsx
+++ executables/LUDUMDARE/Ludum45/resources/tilesets/tileset.tsx
@@ -1,38 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE tileset SYSTEM "http://mapeditor.org/dtd/1.0/map.dtd">
 <tileset version="1.2" tiledversion="1.2.3" name="tileset" tilewidth="640" tileheight="640" tilecount="10" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="1">
-  <properties>
-   <property name="KEEP_ASPECT_RATIO" type="bool" value="false"/>
-   <property name="OBJECT_TYPE" type="int" value="2"/>
-  </properties>
-  <image width="64" height="64" source="../sprites/rectangle.png"/>
- </tile>
- <tile id="2">
-  <image width="400" height="400" source="../sprites/planet1.png"/>
- </tile>
- <tile id="3">
-  <image width="400" height="400" source="../sprites/planet2.png"/>
- </tile>
- <tile id="4">
-  <image width="400" height="400" source="../sprites/planet3.png"/>
- </tile>
- <tile id="5">
-  <image width="640" height="640" source="../sprites/planet4.png"/>
- </tile>
- <tile id="6">
-  <image width="400" height="400" source="../sprites/planet5.png"/>
- </tile>
- <tile id="7">
-  <image width="500" height="500" source="../sprites/planet6.png"/>
- </tile>
- <tile id="8">
-  <image width="464" height="464" source="../sprites/planet7.png"/>
- </tile>
- <tile id="9">
-  <image width="50" height="50" source="../sprites/Particles.png"/>
- </tile>
- <tile id="10">
-  <image width="64" height="64" source="../sprites/restoration_rectangle.png"/>
- </tile>
 </tileset>

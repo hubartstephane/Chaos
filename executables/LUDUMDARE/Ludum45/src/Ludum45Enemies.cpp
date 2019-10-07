4573bd9e580a9bad6bcc4008c1de0a1463a6b618
shubart <hubart.stephane@gmail.com>
2019-10-07 09:59:31 +0200

    LD45 pattern in progress

--- executables/LUDUMDARE/Ludum45/src/Ludum45Enemies.cpp
+++ executables/LUDUMDARE/Ludum45/src/Ludum45Enemies.cpp
@@ -7,6 +7,7 @@ bool EnemyPattern::Initialize(chaos::TiledMap::ObjectTypeDefinition const * def)
 	enemy_count = def->FindPropertyInt("ENEMY_COUNT", 0);
 	particle_initial_range = def->FindPropertyFloat("PARTICLE_INITIAL_RANGE", particle_initial_range);
 	particle_speed = def->FindPropertyFloat("PARTICLE_SPEED", 0);
+	pattern_index =  def->FindPropertyInt("PATTERN_INDEX", pattern_index);
 
 	return true;
 }
@@ -14,9 +15,9 @@ bool EnemyPattern::Initialize(chaos::TiledMap::ObjectTypeDefinition const * def)
 bool EnemyPattern::UpdateParticle(float delta_time, ParticleEnemy * particle, chaos::box2 const & player_box, chaos::box2 const & camera_box)
 {
 
-	float part_ratio = chaos::MathTools::CastAndDiv<float>(particle->enemy_index, particle->enemy_particle_count);
+	float particle_ratio = chaos::MathTools::CastAndDiv<float>(particle->enemy_index, particle->enemy_particle_count);
 
-	float base_offset = particle_initial_range * part_ratio;
+	float base_offset = particle_initial_range * particle_ratio;
 
 	float t = (base_offset + particle_speed * particle->time);
 	
@@ -55,17 +56,13 @@ bool EnemyPattern::UpdateParticle(float delta_time, ParticleEnemy * particle, ch
 	}
 	else if (type == 1)
 	{
-//		target_position.x = t + particle->spawner_surface.position.x;
 
-	//	particle->bounding_box.position.y = part_ratio
+		particle_position.y = spawner_position.y + spawner_half_size.y * (particle_ratio * 2.0f - 1.0f);
+		particle_position.x = spawner_position.x;
 
-	//	particle->velocity = glm::vec2(-2000.0f, 0.0f);
-	//	if (particle->time == 0.0)
-	//		particle->bounding_box.position = p;
-
-	
-	//	if (p.x + particle->bounding_box.half_size.x < min_camera_x)
-	//		return true;
+	// destroy if out of screen for ever
+		if (particle_position.x + particle_half_size.x < min_camera_x)
+			return true;
 	}
 	else if (type == 2)
 	{

b8dea7fc498c11c0a52ed37567bbab73ac415708
shubart <hubart.stephane@gmail.com>
2019-10-11 11:13:32 +0200

    Hide some projects (LUA, NANA, MODEL3D, SOUND) th make solution lighter

--- executables/subpremake5.lua
+++ executables/subpremake5.lua
@@ -7,20 +7,20 @@ local BASE_GROUP = CURRENT_GROUP
 CURRENT_GROUP = BASE_GROUP .. "/GLFW"
 ProcessSubPremake("GLFW")
 
-CURRENT_GROUP = BASE_GROUP .. "/LUA"
-ProcessSubPremake("LUA")
+--CURRENT_GROUP = BASE_GROUP .. "/LUA"
+--ProcessSubPremake("LUA")
 
-CURRENT_GROUP = BASE_GROUP .. "/MISC"
-ProcessSubPremake("MISC")
+--CURRENT_GROUP = BASE_GROUP .. "/MISC"
+--ProcessSubPremake("MISC")
 
-CURRENT_GROUP = BASE_GROUP .. "/SOUND"
-ProcessSubPremake("SOUND")
+--CURRENT_GROUP = BASE_GROUP .. "/SOUND"
+--ProcessSubPremake("SOUND")
 
-CURRENT_GROUP = BASE_GROUP .. "/NANA"
-ProcessSubPremake("NANA")
+--CURRENT_GROUP = BASE_GROUP .. "/NANA"
+--ProcessSubPremake("NANA")
 
-CURRENT_GROUP = BASE_GROUP .. "/MODEL3D"
-ProcessSubPremake("MODEL3D")
+--CURRENT_GROUP = BASE_GROUP .. "/MODEL3D"
+--ProcessSubPremake("MODEL3D")
 
 CURRENT_GROUP = BASE_GROUP .. "/LUDUMDARE"
 ProcessSubPremake("LUDUMDARE")

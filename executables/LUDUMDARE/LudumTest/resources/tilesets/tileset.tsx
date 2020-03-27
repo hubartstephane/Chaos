78c4328580ada9b46162a45e52e47096914f094e
hubartstephane <hubart.stephane@gmail.com>
2020-03-27 01:10:33 +0100

    LDTest : add new sprite in cleaned tileset

--- executables/LUDUMDARE/LudumTest/resources/tilesets/tileset.tsx
+++ executables/LUDUMDARE/LudumTest/resources/tilesets/tileset.tsx
@@ -1,4 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="tileset" tilewidth="0" tileheight="0" tilecount="18" columns="0">
+<tileset version="1.2" tiledversion="1.3.3" name="tileset" tilewidth="32" tileheight="32" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="WALL">
+  <image width="32" height="32" source="../sprites/Brick.png"/>
+ </tile>
+ <tile id="1" type="PLAYER">
+  <image width="32" height="32" source="../sprites/player.png"/>
+ </tile>
 </tileset>

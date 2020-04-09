6c2aa234a663497a4fa07b50b04f0b9ed6aea9cd
Stéphane Hubart <hubart.stephane@gmail.com>
2020-04-10 01:03:49 +0200

    LDTest : Add spike, just for testing. some player GID to fix

--- executables/LUDUMDARE/LudumTest/resources/tilesets/tileset.tsx
+++ executables/LUDUMDARE/LudumTest/resources/tilesets/tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="tileset" tilewidth="32" tileheight="32" tilecount="2" columns="0">
+<tileset version="1.2" tiledversion="1.3.0" name="tileset" tilewidth="32" tileheight="32" tilecount="3" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="WALL">
   <image width="32" height="32" source="../sprites/Brick.png"/>
@@ -10,5 +10,8 @@
    <property name="PARTICLE_CREATION" type="bool" value="false"/>
   </properties>
   <image width="32" height="32" source="../sprites/player.png"/>
+ </tile>
+ <tile id="2" type="Spike">
+  <image width="32" height="32" source="../sprites/spike.png"/>
  </tile>
 </tileset>

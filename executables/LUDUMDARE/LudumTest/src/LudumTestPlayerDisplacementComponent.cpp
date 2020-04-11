7b8b31668e8a8feb13f0e80b0816f1a7ff57b762
Stéphane Hubart <s.hubart@dont-nod.lan>
2020-04-11 21:53:58 +0200

    PlayerDisplacementComponent becomes tickable. First file in LudumTest

---
+++ executables/LUDUMDARE/LudumTest/src/LudumTestPlayerDisplacementComponent.cpp
@@ -0,0 +1,15 @@
+#include "LudumTestPlayer.h"
+#include "LudumTestPlayerDisplacementComponent.h"
+#include "LudumTestLevel.h"
+#include "LudumTestLevelInstance.h"
+#include "LudumTestGame.h"
+#include "LudumTestGameInstance.h"
+#include "LudumTestParticles.h"
+#include "LudumTestGameCheckpoint.h"
+
+bool LudumPlayerDisplacementComponent::DoTick(float delta_time)
+{
+
+
+	return true;
+}

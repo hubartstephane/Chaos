4bc99ae47207d7ff0eb1a5c72229941a319790ad
Stéphane Hubart <s.hubart@dont-nod.lan>
2020-04-12 23:43:22 +0200

    LudumPlayerDisplacementComponent in progress

--- executables/LUDUMDARE/LudumTest/src/LudumTestPlayerDisplacementComponent.cpp
+++ executables/LUDUMDARE/LudumTest/src/LudumTestPlayerDisplacementComponent.cpp
@@ -7,6 +7,14 @@
 #include "LudumTestParticles.h"
 #include "LudumTestGameCheckpoint.h"
 
+glm::vec2 LudumPlayerDisplacementComponent::ClampPlayerVelocity(glm::vec2 velocity) const
+{
+	for (int direction = 0; direction <= 1; ++direction)
+		if (max_pawn_velocity[direction] >= 0.0f && std::abs(velocity[direction]) > max_pawn_velocity[direction])
+			velocity[direction] = max_pawn_velocity[direction] * ((velocity[direction] > 0.0f) ? +1.0f : -1.0f);
+	return velocity;
+}
+
 bool LudumPlayerDisplacementComponent::DoTick(float delta_time)
 {
 	// early exit
@@ -21,15 +29,31 @@ bool LudumPlayerDisplacementComponent::DoTick(float delta_time)
 	if (level_instance != nullptr)
 		level_instance->FindPlayerTileCollisions(player, colliding_tiles);
 
-	// update player displacement
-	chaos::box2 pawn_box = pawn->GetBox();
-
+	// get player inputs of interrests
 	glm::vec2 stick_position = player->GetLeftStickPosition();
 	if (!analogic_stick_mode)
 	{
 		stick_position.x = chaos::MathTools::AnalogicToDiscret(stick_position.x);
 		stick_position.y = chaos::MathTools::AnalogicToDiscret(stick_position.y);
 	}
+
+	int const jump_key_buttons[] = { GLFW_KEY_SPACE, -1 };
+	bool jump_pressed = player->CheckButtonPressed(jump_key_buttons, chaos::XBoxButton::BUTTON_A);
+
+	// get player position
+	chaos::box2 pawn_box = pawn->GetBox();
+	
+	glm::vec2 initial_pawn_position = pawn_box.position;
+
+
+
+
+
+
+
+
+
+
 
 	// sum the forces 
 	glm::vec2 sum_forces = glm::vec2(0.0f, 0.0f);
@@ -58,67 +82,68 @@ bool LudumPlayerDisplacementComponent::DoTick(float delta_time)
 
 	
 
+	
 
 	// clamp the pawn velocity/acceleration
-	for (int direction = 0; direction <= 1; ++direction)
-	{
-		if (max_pawn_velocity[direction] >= 0.0f && std::abs(pawn_velocity[direction]) > max_pawn_velocity[direction])
-		{
-			pawn_velocity[direction] = max_pawn_velocity[direction] * ((pawn_velocity[direction] > 0.0f)? +1.0f : -1.0f);
-			sum_forces[direction] = 0.0f;
-		}
-	}
+
+	// update velocity with force, then clamp
+	pawn_velocity = ClampPlayerVelocity(pawn_velocity + sum_forces * delta_time);
+
+
+	// update internals
+	pawn_box.position += pawn_velocity * delta_time;
+
+
+
+
+
+
+
+
+
+
 
 	
 
 	// handle the collisions
-	bool stopped_x = false;
-	bool stopped_y = false;
-
 	bool touching_floor = false;
 	bool touching_ceil  = false;
 	bool touching_wall  = false;
 
 	for (death::TileParticleCollisionInfo& collision : colliding_tiles)
 	{
+		// WALLS
 		chaos::box2 new_pawn_box = pawn_box;
 		if (chaos::RestrictToOutside(collision.particle.bounding_box, new_pawn_box))
 		{
 			// touching the floor
-			if (!stopped_y && new_pawn_box.position.y > pawn_box.position.y)
+			if (new_pawn_box.position.y > pawn_box.position.y && pawn_velocity.y < 0.0f)
 			{
-				pawn_velocity.y = 0.0f;
-				sum_forces.y = 0.0f;
-				stopped_y = true;
+				initial_pawn_position.y = new_pawn_box.position.y; // force Y velociy to 0 after the loop
 				touching_floor = true;
 			}
 			// touching the ceil
-			if (!stopped_y && new_pawn_box.position.y < pawn_box.position.y)
+			if (new_pawn_box.position.y < pawn_box.position.y && pawn_velocity.y > 0.0f)
 			{
-				pawn_velocity.y = 0.0f;
-				sum_forces.y = 0.0f;
-				stopped_y = true;
+				initial_pawn_position.y = new_pawn_box.position.y; // force Y velociy to 0 after the loop
 				touching_ceil = true;
 			}
 			// touching walls
-			if (!stopped_x && new_pawn_box.position.x != pawn_box.position.x)
+			if ((new_pawn_box.position.x - pawn_box.position.x) * (pawn_velocity.x) < 0.0f)
 			{
-				pawn_velocity.x = 0.0f;
-				sum_forces.x = 0.0f;
-				stopped_x = true;
+				initial_pawn_position.x = new_pawn_box.position.x; // force X velociy to 0 after the loop
 				touching_wall = true;
 			}
 		}		
 		pawn_box = new_pawn_box;
-
-		if (stopped_x && stopped_y)
-			break;
 	}
 
 
-	// update internals
-	pawn_box.position += pawn_velocity * delta_time;
-	pawn_velocity += sum_forces * delta_time;
+	// do not compute velocity with acceleration : just take the difference of positions
+	if (delta_time == 0.0f)
+		pawn_velocity = glm::vec2(0.0f, 0.0f);
+	else
+		pawn_velocity = (pawn_box.position - initial_pawn_position) / delta_time;
 
 	// update the player pawn
 	pawn->SetPosition(pawn_box.position);

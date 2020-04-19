be362d672faabe2ec7dec898bb275213f0e1b126
Stéphane Hubart <s.hubart@dont-nod.lan>
2020-04-19 09:44:48 +0200

    LD46 : prepare Burning fire

--- executables/LUDUMDARE/Ludum46/resources/tilesets/tileset.tsx
+++ executables/LUDUMDARE/Ludum46/resources/tilesets/tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.0" name="tileset" tilewidth="80" tileheight="80" tilecount="12" columns="0">
+<tileset version="1.2" tiledversion="1.3.0" name="tileset" tilewidth="80" tileheight="80" tilecount="13" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0" type="WALL">
   <image width="32" height="32" source="../sprites/Brick.png"/>
@@ -56,5 +56,8 @@
    <property name="TRIGGER_TYPE" value="BurnTrigger"/>
   </properties>
   <image width="64" height="64" source="../sprites/SoulBurnTrigger.png"/>
+ </tile>
+ <tile id="14" type="BurningFire">
+  <image width="64" height="64" source="../sprites/BurningFire.png"/>
  </tile>
 </tileset>

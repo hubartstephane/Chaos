60790fc12e6bae4e506abee5561e070310543450
Stéphane Hubart <hubart.stephane@gmail.com>
2020-04-20 16:03:54 +0200

    New SitesOfInterrest.h

---
+++ libraries/chaos/include/chaos/SitesOfInterrest.h
@@ -0,0 +1,19 @@
+
+#pragma once
+
+#if 0
+
+// Pixel Art editing online
+https://www.piskelapp.com/
+
+// working with GIF's online (cut, crop, skip frame ...)
+https://ezgif.com/
+
+// making seamless texture online
+https://www.imgonline.com.ua/eng/make-seamless-texture.php
+
+// Voice generation
+https://www.naturalreaders.com/online/
+
+
+#endif

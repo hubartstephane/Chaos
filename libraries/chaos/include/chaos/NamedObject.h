10a19b96a893bf4f096fe2ae35f7b1769a32bb8a
hubartstephane <s.hubart@dont-nod.lan>
2020-07-21 23:16:00 +0200

    New NamedFilterObject

--- libraries/chaos/include/chaos/NamedObject.h
+++ libraries/chaos/include/chaos/NamedObject.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <chaos/StandardHeaders.h>
-#include <chaos/Object.h>
 #include <chaos/Tag.h>
 #include <chaos/ObjectRequest.h>
 
@@ -47,47 +46,9 @@ namespace chaos
 		shared_ptr<T> object;
 	};
 
-
-
-
-
-
-
-
-
-
-	/** NamedObjectFilter : a class to filter objects by their name */
-	class NamedObjectFilter : public Object
-	{
-	public:
-
-		virtual bool CheckName(NamedObject const & object) const { return true; }
-	};
-
-	/** NamedObjectFilterList : filter objects with a list */
-	class NamedObjectFilterList : public NamedObjectFilter
-	{
-	public:
-
-		/** override */
-		virtual bool CheckName(NamedObject const & object) const override;
-
-	public:
-
-		/** if not empty, refuse objects that are not in this list (or in enable_tags) */
-		std::vector<std::string> enable_names;
-		/** if not empty, refuse objects that are not in this list (or in enable_names) */
-		std::vector<TagType> enable_tags;
-		/** refuse any object in this list */
-		std::vector<std::string> forbidden_names;
-		/** refuse any object in this list */
-		std::vector<TagType> forbidden_tags;
-	};
-
-
 	/** function to serialize into JSON */
-	bool SaveIntoJSON(nlohmann::json & json_entry, NamedObject const & src);
+	bool SaveIntoJSON(nlohmann::json& json_entry, NamedObject const& src);
 	/** function to serialize from JSON */
-	bool LoadFromJSON(nlohmann::json const & json_entry, NamedObject & dst);
+	bool LoadFromJSON(nlohmann::json const& json_entry, NamedObject& dst);	
 
 }; // namespace chaos
--- libraries/chaos/src/NamedObject.cpp
+++ libraries/chaos/src/NamedObject.cpp
@@ -11,56 +11,17 @@ namespace chaos
 			name = in_name;
 	}
 
-	bool NamedObjectFilterList::CheckName(NamedObject const & object) const
-	{
-		// test for forbidden list
-		char const * object_name = object.GetName();
-		if (object_name != nullptr && strlen(object_name) > 0)
-		{
-			size_t c1 = forbidden_names.size();
-			for (size_t i = 0; i < c1; ++i)
-				if (StringTools::Stricmp(forbidden_names[i].c_str(), object_name) == 0)
-					return false;
-		}
-
-		TagType tag = object.GetTag();
-
-		size_t c2 = forbidden_tags.size();
-		for (size_t i = 0; i < c2; ++i)
-			if (forbidden_tags[i] == tag)
-				return false;
-
-		// check for requirement (OR condition)
-		size_t c3 = enable_names.size();
-		size_t c4 = enable_tags.size();
-		if (c3 > 0 || c4 > 0)
-		{
-			if (object_name != nullptr && strlen(object_name) > 0)
-			{
-				for (size_t i = 0; i < c3; ++i)
-					if (StringTools::Stricmp(enable_names[i], object_name) == 0)
-						return true;
-			}
-			for (size_t i = 0; i < c4; ++i)
-				if (enable_tags[i] == tag)
-					return true;
-			// if requirement list, we must met at least one condition
-			return false; 
-		}
-		return true; // no requirement => OK
-	}
-
-	bool SaveIntoJSON(nlohmann::json & json_entry, NamedObject const & src)
+	bool SaveIntoJSON(nlohmann::json& json_entry, NamedObject const& src)
 	{
 		if (!json_entry.is_object())
-			json_entry = nlohmann::json::object(); 
+			json_entry = nlohmann::json::object();
 
 		JSONTools::SetAttribute(json_entry, "name", src.GetName());
 		JSONTools::SetAttribute(json_entry, "tag", src.GetTag());
 		return true;
 	}
 
-	bool LoadFromJSON(nlohmann::json const & json_entry, NamedObject & dst)
+	bool LoadFromJSON(nlohmann::json const& json_entry, NamedObject& dst)
 	{
 		if (!json_entry.is_object())
 			return false;

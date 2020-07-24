d29c58e7938a35aa57a5646a01a1a24b1c25e21e
hubartstephane <s.hubart@dont-nod.lan>
2020-07-25 01:14:05 +0200

    Remove some unused files

--- libraries/chaos/include/chaos/StreamTools.h
+++ libraries/chaos/include/chaos/StreamTools.h
@@ -13,20 +13,21 @@ namespace chaos
 	{
 
 		/** a class that deserves to insert indents into a output stream */
-		class OStreamIndent
+		class Indent
 		{
 		public:
 
 			/** constructor */
-			OStreamIndent(int in_value = 0) :
+			Indent(int in_value = 0) :
 				value(in_value) {}
 			/** friend function to add indents */
-			friend std::ostream & operator << (std::ostream & stream, OStreamIndent const & stream_indent)
+			friend std::ostream & operator << (std::ostream & stream, Indent const & stream_indent)
 			{
 				for (int i = 0; i < stream_indent.value; ++i)
 					stream << "  ";
 				return stream;
 			}
+
 		protected:
 
 			/** number of indent to insert */

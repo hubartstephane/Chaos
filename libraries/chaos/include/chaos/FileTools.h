03d43421bb4898f33230a4fc825562e45bb5a982
hubartstephane <s.hubart@dont-nod.lan>
2020-07-25 01:35:25 +0200

    FileTools becomes a namespace

--- libraries/chaos/include/chaos/FileTools.h
+++ libraries/chaos/include/chaos/FileTools.h
@@ -10,42 +10,32 @@ namespace chaos
 	* FileTools is namespace-class for methods to handle files
 	*/
 
-	class FileTools
+	namespace FileTools
 	{
-	public:
-
 		/** returns true if the extension of a file correspond to a string */
-		static bool IsTypedFile(FilePathParam const & path, char const * expected_ext);
+		bool IsTypedFile(FilePathParam const & path, char const * expected_ext);
 		/** loading a whole file into memory */
-		static Buffer<char> LoadFile(FilePathParam const & path, bool ascii, bool * success_open = nullptr);
+		Buffer<char> LoadFile(FilePathParam const & path, bool ascii, bool * success_open = nullptr);
 
 		/** returns an iterator over a directory (can use resource direct access) */
-		static boost::filesystem::directory_iterator GetDirectoryIterator(FilePathParam const & path);
+		boost::filesystem::directory_iterator GetDirectoryIterator(FilePathParam const & path);
 		/** returns a filepath that is unused */
-		static boost::filesystem::path GetUniquePath(FilePathParam const & path, char const * format, bool create_empty_file, int max_iterations = -1);
+		boost::filesystem::path GetUniquePath(FilePathParam const & path, char const * format, bool create_empty_file, int max_iterations = -1);
 
 		/** create a temporary directory */
-		static bool CreateTemporaryDirectory(char const * pattern, boost::filesystem::path & result);
+		bool CreateTemporaryDirectory(char const * pattern, boost::filesystem::path & result);
 
 		/** read file as a vector of strings */
-		static std::vector<std::string> ReadFileLines(FilePathParam const & path, bool * success_open = nullptr);
+		std::vector<std::string> ReadFileLines(FilePathParam const & path, bool * success_open = nullptr);
 		/** write a file with a vector of strings */
-		static bool WriteFileLines(FilePathParam const & path, std::vector<std::string> const & lines);
+		bool WriteFileLines(FilePathParam const & path, std::vector<std::string> const & lines);
 
 		/** redirect any access (under conditions) to the direct resources path of the project (not the build directory) */
 #if CHAOS_CAN_REDIRECT_RESOURCE_FILES
-		static bool GetRedirectedPath(boost::filesystem::path const & p, boost::filesystem::path & redirected_path);
+		bool GetRedirectedPath(boost::filesystem::path const & p, boost::filesystem::path & redirected_path);
 #endif
 
-	protected:
-
-		/** an utility function to test for extension */
-		static bool DoIsTypedFile(char const * filename, char const * expected_ext);
-		/** an utility function to load a file */
-		static Buffer<char> DoLoadFile(boost::filesystem::path const & resolved_path, bool ascii, bool * success_open);
-		/** utility function to load a file line per line */
-		static std::vector<std::string> DoReadFileLines(boost::filesystem::path const & path, bool * success_open);
-	};
+	}; // namespace FileTools
 
 }; // namespace chaos
 

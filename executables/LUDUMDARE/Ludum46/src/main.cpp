ec333b81b7ca7a1322f4dcb551192186aed7f9b9
hubartstephane <s.hubart@dont-nod.lan>
2020-08-11 12:05:48 +0200

    Some tests in LD46 for SpecialClasses

--- executables/LUDUMDARE/Ludum46/src/main.cpp
+++ executables/LUDUMDARE/Ludum46/src/main.cpp
@@ -1,7 +1,46 @@
 #include <chaos/StandardHeaders.h> 
 #include <death/Death.h> 
 
+#include <chaos/Object.h> 
+#include <chaos/JSONSerializable.h> 
+
 #include "Ludum46Game.h"
+
+class toto : public chaos::Object, public chaos::JSONSerializable
+{
+public:
+
+	CHAOS_OBJECT_DECLARE_CLASS2(toto, chaos::Object);
+
+	virtual bool SerializeFromJSON(nlohmann::json const& json) override;
+
+	virtual bool SerializeIntoJSON(nlohmann::json & json) const override;
+
+	int myvalue = 777;
+
+};
+
+
+bool toto::SerializeFromJSON(nlohmann::json const& json)
+{
+	if (!chaos::JSONSerializable::SerializeFromJSON(json))
+		return false;
+
+	chaos::JSONTools::GetAttribute(json, "myvalue", myvalue);
+
+	return true;
+}
+
+bool toto::SerializeIntoJSON(nlohmann::json& json) const
+{
+	if (!chaos::JSONSerializable::SerializeIntoJSON(json))
+		return false;
+
+	chaos::JSONTools::SetAttribute(json, "myvalue", myvalue);
+
+	return true;
+}
+
 
 int CHAOS_MAIN(int argc, char ** argv, char ** env)
 {

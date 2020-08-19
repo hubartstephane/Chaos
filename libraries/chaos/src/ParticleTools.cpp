d7897e3ab19128f8bfc52a8aa533a06e5356ee65
hubartstephane <hubart.stephane@gmail.com>
2020-08-19 17:42:10 +0200

    LD46 : Half Pixel correction implemented in VertexShader

--- libraries/chaos/src/ParticleTools.cpp
+++ libraries/chaos/src/ParticleTools.cpp
@@ -89,6 +89,9 @@ namespace chaos
 
 		static float EncodeTexcoordZ(int bitmap_index, int vertex_flags)
 		{
+
+			vertex_flags |= ParticleFlags::HEIGHT_BITS_MODE;
+
 			int result = (bitmap_index << 8) | vertex_flags;
 			return *(float*)&result;
 		}

065ed7c142c04ef956bd8613cd3cff9a5acda0e8
hubartstephane <hubart.stephane@gmail.com>
2020-08-20 16:31:41 +0200

    Remove useless methods in ParticleTools

--- libraries/chaos/include/chaos/ParticleTools.h
+++ libraries/chaos/include/chaos/ParticleTools.h
@@ -31,13 +31,8 @@ namespace chaos
         bool SetParticleBox(ParticleAllocationBase* allocation, size_t index, box2 const& box);
 
         /** fill the vertices array with 4 vertices corresponding to 1 QUAD */
-        void GenerateBoxParticle(QuadPrimitive<VertexDefault>& primitive, ParticleCorners const& corners, ParticleTexcoords const& texcoords, float rotation = 0.0f, int flags = 0);
-
         void GenerateBoxParticle(QuadPrimitive<VertexDefault>& primitive, box2 const& box, ParticleTexcoords const& texcoords, float rotation = 0.0f, int flags = 0);
-
         /** fill the vertices array with 6 vertices corresponding to 1 TRIANGLE PAIR */
-        void GenerateBoxParticle(TrianglePairPrimitive<VertexDefault>& primitive, ParticleCorners const& corners, ParticleTexcoords const& texcoords, float rotation = 0.0f, int flags = 0);
-
         void GenerateBoxParticle(TrianglePairPrimitive<VertexDefault>& primitive, box2 const& box, ParticleTexcoords const& texcoords, float rotation = 0.0f, int flags = 0);
 
 		/** an utility method to generate the 4 vertex attributes for a Particle */

4cf4a0e8f973717c34d9527b24724f3139ebc686
hubartstephane <s.hubart@dont-nod.lan>
2020-09-06 23:16:14 +0200

    TMParticleLayerTrait has now particles/vertices dynamic = true by default

--- libraries/death/include/death/TM/TMParticle.h
+++ libraries/death/include/death/TM/TMParticle.h
@@ -13,11 +13,11 @@ namespace death
 	{
 	public:
 
-		// the ID for the tile
+		// the ID for the "tile" (0 if the particle is not a tile)
 		int gid = 0;
+		// the bitmap for the particle
 		chaos::BitmapAtlas::BitmapInfo const* bitmap_info = nullptr; 
 	};
-
 
 	CHAOS_REGISTER_CLASS2(TMParticle, chaos::ParticleDefault);
 
@@ -25,15 +25,6 @@ namespace death
 	// TMParticleLayerTrait
 	// =====================================
 
-	class TMParticleLayerTrait : public chaos::ParticleLayerTrait<TMParticle, chaos::VertexDefault>
-	{
-	public:
-
-		TMParticleLayerTrait()
-		{
-			dynamic_particles = false;
-			dynamic_vertices = false;
-		}
-	};
+	using TMParticleLayerTrait = chaos::ParticleLayerTrait<TMParticle, chaos::VertexDefault>;
 
 }; // namespace death

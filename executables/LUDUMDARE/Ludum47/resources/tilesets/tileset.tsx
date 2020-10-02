22153dffda6d0239792408bd5edfb76adc3fde06
Stephane Hubart <hubart.stephane@gmail.com>
2020-10-03 01:20:49 +0200

    LD47 : clean some assets (mainly sprites)

--- executables/LUDUMDARE/Ludum47/resources/tilesets/tileset.tsx
+++ executables/LUDUMDARE/Ludum47/resources/tilesets/tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="tileset" tilewidth="32" tileheight="32" tilecount="1" columns="0" objectalignment="center">
+<tileset version="1.4" tiledversion="1.4.2" name="tileset" tilewidth="32" tileheight="32" tilecount="2" columns="0" objectalignment="center">
  <grid orientation="orthogonal" width="1" height="1"/>
  <terraintypes>
   <terrain name="Terrain1" tile="0"/>
@@ -9,10 +9,13 @@
  </terraintypes>
  <tile id="1" type="PlayerStart" terrain="0,0,,">
   <properties>
-   <property name="BITMAP_NAME" value="PlayerAnim"/>
+   <property name="BITMAP_NAME" value="player"/>
    <property name="PARTICLE_CREATION" type="bool" value="false"/>
   </properties>
   <image width="32" height="32" source="../sprites/player.png"/>
+ </tile>
+ <tile id="32">
+  <image width="32" height="32" source="../sprites/RedSquare.png"/>
  </tile>
  <wangsets>
   <wangset name="CollisionPlatformer" tile="-1">

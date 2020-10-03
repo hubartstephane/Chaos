61332d4fe9f2c454c4f504c4fee4d599962a8133
Stephane Hubart <hubart.stephane@gmail.com>
2020-10-03 19:19:28 +0200

    LD47: new HUD for Lap count

--- executables/LUDUMDARE/Ludum47/src/Ludum47HUD.h
+++ executables/LUDUMDARE/Ludum47/src/Ludum47HUD.h
@@ -13,7 +13,7 @@ namespace death
 	namespace GameHUDKeys
 	{
 		CHAOS_DECLARE_TAG(RACE_POSITION_ID);
-
+		CHAOS_DECLARE_TAG(RACE_LAPS_ID);		
 	};
 };
 
@@ -36,7 +36,31 @@ public:
 	GameHUDRacePositionComponent(chaos::TagType in_layer_id = death::GameHUDKeys::TEXT_LAYER_ID);
 	/** constructor */
 	GameHUDRacePositionComponent(chaos::ParticleTextGenerator::GeneratorParams const& in_params, chaos::TagType in_layer_id = death::GameHUDKeys::TEXT_LAYER_ID) :
-		death::GameHUDCacheValueComponent<glm::ivec2>("Position: %d/%d", glm::ivec2(-1, -1), in_params, in_layer_id) {}
+		death::GameHUDCacheValueComponent<glm::ivec2>("Pos %d/%d", glm::ivec2(-1, -1), in_params, in_layer_id) {}
+
+	virtual std::string FormatText() const override;
+
+protected:
+
+	/** override */
+	virtual bool UpdateCachedValue(bool& destroy_allocation) override;
+};
+
+
+// ====================================================================
+// GameHUDRaceLapsComponent
+// ====================================================================
+
+class GameHUDRaceLapsComponent : public death::GameHUDCacheValueComponent<glm::ivec2>
+{
+public:
+
+
+	/** constructor */
+	GameHUDRaceLapsComponent(chaos::TagType in_layer_id = death::GameHUDKeys::TEXT_LAYER_ID);
+	/** constructor */
+	GameHUDRaceLapsComponent(chaos::ParticleTextGenerator::GeneratorParams const& in_params, chaos::TagType in_layer_id = death::GameHUDKeys::TEXT_LAYER_ID) :
+		death::GameHUDCacheValueComponent<glm::ivec2>("Pos %d/%d", glm::ivec2(-1, -1), in_params, in_layer_id) {}
 
 	virtual std::string FormatText() const override;
 

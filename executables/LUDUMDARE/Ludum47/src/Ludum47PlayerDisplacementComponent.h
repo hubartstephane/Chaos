63f924e8bdd3938e6ef630e5b652fcfe8ce3bf86
Stephane Hubart <hubart.stephane@gmail.com>
2020-10-04 15:52:28 +0200

    LD47 : opponent can have now collisions with borders

--- executables/LUDUMDARE/Ludum47/src/Ludum47PlayerDisplacementComponent.h
+++ executables/LUDUMDARE/Ludum47/src/Ludum47PlayerDisplacementComponent.h
@@ -8,9 +8,19 @@ class LudumPlayerDisplacementComponent : public death::PlayerDisplacementCompone
 {
 	CHAOS_OBJECT_DECLARE_CLASS2(LudumPlayerDisplacementComponent, death::PlayerDisplacementComponent);
 
+
+public:
+
+	static void ComputeBorderCollision(class ParticleBase& particle, class LudumLevelInstance* li, class CarData const& car_data);
+
+
 protected:
 
 	/** override */
 	virtual bool DoTick(float delta_time) override;
+
+
+
+
 
 };

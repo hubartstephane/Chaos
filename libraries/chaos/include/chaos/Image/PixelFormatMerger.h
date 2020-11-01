05593830a68eca21c57da066a42c36a168e33104
Stephane Hubart <s.hubart@dont-nod.lan>
2020-11-01 01:10:27 +0100

    new PixelFormatMerger files

---
+++ libraries/chaos/include/chaos/Image/PixelFormatMerger.h
@@ -0,0 +1,74 @@
+#ifdef CHAOS_FORWARD_DECLARATION
+
+namespace chaos
+{
+	class PixelFormatMergeParams;
+	class PixelFormatMerger;
+
+}; // namespace chaos
+
+#else 
+
+namespace chaos
+{
+
+	/**
+	* PixelFormatMergeParams
+	*/
+
+	class PixelFormatMergeParams
+	{
+
+	public:
+
+		/** if valid, the resulting pixel format should be set to this */
+		PixelFormat pixel_format;
+		/** if false the pixel_format cannot be 'upgraded' with incomming format */
+		bool upgrade_pixel_format = true;
+		/** if set to false, all luminances formats will become RGB */
+		bool accept_luminance = true;
+		/** if set to false, all float formats will become unsigned char */
+		bool accept_float = true;	
+	};
+
+	bool LoadFromJSON(nlohmann::json const& json_entry, PixelFormatMergeParams& dst);
+
+	bool SaveIntoJSON(nlohmann::json& json_entry, PixelFormatMergeParams const& src);
+
+	/**
+	* PixelFormatMerger : utility function to find a common PixelFormat for multiple images
+	*/
+
+	class PixelFormatMerger
+	{
+
+	public:
+
+		/** constructor */
+		PixelFormatMerger(PixelFormatMergeParams const& in_params = {});
+
+		/** reset the merger */
+		void Reset(PixelFormatMergeParams const& in_params = {});
+		/** the merge method */
+		void Merge(PixelFormat src);
+		/** get the result if available */
+		PixelFormat GetResult() const;
+		/** returns whether all incomming format match the result */
+		bool AreIncommingFormatIdentical() const { return identical_incomming_format; }
+
+	protected:
+
+		/** the result */
+		PixelFormat result;
+		/** whether a valid result is available */
+		bool result_is_available = false;
+		/** the parameter for the merge */
+		PixelFormatMergeParams params;
+		/** true whether all incomming format are identical (no conversion required) */
+		bool identical_incomming_format = true;
+	};
+
+}; // namespace chaos
+
+#endif // CHAOS_FORWARD_DECLARATION
+

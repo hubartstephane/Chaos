6bea62b8e7e02aeaa6ef09c40a3b04883ad04e40
Stephane Hubart <hubart.stephane@gmail.com>
2021-03-09 23:03:33 +0100

    put TMObjectReferenceSolver in its own file

--- libraries/chaos/src/Gameplay/TM/TMObject.cpp
+++ libraries/chaos/src/Gameplay/TM/TMObject.cpp
@@ -2,16 +2,6 @@
 
 namespace chaos
 {
-	// =====================================
-	// TMObjectReferenceSolver implementation
-	// =====================================
-
-	void TMObjectReferenceSolver::SolveReferences(TMLevelInstance* level_instance)
-	{
-		assert(level_instance != nullptr);
-		for (auto& entry : entries)
-			entry.class_solver->Solve(level_instance, entry.weak_ptr_result, entry.id);
-	}
 
 	// =====================================
 	// TMObject implementation

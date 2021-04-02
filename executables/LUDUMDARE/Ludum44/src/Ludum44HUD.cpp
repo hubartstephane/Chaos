1a8159063851d9ba9f191caf6514ebe237daf757
Stephane Hubart <hubart.stephane@gmail.com>
2021-04-02 23:45:33 +0200

    Fix LD44

--- executables/LUDUMDARE/Ludum44/src/Ludum44HUD.cpp
+++ executables/LUDUMDARE/Ludum44/src/Ludum44HUD.cpp
@@ -117,89 +117,22 @@ bool GameHUDPowerUpComponent::DoTick(float delta_time)
 bool GameHUDHealthBarComponent::DoTick(float delta_time)
 {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if 0
-
-
-	LudumPlayingHUD const * playing_hud = auto_cast(hud);
-	if (playing_hud == nullptr)
-		return true;
-
-	LudumGame const * ludum_game = playing_hud->GetGame();
+	LudumGame const* ludum_game = GetGame();
 	if (ludum_game == nullptr)
 		return true;
 
-	LudumGameInstance const * ludum_game_instance = playing_hud->GetGameInstance();
+	LudumGameInstance const* ludum_game_instance = GetGameInstance();
 	if (ludum_game_instance == nullptr)
 		return true;
 
-	LudumPlayer const * ludum_player = ludum_game->GetPlayer(0);
+	LudumPlayer const* ludum_player = GetPlayer(0);
 	if (ludum_player == nullptr)
 		return true;
 
-	// create the allocation
-	if (allocations == nullptr)
-	{
-		chaos::ParticleLayerBase * layer = hud->GetParticleManager()->FindLayer(chaos::GameHUDKeys::LIFE_LAYER_ID);
-		if (layer == nullptr)
-			return true;
-		allocations = layer->SpawnParticles(1);
-		if (allocations == nullptr)
-			return true;
-	}
-	else
-	{
-		allocations->Resize(1);
-	}
-	// fill the particle
-	chaos::ParticleAccessor<ParticleLife> particles = allocations->GetParticleAccessor();
-	if (particles.GetDataCount() == 0)
-		return true;
+	float health = ludum_player->GetHealth();
+	float max_health = ludum_player->GetMaxHealth();
 
-	ParticleLife * part = &particles[0];
-
-	chaos::box2 canvas_box = ludum_game->GetCanvasBox();
-
-	glm::vec2 canvas_size = 2.0f * canvas_box.half_size;
-
-	glm::vec2 position1, position2;
-	position1.x = -canvas_size.x * 0.5f + 40.0f;
-	position1.y = -canvas_size.y * 0.5f + 40.0f;
-
-	position2.x = canvas_size.x * 0.5f - 40.0f;
-	position2.y = -canvas_size.y * 0.5f + 80.0f;
-
-	part->bounding_box = chaos::box2(std::make_pair(position1, position2));
-	part->texcoords.bottomleft = glm::vec2(0.0f, 0.0f);
-	part->texcoords.topright = glm::vec2(1.0f, 1.0f);
-	part->color = glm::vec4(1.0f, 0.0f, 0.0f, 1.0f);
-
-
-
-	part->color.x = ludum_player->GetHealth();
-	part->color.y = ludum_player->GetMaxHealth();
-	part->color.z = 0.0f;
-	part->color.w = 0.0f;
-
+	// change virtually the life displayed
 	if (ludum_game_instance->current_power_up != nullptr && ludum_game_instance->current_powerup_trigger != nullptr)
 	{
 		bool decreasing_power_up = ludum_game_instance->current_powerup_trigger->decrease_power;
@@ -207,17 +140,55 @@ bool GameHUDHealthBarComponent::DoTick(float delta_time)
 		float cost = ludum_game_instance->current_power_up->GetLifeCost();
 		float paid_cost_ratio = ludum_player->GetBuyTimer() / ludum_game->GetBuyUpgradeTime();
 
-		float sign1 = (decreasing_power_up) ? 1.0f : 0.0f;
-		float sign2 = (decreasing_power_up) ? 1.0f : -1.0f;
+		float sign1 = (decreasing_power_up) ? +1.0f :  0.0f;
+		float sign2 = (decreasing_power_up) ? +1.0f : -1.0f;
 
-		part->color.x += sign1 * cost * paid_cost_ratio;
-		part->color.y += sign2 * cost * paid_cost_ratio;
-		if (part->color.y > 1.0f)
-			part->color.y = 1.0f;
-		if (part->color.x > part->color.y)
-			part->color.x = part->color.y;
+		health = health + sign1 * cost * paid_cost_ratio;
+		max_health = max_health + sign2 * cost * paid_cost_ratio;
+		health = std::min(health, max_health);
 	}
-#endif	
+
+	chaos::GPUDrawInterface<chaos::VertexDefault> DI(nullptr);
+
+	chaos::box2 canvas_box = ludum_game->GetCanvasBox();
+	glm::vec2 canvas_size = 2.0f * canvas_box.half_size;
+
+	chaos::QuadPrimitive<chaos::VertexDefault> quads = DI.AddQuads(3);
+
+	glm::vec2 position1, position2;
+	position1.x = -canvas_size.x * 0.5f + 40.0f;
+	position1.y = -canvas_size.y * 0.5f + 40.0f;
+
+	position2.x = canvas_size.x * 0.5f - 40.0f;
+	position2.y = -canvas_size.y * 0.5f + 70.0f;
+
+	// compute the border size
+	position2.x = position1.x + (position2.x - position1.x) * max_health;
+
+	chaos::ParticleDefault particle;
+	particle.texcoords.bitmap_index = -1;
+	// the border
+	particle.bounding_box = chaos::box2(std::make_pair(position1, position2));
+	particle.color = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	ParticleToPrimitive(particle, quads);
+	++quads;
+
+	// the background
+	particle.bounding_box.half_size -= glm::vec2(3.0f, 3.0f);
+	particle.color = glm::vec4(0.0, 0.0, 0.0, 1.0);
+	ParticleToPrimitive(particle, quads);
+	++quads;
+
+	// the life bar
+	std::pair<glm::vec2, glm::vec2> corners = chaos::GetBoxCorners(particle.bounding_box);
+	corners.second.x = corners.first.x + (health / max_health) * (corners.second.x - corners.first.x);
+	particle.bounding_box = chaos::box2(corners);
+	particle.color = glm::vec4(1.0, 0.0, 0.0, 1.0);
+	ParticleToPrimitive(particle, quads);
+	++quads;
+
+	mesh = DI.ExtractMesh();
+
 	return true;
 }
 

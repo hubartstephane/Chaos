f8a464a852c669d86a5066497b1dd4dff26842a1
Stephane Hubart <hubart.stephane@gmail.com>
2021-04-06 18:13:32 +0200

    Clean LD47 HUD

--- executables/LUDUMDARE/Ludum47/src/Ludum47HUD.cpp
+++ executables/LUDUMDARE/Ludum47/src/Ludum47HUD.cpp
@@ -20,76 +20,50 @@ GameHUDRacePositionComponent::GameHUDRacePositionComponent() :
 	generator_params.hotpoint = chaos::Hotpoint::TOP_LEFT;
 }
 
-glm::ivec2 GameHUDRacePositionComponent::QueryValue() const
+bool GameHUDRacePositionComponent::QueryValue(glm::ivec2 & result) const
 {
+	LudumLevelInstance const* li = GetLevelInstance();
+	if (li == nullptr || li->road == nullptr)
+		return false;
 
+	LudumPlayer const * player = GetPlayer(0);
+	if (player == nullptr || player->race_position.IsCompleted())
+		return false;
+
+	result = li->GetPlayerRacePosition(player);
+	return true;
 }
 
 
-void GameHUDRacePositionComponent::UpdateText()
+void GameHUDRacePositionComponent::UpdateMesh()
 {
 	char const* f = ((cached_value.x > cached_value.y / 2) && blink_value) ? "[WARNING Pos %d/%d]" : "Pos %d/%d";
-
-
-
-
 	SetText(chaos::StringTools::Printf(f, 1 + cached_value.x, 1 + cached_value.y).c_str());
 }
-
 
 bool GameHUDRacePositionComponent::DoTick(float delta_time)
 {
 	if (!chaos::GameHUDCacheValueTextComponent<glm::ivec2>::DoTick(delta_time))
 		return false;
-	current_dt = delta_time;
-	return true;
-}
 
-bool GameHUDRacePositionComponent::UpdateCachedValue(bool& destroy_mesh)
-{
-	LudumPlayingHUD const* playing_hud = auto_cast(hud);
-	if (playing_hud != nullptr)
+	if (cached_value.x > cached_value.y / 2)
 	{
-		LudumLevelInstance const* li = playing_hud->GetLevelInstance();
-		if (li != nullptr && li->road != nullptr)
+		blink_timer = std::max(blink_timer - delta_time, 0.0f);
+		if (blink_timer == 0.0f)
 		{
-			LudumPlayer* player = GetPlayer(0);
-			if (player != nullptr && !player->race_position.IsCompleted())
+			blink_timer = 0.5f;
+			blink_value = !blink_value;
+
+			LudumLevelInstance const* li = GetLevelInstance();
+			if (li != nullptr && li->effective_start_timer == 0.0)
 			{
-
-
-				glm::ivec2 p = li->GetPlayerRacePosition(player);
-
-				
-				bool force_update = false;
-				if (p.x > p.y / 2)
-				{
-					blink_timer = std::max(blink_timer - current_dt, 0.0f);
-					if (blink_timer == 0.0f)
-					{
-						blink_timer = 0.5f;
-						blink_value = !blink_value;
-
-						LudumLevelInstance* li = GetLevelInstance();
-						if (li != nullptr && li->effective_start_timer == 0.0)
-						{
-							if (blink_value)
-								player->GetGame()->PlaySound("bip", false, false, 0.0f, chaos::SoundContext::GAME);
-						}
-					}
-					force_update = true;
-				}
-
-
-				if (force_update || p != cached_value)
-				{
-					cached_value = p;
-					return true;
-				}
+				if (blink_value)
+					GetGame()->PlaySound("bip", false, false, 0.0f, chaos::SoundContext::GAME);
 			}
+			InvalidateMesh();
 		}
 	}
-	return false;
+	return true;
 }
 
 // ====================================================================
@@ -111,7 +85,7 @@ bool GameHUDRaceLapsComponent::QueryValue(glm::ivec2 & result) const
 	if (li == nullptr || li->road == nullptr)
 		return false;
 	LudumPlayer const* player = GetPlayer(0);
-	if (player == nullptr || !player->race_position.IsCompleted())
+	if (player == nullptr || player->race_position.IsCompleted())
 		return false;
 	result = { player->race_position.current_lap, li->road->lap_count };
 	return true;

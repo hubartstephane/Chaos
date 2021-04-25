1c8705f846cef197de1ac2b326099767e75b70aa
Stephane Hubart <hubart.stephane@gmail.com>
2021-04-25 03:25:42 +0200

    LD48: Fix fake displacement

--- executables/LUDUMDARE/Ludum48/src/Ludum48LevelInstance.cpp
+++ executables/LUDUMDARE/Ludum48/src/Ludum48LevelInstance.cpp
@@ -129,6 +129,25 @@ bool LudumLevelInstance::DoTick(float delta_time)
 {
 	CollectObjects();
 
+	if (grid_info.cells != nullptr)
+	{
+		HandlePlayerObject(delta_time);
+		HandleFallingObjects(delta_time);
+	}
+
+
+	chaos::TMLevelInstance::DoTick(delta_time);
+
+
+	// completed ?
+	if (completion_timer > 0.0f)
+		completion_timer = std::max(0.0f, completion_timer - delta_time);
+	return true;
+}
+
+
+bool LudumLevelInstance::HandleFallingObjects(float delta_time)
+{
 	auto DI = chaos::GetDebugDrawInterface();
 
 	for (int step : {0, 1})
@@ -306,7 +325,139 @@ bool LudumLevelInstance::DoTick(float delta_time)
 #endif
 
 
-				
+
+	return true;
+}
+
+
+bool LudumLevelInstance::HandlePlayerObject(float delta_time)
+{	
+	LudumPlayer* player = GetPlayer(0);
+	if (player == nullptr)
+		return true;
+
+	LudumPlayerDisplacementComponent* displacement_component = player->GetDisplacementComponent();
+	if (displacement_component == nullptr)
+		return true;
+	
+	chaos::PlayerPawn* pawn = player->GetPawn();
+	if (pawn == nullptr)
+		return true;
+
+	ParticlePlayer* particle = pawn->GetParticle<ParticlePlayer>(0);
+	if (particle == nullptr)
+		return true;
+
+	
+
+
+
+
+	chaos::Key const fake_displacement_key_buttons[] = { chaos::KeyboardButton::LEFT_CONTROL, chaos::KeyboardButton::RIGHT_CONTROL, chaos::GamepadButton::A, chaos::Key() };
+	bool fake_displacement = player->CheckButtonPressed(fake_displacement_key_buttons);
+
+	if (fake_displacement)
+		fake_displacement = true;
+
+
+	// get player inputs of interrests
+	glm::vec2 stick_position = player->GetLeftStickPosition();
+	stick_position.x = chaos::MathTools::AnalogicToDiscret(stick_position.x);
+	stick_position.y = chaos::MathTools::AnalogicToDiscret(stick_position.y);
+	if (std::abs(stick_position.x) > std::abs(stick_position.y))
+		stick_position.y = 0.0f;
+	else
+		stick_position.x = 0.0f;
+
+
+
+	// change pawn direction
+	if (particle->direction.x == 0.0f && particle->direction.y == 0.0f)
+	{
+		if (stick_position != glm::vec2(0.0f, 0.0f))
+		{
+			glm::ivec2 p = grid_info.GetIndexForPosition(particle->bounding_box.position);
+
+			bool inside_world = false;
+			if (stick_position.x > 0 && p.x < grid_info.size.x - 1)
+				inside_world = true;
+			else if (stick_position.x < 0 && p.x > 0.0f)
+				inside_world = true;
+			else if (stick_position.y > 0 && p.y < grid_info.size.y - 1)
+				inside_world = true;
+			else if (stick_position.y < 0 && p.y > 0.0f)
+				inside_world = true;
+
+			if (inside_world)
+			{
+				glm::ivec2 other_index = (p + chaos::RecastVector<glm::ivec2>(stick_position));
+
+				GridCellInfo& other = grid_info(other_index);
+
+
+				bool can_go = false;
+
+				if (other.particle != nullptr)
+				{
+					if (other.particle->type == GameObjectType::Foam)
+					{
+						other.particle->destroy_particle = true;
+						if (!fake_displacement)
+							other.particle = nullptr;
+						can_go = true;
+					}
+					else if (other.particle->type == GameObjectType::Diamond)
+					{
+						other.particle->destroy_particle = true;
+						TakeDiamond();
+						if (!fake_displacement)
+							other.particle = nullptr;
+						can_go = true;
+					}
+					else if (other.particle->type == GameObjectType::Rock)
+					{
+						if (stick_position.x != 0.0f && other.particle->direction == glm::vec2(0.0f, 0.0f)) // can only push horizontally
+						{
+							glm::ivec2 rock_p = grid_info.GetCellCoord(other);
+							if ((rock_p.x > 0 && stick_position.x < 0.0f) || (rock_p.x < grid_info.size.x - 1 && stick_position.x > 0.0f))
+							{
+								GridCellInfo& next_to_rock = grid_info(rock_p + chaos::RecastVector<glm::ivec2>(stick_position));
+								if (next_to_rock.CanLock(other.particle))
+								{
+									next_to_rock.Lock(other.particle);
+									other.particle->direction = stick_position;
+									other.particle->speed = displacement_component->push_speed;
+									particle->direction = stick_position;
+									particle->speed = displacement_component->push_speed;
+								}
+							}
+						}
+					}
+				}
+				else
+					can_go = true;
+
+
+
+				if (can_go && other.CanLock(particle))
+				{
+					if (!fake_displacement)
+					{
+						other.Lock(particle);
+						particle->direction = stick_position;
+						particle->speed = displacement_component->pawn_speed;
+					}
+
+				}
+			}
+		}
+
+	}
+	// update pawn position
+	else
+	{
+		UpdateParticlePositionInGrid(particle, delta_time, grid_info);
+	}
 
 
 
@@ -316,12 +467,9 @@ bool LudumLevelInstance::DoTick(float delta_time)
 
 
 
-	chaos::TMLevelInstance::DoTick(delta_time);
 
 
-	// completed ?
-	if (completion_timer > 0.0f)
-		completion_timer = std::max(0.0f, completion_timer - delta_time);
+
 
 
 

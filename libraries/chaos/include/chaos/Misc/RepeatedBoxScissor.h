6ba26e79498d14b7469d6d6008bff96cfb500ad9
Stephane Hubart <hubart.stephane@gmail.com>
2021-04-28 00:32:16 +0200

    LD48: Fix badly clipped layers

--- libraries/chaos/include/chaos/Misc/RepeatedBoxScissor.h
+++ libraries/chaos/include/chaos/Misc/RepeatedBoxScissor.h
@@ -20,6 +20,8 @@ namespace chaos
 	public:
 
 		/** constructor */
+		RepeatedBoxScissor() = default;
+		/** constructor */
 		RepeatedBoxScissor(box2 const& in_target_box, box2 const& in_scissor_box, bool in_wrap_x, bool in_wrap_y);
 
 		/** offset of a given instance */
@@ -32,7 +34,7 @@ namespace chaos
 		/** the 'index' of the last instance to render/collide ... (excluded) */
 		glm::ivec2 last_instance = glm::ivec2(0, 0);
 		/** copy of the construction parameters */
-		box2 target_box;;
+		box2 target_box;
 		/** copy of the construction parameters */
 		box2 scissor_box;
 		/** copy of the construction parameters */

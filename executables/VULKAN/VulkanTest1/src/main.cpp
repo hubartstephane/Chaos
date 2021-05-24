68866f1533f3397cb41d660ba50cf445f8212782
Stephane Hubart <hubart.stephane@gmail.com>
2021-05-25 00:12:27 +0200

    Vulkan test in progress

--- executables/VULKAN/VulkanTest1/src/main.cpp
+++ executables/VULKAN/VulkanTest1/src/main.cpp
@@ -1,9 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <boost/crc.hpp> 
 #include <chaos/chaos.h>
-
-
 
 #include <range/v3/numeric/accumulate.hpp>
 #include <range/v3/view/iota.hpp>
@@ -28,53 +23,33 @@
 
 #include <range/v3/action/sort.hpp>
 
-class A
-{
-public:
-
-    A(int i)
-        : values(i, i) {};
-
-    auto const & f() const { return values; }
-
-    std::vector<int> values;
-};
-
-#include <vector>
-using std::cout;
-
-
-auto is_six = [](int i) { return i == 6; };
-
-int main(int argc, char** argv, char** env)
-{
-   
-
-
-    chaos::WinTools::AllocConsoleAndRedirectStdOutput();
-
-    std::vector<int> v{ 6, 2, 3, 4, 5, 6 };
-
-    ranges::actions::sort(v);
-
-    for (auto i : v)
-    {
-        std::cout << i << "\n";
-    }
-
-
-
-    return 0;
-}
-
-#if 0
-#include <chaos/chaos.h>
 
 #include <vulkan/vulkan.h>
 
 
+class VulkanApplication
+{
+public:
+
+};
+
+
+static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+    VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+    VkDebugUtilsMessageTypeFlagsEXT messageType,
+    const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+    void* pUserData) 
+{
+    if (messageSeverity >= VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT)
+        std::cerr << "validation layer: " << pCallbackData->pMessage << "\n";
+
+    return VK_FALSE;
+}
+
+
 int CHAOS_MAIN(int argc, char ** argv, char ** env)
 {
+     chaos::WinTools::AllocConsoleAndRedirectStdOutput();
 
 
 
@@ -94,19 +69,38 @@ int CHAOS_MAIN(int argc, char ** argv, char ** env)
         for (uint32_t i = 0; i < glfwExtensionCount; ++i)
         {
             char const* ext = glfwExtensions[i];
-            std::cout << ext << "\n";
+            std::cout << "GLFW extension: " << ext << "\n";
         }
             
         // extensions VULKAN
         uint32_t extensionCount = 0;
         vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-        std::vector<VkExtensionProperties> extensions;
-        extensions.insert(extensions.begin(), extensionCount, {});
-
+        std::vector<VkExtensionProperties> extensions(extensionCount);
         vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+        for (VkExtensionProperties const& ext : extensions)
+            std::cout << "Vulkan extension: " << ext.extensionName << "\n";
+        
+        // layers
+        uint32_t layerCount = 0;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+        for (VkLayerProperties const& layer : availableLayers)
+            std::cout << "Vulkan layer: " << layer.layerName << "\n";
 
         // create vulkan
+
+#if 0
+        VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = debugCallback;
+        createInfo.pUserData = nullptr; // Optional
+#endif
+
         VkInstance instance;
 
         VkApplicationInfo appInfo{};
@@ -116,27 +110,132 @@ int CHAOS_MAIN(int argc, char ** argv, char ** env)
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.apiVersion = VK_API_VERSION_1_0;
+#if 0
+        createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &debugCreateInfo; // create a temporary debugger for the creation only
+#endif
 
+
+
+
+        std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation" /*, "VK_EXT_debug_utils"*/};
 
         VkInstanceCreateInfo createInfo{};
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
         createInfo.enabledExtensionCount = glfwExtensionCount;
         createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = (uint32_t)validationLayers.size();
+        createInfo.ppEnabledLayerNames = validationLayers.data();
 
         VkResult result = vkCreateInstance(&createInfo, nullptr, &instance);
         if (result == VK_SUCCESS)
         {
 
 
+ 
+#if 0
+            
+            VkDebugUtilsMessengerEXT debug_messager;
+            auto func = (PFN_vkCreateDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+            if (func != nullptr)
+                func(instance, &createInfo, nullptr, &debug_messager);
+#endif
 
-            // Loop
-            while (!glfwWindowShouldClose(window))
+            // Physical device
+            VkPhysicalDevice physical_device = VK_NULL_HANDLE;
+
+            uint32_t deviceCount = 0;
+            vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+
+            std::vector<VkPhysicalDevice> devices(deviceCount);
+            vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+            for (VkPhysicalDevice device : devices)
             {
-                glfwPollEvents();
+                VkPhysicalDeviceProperties device_properties;
+                vkGetPhysicalDeviceProperties(device, &device_properties);
+
+                std::cout << "Device Name:            " << device_properties.deviceName << "\n";
+                std::cout << "Max Image Dimension 2D: " << device_properties.limits.maxImageDimension2D << "\n";
+                if (device_properties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+                {
+                    physical_device = device;
+                }
             }
 
+            // queue famillies
+            std::optional<uint32_t> queue_family;
 
+            uint32_t queueFamilyCount = 0;
+            vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queueFamilyCount, nullptr);
+
+            std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+            vkGetPhysicalDeviceQueueFamilyProperties(physical_device, &queueFamilyCount, queueFamilies.data());
+            for (VkQueueFamilyProperties const& qf : queueFamilies)
+            {
+                if (qf.queueFlags & VK_QUEUE_GRAPHICS_BIT)
+                {
+                    queue_family = uint32_t(&qf - &queueFamilies[0]);
+                    break;
+                }
+            }
+
+            if (queue_family.has_value())
+            {
+                // create logical device + queues
+                float priority = 1.0f;
+
+                VkDeviceQueueCreateInfo queueCreateInfo{};
+                queueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+                queueCreateInfo.queueFamilyIndex = queue_family.value();
+                queueCreateInfo.queueCount = 1;
+                queueCreateInfo.pQueuePriorities = &priority;
+
+                VkPhysicalDeviceFeatures deviceFeatures{};
+
+                // see VK_KHR_swapchain layer later
+
+                VkDeviceCreateInfo createInfo{};
+                createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+                createInfo.pQueueCreateInfos = &queueCreateInfo;
+                createInfo.queueCreateInfoCount = 1;
+                createInfo.pEnabledFeatures = &deviceFeatures;
+                createInfo.enabledLayerCount = uint32_t(validationLayers.size());
+                createInfo.ppEnabledLayerNames = validationLayers.data();
+
+                VkDevice device;
+                if (vkCreateDevice(physical_device, &createInfo, nullptr, &device) == VK_SUCCESS)
+                {
+                    // get the queue
+                    VkQueue graphics_queue;
+                    vkGetDeviceQueue(device, queue_family.value(), 0, &graphics_queue);
+
+
+                    // Loop
+                    while (!glfwWindowShouldClose(window))
+                    {
+                        glfwPollEvents();
+                    }
+
+
+
+
+                    // destroy logical device
+                    vkDestroyDevice(device, nullptr);
+                }
+            }
+
+                
+
+
+
+
+            // destroy the logger messager
+#if 0
+            auto func = (PFN_vkDestroyDebugUtilsMessengerEXT) vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+            if (func != nullptr) {
+                func(instance, debug_messager, nullptr);
+#endif
+   
 
 
 
@@ -151,7 +250,3 @@ int CHAOS_MAIN(int argc, char ** argv, char ** env)
 
 	return 0;
 }
-
-#endif
-
-

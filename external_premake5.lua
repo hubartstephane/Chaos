20ea3ed72918408cd32b3b8e80257213c2d50453
Stephane Hubart <hubart.stephane@gmail.com>
2021-09-07 01:00:12 +0200

    Fix compilation issue with JSON methods & path

--- external_premake5.lua
+++ external_premake5.lua
@@ -310,9 +310,13 @@ local TRISTRIPPER_LIB_PATH = {
 DeclareExternalLib("TRISTRIPPER", TRISTRIPPER_INC_PATH, TRISTRIPPER_LIB_PATH, "tristripper.lib")
 
   -- JSON
+  --[[
 local JSON_PATH     = "json-develop"
 local JSON_INC_PATH = path.join(JSON_PATH, "src")  
-
+DeclareExternalLib("JSON", JSON_INC_PATH, nil, nil)
+]]--
+local JSON_PATH     = "json-develop"
+local JSON_INC_PATH = path.join(JSON_PATH, "single_include", "nlohmann")  
 DeclareExternalLib("JSON", JSON_INC_PATH, nil, nil)
 
 

469c03f84d373b7eae3cbda6050784367a983f5a
Stephane Hubart <hubart.stephane@gmail.com>
2021-09-30 02:17:07 +0200

    new JSONParam

---
+++ libraries/chaos/include/chaos/Core/JSONParam.h
@@ -0,0 +1,35 @@
+namespace chaos
+{
+#ifdef CHAOS_FORWARD_DECLARATION
+
+	class JSONParam;
+
+#elif !defined CHAOS_TEMPLATE_IMPLEMENTATION
+
+	// ==============================================================
+	// JSONParam
+	// ==============================================================
+
+	class JSONParam
+	{
+	public:
+
+		/** constructor */
+		JSONParam(nlohmann::json const & in_json);
+		/** constructor */
+		JSONParam(JSONParam const & src) = default;
+		/** constructor */
+		JSONParam(JSONParam && src) = default;
+
+		/** conversion operator */
+		operator nlohmann::json const& () const;
+
+	protected:
+
+		/** the real json object */
+		nlohmann::json const& json;
+	};
+
+#endif
+
+}; // namespace chaos
---
+++ libraries/chaos/src/Core/JSONParam.cpp
@@ -0,0 +1,17 @@
+#include <chaos/Chaos.h>
+
+namespace chaos
+{
+	// ==============================================================
+	// JSONParam
+	// ==============================================================
+
+	JSONParam::JSONParam(nlohmann::json const& in_json):
+		json(in_json) {}
+
+	JSONParam::operator nlohmann::json const& () const
+	{
+		return json;
+	}
+
+}; // namespace chaos

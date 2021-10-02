3b39390a7863b4930a860202922032c86f61c707
Stephane Hubart <hubart.stephane@gmail.com>
2021-10-02 18:53:07 +0200

    LD49: Blocking collision

--- executables/LUDUMDARE/Ludum49/src/Ludum49PlayerDisplacementComponent.cpp
+++ executables/LUDUMDARE/Ludum49/src/Ludum49PlayerDisplacementComponent.cpp
@@ -44,6 +44,11 @@ glm::vec2 ClosestPoint(glm::vec2 const& p, glm::vec2 const& a, glm::vec2 const& 
 
 
 #if 0
+
+	Key const left_buttons[] = { KeyboardButton::LEFT, Key() };
+	bool run_pressed = player->CheckButtonPressed(left_buttons);
+
+
 		if (pawn_velocity.x > 0.0f)
 			pawn_velocity.x = std::max(0.0f, pawn_velocity.x - ludum_player->slow_down * delta_time);
 		else
@@ -51,6 +56,72 @@ glm::vec2 ClosestPoint(glm::vec2 const& p, glm::vec2 const& a, glm::vec2 const& 
 #endif
 		
 
+
+struct CollisionEntry
+{
+	glm::vec2 a;
+	glm::vec2 b;
+	glm::vec2 proj;
+	float l2;
+};
+
+
+std::vector<CollisionEntry> ComputeCollisions(box2 const box, LudumLevelInstance* ludum_level)
+{
+	GPUDrawInterface<VertexDefault> * DI = GetDebugDrawInterface();
+
+	std::vector<CollisionEntry> result;
+
+	sphere2 pawn_sphere = GetInnerSphere(box);
+
+	TMObjectCollisionIterator Iterator = ludum_level->GetObjectCollisionIterator(box, COLLISION_LANDSCAPE, false);
+	while (Iterator)
+	{
+		TMObject & object = *Iterator;
+
+		if (Landscape* landscape = auto_cast(&object))
+		{
+			glm::vec2 offset = landscape->GetBoundingBox(true).position;
+
+			size_t count = landscape->points.size();
+			for (size_t i = 0; i < count; ++i)
+			{
+				glm::vec2 const& a = landscape->points[i] + offset;
+				glm::vec2 const& b = landscape->points[(i + 1) % count] + offset;
+
+				glm::vec2 proj = ClosestPoint(pawn_sphere.position, a, b);
+
+				float l2 = glm::length2(proj - pawn_sphere.position);
+				if (l2 <= pawn_sphere.radius * pawn_sphere.radius)
+				{
+					result.push_back({ a, b, proj, l2 });
+				}
+			}
+		}
+		++Iterator;
+	}
+
+	if (result.size() > 0)
+	{
+		float min_l2 = std::numeric_limits<float>::max();
+
+		CollisionEntry nearest_collision;
+		for (auto const& entry : result)
+		{
+#if 1
+			PointPrimitive<VertexDefault> P = DI->AddPoints(1);
+			P[0].position = entry.proj;
+			P[0].color = { 1.0f, 1.0f, 1.0f, 1.0f };
+			P[0].flags = 0;
+			P[0].texcoord = { -1.0f, -1.0f, -1.0f };
+#endif
+			min_l2 = std::min(min_l2, entry.l2);
+			nearest_collision = entry;
+		}
+	}
+
+	return result;
+}
 
 
 
@@ -68,31 +139,7 @@ bool LudumPlayerDisplacementComponent::DoTick(float delta_time)
 	box2 pawn_box = pawn->GetBoundingBox();
 	box2 pawn_box_before = pawn_box;
 
-	GPUDrawInterface<VertexDefault> * DI = GetDebugDrawInterface();
-	
-#if 0
-		std::pair<glm::vec2, glm::vec2> corners = GetBoxCorners(pawn_box);
-
-		auto quad = DI->AddQuads();
-		for (auto& v : quad)
-		{			
-			v.color = { 1.0f, 0.0f, 0.0f, 1.0f };
-			v.texcoord = { -1.0f, -1.0f, -1.0f };
-			v.flags = 0;
-		}
-
-		quad[0].position = { corners.first.x, corners.first.y };
-		quad[1].position = { corners.second.x, corners.first.y };
-		quad[2].position = { corners.second.x, corners.second.y };
-		quad[3].position = { corners.first.x, corners.second.y };
-		
-#endif
-
-
-
-
-
-
+	LudumLevelInstance* ludum_level = GetLevelInstance();
 
 //	pawn_velocity.y += ludum_player->gravity * delta_time;
 
@@ -117,70 +164,32 @@ bool LudumPlayerDisplacementComponent::DoTick(float delta_time)
 	float acceleration_factor = (direction.x * pawn_velocity.x < 1.0f) ? 2.0f : 1.0f;
 	pawn_velocity.x += acceleration_factor * direction.x * ludum_player->acceleration * delta_time;
 	
+	pawn_box.position += pawn_velocity * delta_time;
 
 #endif
 
 
+	std::vector<CollisionEntry> collisions = ComputeCollisions(pawn_box_before, ludum_level);
 
-
-
-
-
-
-
-
-	Key const left_buttons[] = { KeyboardButton::LEFT, Key() };
-	bool run_pressed = player->CheckButtonPressed(left_buttons);
-
-
-
-	
-
-	//pawn_box.position += pawn_velocity * delta_time;
-
-
-	LudumLevelInstance* ludum_level = GetLevelInstance();
-
-	struct CollisionEntry
+	bool can_move = true;
+	for (CollisionEntry const& entry : collisions)
 	{
-		glm::vec2 a;
-		glm::vec2 b;
-		glm::vec2 proj;
-		float l2;
-	};
+		glm::vec2 p = ClosestPoint(pawn_box.position, entry.a, entry.b);
 
-
-	std::vector<CollisionEntry> collisions;
-
-	sphere2 pawn_sphere = GetInnerSphere(pawn_box);
-
-	TMObjectCollisionIterator Iterator = ludum_level->GetObjectCollisionIterator(pawn_box, COLLISION_LANDSCAPE, false);
-	while (Iterator)
-	{
-		TMObject & object = *Iterator;
-
-		if (Landscape* landscape = auto_cast(&object))
+		if (glm::length2(p - pawn_box.position) < glm::length2(p - pawn_box_before.position))
 		{
-			glm::vec2 offset = landscape->GetBoundingBox(true).position;
-
-			size_t count = landscape->points.size();
-			for (size_t i = 0; i < count; ++i)
-			{
-				glm::vec2 const& a = landscape->points[i] + offset;
-				glm::vec2 const& b = landscape->points[(i + 1) % count] + offset;
-
-				glm::vec2 proj = ClosestPoint(pawn_sphere.position, a, b);
-
-				float l2 = glm::length2(proj - pawn_sphere.position);
-				if (l2 <= pawn_sphere.radius * pawn_sphere.radius)
-				{
-					collisions.push_back({ a, b, proj, l2 });
-				}
-			}
+			can_move = false;
+			break;
 		}
-		++Iterator;
-	}
 
+	}
+	if (can_move)
+		pawn->SetPosition(pawn_box.position);
+
+
+
+
+#if 0
 	if (collisions.size() > 0)
 	{
 		float min_l2 = std::numeric_limits<float>::max();
@@ -215,6 +224,7 @@ bool LudumPlayerDisplacementComponent::DoTick(float delta_time)
 
 
 	}
+#endif
 
 
 
@@ -224,7 +234,6 @@ bool LudumPlayerDisplacementComponent::DoTick(float delta_time)
 
 
 
-	pawn->SetPosition(pawn_box.position);
 
 
 	return true;

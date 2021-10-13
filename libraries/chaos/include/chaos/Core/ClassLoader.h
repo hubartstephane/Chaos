b1ca23da0816b2d74c3260ee2499af15df8f48b0
Stephane Hubart <hubart.stephane@gmail.com>
2021-10-14 00:44:10 +0200

    displace some code from Class into ClassLoader

--- libraries/chaos/include/chaos/Core/ClassLoader.h
+++ libraries/chaos/include/chaos/Core/ClassLoader.h
@@ -14,6 +14,22 @@
 		virtual Class const* LoadClass(FilePathParam const& path);
 		/** load all classes in one directory */
 		virtual bool LoadClassesInDirectory(FilePathParam const& path);
+
+	protected:
+
+		/** internal method to declare a class without finding yet its parent (used for directory iteration) */
+		Class* DoDeclareSpecialClassStep1(char const* class_name, nlohmann::json const& json); // XXX : no const return value here !! (for Finalization of special class)
+		/** finalization of a special class (called from ClassLoader) : find parent */
+		bool DoDeclareSpecialClassStep2(Class* cls);
+		/** finalization of a special class (called from ClassLoader) : creation delegate */
+		bool DoDeclareSpecialClassStep3(Class* cls);
+		/** declare a pseudo class, that is a class with additionnal json initialization */
+		Class const* DeclareSpecialClass(char const* class_name, nlohmann::json const& json);
+		/** gets the depth of the class in the inheritance hierarchy */
+		size_t GetDepth(Class const * p) const;
+
+		/** internal method called from ClassLoader to abord a failed loaded class */
+		 void DoInvalidateSpecialClass(Class const* cls);
 	};
 
 #endif

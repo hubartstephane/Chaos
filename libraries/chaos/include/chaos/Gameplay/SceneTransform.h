76cd8383dac813f2b3648e2efd2d17affcd970e5
Stephane Hubart <hubart.stephane@gmail.com>
2021-11-16 23:52:11 +0100

    update SceneTransform

--- libraries/chaos/include/chaos/Gameplay/SceneTransform.h
+++ libraries/chaos/include/chaos/Gameplay/SceneTransform.h
@@ -7,6 +7,54 @@ namespace chaos
 
 #elif !defined CHAOS_TEMPLATE_IMPLEMENTATION
 
+	// Translation.Rotation.Scaling
+	// ----------------------------
+	//
+	//   Translation.Rotation =
+	//
+	//  |  1  0  0 |   | a  b  0 |   | a  b  0 |       X = a.tx + c.ty
+	//  |  0  1  1 | x | c  d  0 | = | c  d  0 |  with 
+	//  | tx ty  1 |   | 0  0  1 |   | X  Y  1 |       Y = b.tx + d.ty
+	//
+	//   Translation.Rotation.Scaling = 
+	// 
+	//  | a  b  0 |   | sx  0  0 |   | sx.a  sy.b  0 |
+	//  | c  d  0 | x |  0 sy  0 | = | sx.c  sy.d  0 |
+	//  | X  Y  1 |   |  0  0  1 |   | sx.X  sy.Y  1 |
+	//
+	// 
+	// Scaling.Rotation.Translation
+	// ----------------------------
+	//
+	//  Scaling.Rotation = 
+	// 
+	//  | sx  0  0 |   | a  b  0 |   | sx.a  sx.b  0 |
+	//  |  0 sy  0 | x | c  d  0 | = | sy.c  sy.d  0 |
+	//  |  0  0  1 |   | 0  0  1 |   |    0     0  1 |
+	//
+	// Scaling.Rotation.Translation =
+	//
+	//  | sx.a  sx.b  0 |   |  1   0  0 |   |  sx.a  sx.b  0 |
+	//  | sy.c  sy.d  0 | x |  0   1  0 | = |  sy.c  sy.d  0 |
+	//  |    0     0  1 |   | tx  ty  1 |   |    tx    ty  1 |
+	//
+	//
+	// The SceneTransform is a T.R.S
+	// by definition, the Inverse(SceneTransform) is a S.R.T with
+	//
+	// Translation(V).Rotation(A).Scaling(S)    x    Scaling(1/S).Rotation(-A).Translation(-V) = identity
+	// 
+	// To convert T.R.S into S.R.T we should be able to transform
+	// 
+	//  | sx.a  sy.b  0 |      |  sx'.a'  sx'.b'  0 |
+	//  | sx.c  sy.d  0 | into |  sy'.c'  sy'.d'  0 |
+	//  | sx.X  sy.Y  1 |      |     tx'      ty  1 |
+	// 
+	//  This is not possible due to non-uniform scale (see how scale values are spread in column for first matrix, but in row for second one)
+	// 
+
+	// glCopyImageSubData 
+
 	template<typename T, int dimension>
 	class SceneTransform
 	{
@@ -19,40 +67,40 @@ namespace chaos
 		/** gets the local to parent transform */
 		mat_type GetLocalToParent() const
 		{
-			mat_type t, r, s;
+			mat_type s, r, t;
+
+			if constexpr (dimension == 2)
+				s = glm::scale(glm::tvec3<T>(scale, 1));
+			else if constexpr (dimension == 2)
+				s = glm::scale(position);
+
+			r = GetRotatorMatrix(rotator);
 
 			if constexpr (dimension == 2)
 				t = glm::translate(glm::tvec3<T>(position, 0));
 			else if constexpr (dimension == 2)
 				t = glm::translate(position);
 
-			r = GetRotatorMatrix(rotator);
-
-			if constexpr (dimension == 2)
-				t = glm::scale(glm::tvec3<T>(scale, 1));
-			else if constexpr (dimension == 2)
-				t = glm::scale(position);
-
-			return t * r * s;
+			return s * r * t;
 		}
 		/** gets the parent to local */
 		mat_type GetParentToLocal() const
 		{
-			mat_type s, r, t;
-
-			if constexpr (dimension == 2)
-				t = glm::scale(glm::tvec3<T>(T(1) / scale, 1));
-			else if constexpr (dimension == 2)
-				t = glm::scale(T(1) / scale);
-
-			r = GetRotatorMatrix(-rotator);
+			mat_type t, r, s;
 
 			if constexpr (dimension == 2)
 				t = glm::translate(glm::tvec3<T>(-position, 0));
 			else if constexpr (dimension == 2)
 				t = glm::translate(-position);
 
-			return s * r * t;
+			r = GetRotatorMatrix(-rotator);
+
+			if constexpr (dimension == 2)
+				s = glm::scale(glm::tvec3<T>(T(1) / scale, 1));
+			else if constexpr (dimension == 2)
+				s = glm::scale(T(1) / scale);
+
+			return t * r * s;
 		}
 
 	public:

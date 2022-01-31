0f04626d5c3078149c6ee40c708733775610c107
Stephane Hubart <hubart.stephane@gmail.com>
2022-01-31 23:25:19 +0100

    ViewpoirtGridLayout in progress

--- libraries/chaos/src/Windowing/ViewportLayout.cpp
+++ libraries/chaos/src/Windowing/ViewportLayout.cpp
@@ -30,51 +30,60 @@ namespace chaos
 
 	ViewportPlacement ViewportGridLayout::ComputeViewportPlacement(Viewport* viewport, glm::ivec2 const& window_size, size_t viewport_index, size_t viewport_count) const
 	{
-		int major_axis = (orientation == Orientation::HORIZONTAL) ? 0 : 1;
-		int minor_axis = 1 - major_axis;
-
 		ViewportPlacement result;
 
+		glm::vec2 ws = auto_cast_vector(window_size);
+		if (orientation == Orientation::VERTICAL)
+			std::swap(ws.x, ws.y);
+
+		// compute the result as if the orientation was horizontal
 		if (max_viewport_count <= 0 || viewport_count <= max_viewport_count) // a single line/row
 		{
-			result.size[minor_axis] = window_size[minor_axis];
-			result.size[major_axis] = window_size[major_axis] / int(viewport_count);
+			float p = float(viewport_index);
+			float next_p = float(viewport_index + 1);
 
-			result.position[minor_axis] = 0;
-			result.position[major_axis] = int(viewport_index) * result.size[major_axis];
+			float viewport_size = ws.x / float(viewport_count);
 
-			if (viewport_index == viewport_count - 1)
-			{
+			int position = int(p * viewport_size);
+			int next_position = int(next_p * viewport_size);
 
-			}
+			result.size.y = int(ws.y);
+			result.size.x = next_position - position;
 
-
+			result.position.y = 0;
+			result.position.x = position;
 		}
 		else
 		{
-			assert(0);
-#if 0
-;			int line_count = max_viewport_count;
-			int row_count  = viewport_count / max_viewport_count;
+			size_t line_count = (viewport_count + max_viewport_count - 1) / max_viewport_count;
+			size_t x = viewport_index % max_viewport_count;
+			size_t y = viewport_index / max_viewport_count;
 
-			int X = viewport_index % line_count;
-			int Y = viewport_index / line_count;
+			size_t viewport_on_line = (y != line_count - 1)? // the number of viewports for this line
+				max_viewport_count :
+				viewport_count - y * max_viewport_count;
+
+			glm::vec2 p      = {float(x), float(y)};
+			glm::vec2 next_p = {float(x + 1), float(y + 1) };
+			glm::vec2 grid   = {float(max_viewport_count), float(line_count)};
+
+			glm::vec2 viewport_size = ws / grid;
+
+			glm::ivec2 position = auto_cast_vector(p * viewport_size);
+			glm::ivec2 next_position = auto_cast_vector(next_p * viewport_size);
+
+			result.position = position;
+			result.size = next_position - position;
 
 
-
-
-			result.position = window_size[]
-			result.size[minor_axis] = window_size[minor_axis];
-
-			float f = window_size[major_axis] / (float)viewport_count;
-
-			window_size[major_axis] / viewport
-#endif
 		}
 
-
-
-
+		// correct orientation
+		if (orientation == Orientation::VERTICAL)
+		{
+			std::swap(result.position.x, result.position.y);
+			std::swap(result.size.x, result.size.y);
+		}
 		return result;
 	}
 

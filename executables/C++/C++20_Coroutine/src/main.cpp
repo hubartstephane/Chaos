951a515cd248974d02872f82868935434bfcd093
Stephane Hubart <hubart.stephane@gmail.com>
2022-03-17 00:08:23 +0100

    New Ranges exe

--- executables/C++/C++20_Coroutine/src/main.cpp
+++ executables/C++/C++20_Coroutine/src/main.cpp
@@ -3,7 +3,7 @@
 #include <coroutine>
 
 
-
+#if 0
 
 // =====================================
 template<typename T, typename G, typename ...BASES>
@@ -52,8 +52,12 @@ public:
 
 	UU(int i) :value(i) {}
 
+	UU() = default;
+
 	int value = 0;
 };
+
+class AA {};
 
 template<typename T>
 struct awaitable_promise_type_base
@@ -85,6 +89,11 @@ struct awaitable_promise_type_base
 
 
 	[[nodiscard]] awaiter await_transform(UU uu)
+	{
+		return awaiter{ mRecentSignal };
+	}
+
+	[[nodiscard]] awaiter await_transform(AA, int)
 	{
 		return awaiter{ mRecentSignal };
 	}
@@ -313,14 +322,35 @@ FSM Parse()
 {
 	byte b = co_await 3;
 	co_yield "toto";
-	b = co_await byte{};
+	b = co_await UU{};
 	co_yield "toto2";
-	b = co_await byte{};
+	b = co_await (AA{}, 1);
 	co_yield "toto3";
 
 
 
 };
+
+
+
+FSM Parse2(DataStreamReader& stream)
+{
+	while (true)
+	{
+		byte 
+
+	}
+}
+
+
+
+
+
+
+
+
+
+
 
 #if 1
 void ProcessString(generator<byte>& stream, FSM& parse)
@@ -380,6 +410,182 @@ int CHAOS_MAIN(int argc, char** argv, char** env)
 	{
 		//i = i;
 	}
+
+
+	return 0;
+}
+
+#endif
+
+// ====================================================================
+
+template<typename T, typename G, bool InitialSuspend>
+struct promise_type_base
+{
+	T mValue;
+
+	std::suspend_always yield_value(T value)
+	{
+		mValue = value;
+		return {};
+	}
+
+	auto initial_suspend()
+	{
+		if constexpr (InitialSuspend)
+			return std::suspend_always{};
+		else
+			return std::suspend_never{};
+	}
+
+	std::suspend_always final_suspend() noexcept { return {}; }
+	G get_return_object() { return G(this); }
+	void unhandled_exception();
+	void return_void();
+
+};
+
+
+class DataStreamReader
+{
+
+public:
+
+	DataStreamReader() = default;
+
+	DataStreamReader& operator=(DataStreamReader&&) noexcept = delete;
+
+	// ---
+	struct Awaiter
+	{
+		Awaiter& operator = (Awaiter&&) noexcept = delete;
+
+		Awaiter(DataStreamReader& event) noexcept:
+			mEvent(event)
+		{
+			mEvent.mAwaiter = this;
+		}
+
+		void await_suspend(std::coroutine_handle<> coroHdl)
+		{
+			mCoroHdl = coroHdl;
+		}
+
+		byte await_resume() noexcept
+		{
+			assert(mEvent.mData.has_value());
+			return *std::exchange(mEvent.mData, std::nullopt);
+		}
+
+		void resume()
+		{
+			mCoroHdl.resume();
+		}
+
+	private:
+
+		DataStreamReader& mEvent;
+		std::coroutine_handle<> mCoroHdl{};
+
+	};
+	// ---
+
+	auto operator co_await() noexcept
+	{
+		return Awaiter(*this);
+	}
+
+	void set(byte b)
+	{
+		mData.emplace(b);
+		if (mAwaiter)
+			mAwaiter->resume();
+	}
+
+private:
+
+	friend struct Awaiter;
+
+	Awaiter* mAwaiter{};
+
+	std::optional<byte> mData;
+};
+
+
+
+
+template<typename T, bool InitialSuspend = true>
+struct generator
+{
+	using promise_type = promise_type_base<
+		T,
+		generator,
+		InitialSuspend>;
+
+	using PromiseTypeHandle = std::coroutine_handle<promise_type>;
+	
+
+	generator(generator const&) = delete;
+
+	generator(generator&& rhs) :
+		mCoroHdl(std::exchange(rhs.mCoroHdl, nullptr)) {}
+
+	~generator()
+	{
+		if (mCoroHdl)
+			mCoroHdl.destroy();
+	}
+
+	T operator()
+	{
+		T tmp{};
+		std::swap(tmp, mCoroHdl.promise().mValue);
+		return tmp;
+	}
+
+private:
+
+	friend promise_type;
+	
+	explicit generator(promise_type* p):mCoroHdl(PromiseTypeHandle::from_promise(*p))
+	{
+
+	}
+
+protected:
+
+	PromiseTypeHandle mCoroHdl;
+
+
+};
+
+using FSM = generator<std::string, false>;
+
+
+DataStreamReader GetReader(std::vector<byte>& v)
+{
+	for (auto b : v)
+		co_yield b;
+}
+
+FSM Parse(DataStreamReader& reader)
+{
+	while (true)
+	{
+		byte b = co_await reader;
+
+		co_yield "toto";
+
+	}
+
+
+}
+
+
+
+
+int CHAOS_MAIN(int argc, char** argv, char** env)
+{
 
 
 	return 0;
---
+++ executables/C++/C++20_Ranges/src/main.cpp
@@ -0,0 +1,9 @@
+#include <chaos/Chaos.h>
+
+
+
+int CHAOS_MAIN(int argc, char** argv, char** env)
+{
+
+	return 0;
+}

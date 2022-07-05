a608c7c20f0f30a51fab92f7541575b553bc77cf
Stephane Hubart <hubart.stephane@gmail.com>
2022-07-06 00:16:23 +0200

    Premake in progressé

--- premake_scripts/BuildSystem.lua
+++ premake_scripts/BuildSystem.lua
@@ -123,7 +123,7 @@ function BuildSystem:DeclareExternalLib(external_name, inc_path, lib_path, libna
 		function(plat, conf)
 			Utility:ForEachElement(result.libname[plat][conf],
 				function(libname)
-					Utility:ForEachElement(result.targetdir[plat][conf],
+					Utility:ForEachElement(result.targetdir[plat][conf], -- there should be a single entry
 						function(dir)
 							local fullpath = dir .. "/" .. libname
 							if not os.isfile(fullpath) then
@@ -139,79 +139,6 @@ function BuildSystem:DeclareExternalLib(external_name, inc_path, lib_path, libna
 
 end
 
---------------------------------------------------------------------
--- function to initialize project for DEBUG
---------------------------------------------------------------------
-
--- note on staticruntime -> dll or static
---         runtime       -> debug or rekease
--- /MT static multithread
--- /MTd static debug multithread
--- /MD  DLL
--- /MDd DLL debug
---
--- LinkTimeOptimization -> see /LTCG
--- staticruntime "on"
--- runtime "debug"
--- flags {"LinkTimeOptimization"}
-
-function BuildSystem:DebugConf(plat)
-	filter {"configurations:" .. DEBUG, "platforms:" .. plat}
-	defines {DEBUG}
-	defines {"_DEBUG"}
-	symbols "On"
-	flags {"MultiProcessorCompile"}
-end
-
-
---------------------------------------------------------------------
--- function to initialize project for RELEASE
---------------------------------------------------------------------
-
--- staticruntime "on"
-
-function BuildSystem:ReleaseConf(plat)
-	filter {"configurations:" .. RELEASE, "platforms:" .. plat}
-	runtime "release"
-	defines {"NDEBUG"}
-	defines {RELEASE}
-	defines {"_RELEASE"}
-	optimize "On"
-	symbols "Off"
-	flags {"MultiProcessorCompile"}
-end
-
---------------------------------------------------------------------
--- generic initializations
---------------------------------------------------------------------
-
-function BuildSystem:OnConfig(in_kind, proj_type, plat, conf, proj)
-
-	-- where the result EXE/LIB is been saved
-	local targ = path.join(self.project_build_path, plat, conf)
-	targetdir(targ)
-	proj.targetdir[plat][conf] = targ
-
-	-- by default, the editor start the exe in the source path. We prefere to start where it has been build
-	debugdir("$(TargetDir)")
-
-	-- where the includes are for the project
-	local inc = path.join(self.project_src_path, "include")
-	includedirs(inc)
-	proj.includedirs[plat][conf] = inc
-
-	-- some definition for FILE REDIRECTION
-	if (proj_type == ProjectType.EXECUTABLE) then
-		defines('CHAOS_PROJECT_PATH="' .. Utility:Base64Encode(self.project_path) .. '"')
-		defines('CHAOS_PROJECT_SRC_PATH="' .. Utility:Base64Encode(self.project_src_path) .. '"')
-		defines('CHAOS_PROJECT_BUILD_PATH="' .. Utility:Base64Encode(targ) .. '"')
-		prebuildcommands('{ECHO} CHAOS_PROJECT_PATH       = "' .. self.project_path .. '"')
-		prebuildcommands('{ECHO} CHAOS_PROJECT_SRC_PATH   = "' .. self.project_src_path .. '"')
-		prebuildcommands('{ECHO} CHAOS_PROJECT_BUILD_PATH = "' .. targ .. '"')
-	end
-
-	characterset("ASCII")
-end
 
 
 
@@ -223,149 +150,28 @@ end
 -- function to create any kind of project
 --------------------------------------------------------------------
 
-function BuildSystem:CppProject(in_kind, proj_type)
-
-
+function BuildSystem:CppProject(proj_type)
 
 	local result = self:AddProject(self.project_name, {
-	
-	
-	
-	
-
-
+		current_group = self.current_group,
+		project_path = self.project_path,
+		project_src_path = self.current_group,
+		project_build_path = self.project_build_path
 	})
-
-
-
-	-- the name of the group
 	
-	local group_name = nil
-	if (self.current_group ~= nil) then
-		group_name = path.join(self.current_group, self.project_name)
-		group(group_name)
-	end
-
-	-- create a project for the resources
-	local proj_location = path.join(SOLUTION_PATH, self.project_path)
-	local res_path = path.join(self.project_src_path, "resources")
-
-	local resource_proj_name = result:GetResourceProjectName()
-	project(resource_proj_name)
-	kind(SPECIAL_PROJECT)
-	location(proj_location)
-	files {path.join(res_path, "**")}
-
-	local resource_project = project()
-
-	-- create the project itself
-	local proj_location = path.join(SOLUTION_PATH, self.project_path)
-	local inc_path = path.join(self.project_src_path, "include")
-	local src_path = path.join(self.project_src_path, "src")
-	local res_path = path.join(self.project_src_path, "resources")
-
-	project(PROJ_NAME)
-	kind(in_kind)
-	location(proj_location)
-	
-	local lua_project = project()
-
-	local result = {
-		proj_type = proj_type,
-		path = self.project_path,
-		root_path = self.project_src_path,
-		build_path = self.project_build_path,
-		lua_project = lua_project,
-		resource_project = resource_project,
-		targetdir = GetPlatConfArray({}),
-		includedirs = GetPlatConfArray({}),
-		group_name = group_name,
-		proj_location = proj_location,
-		inc_path = GetPlatConfArray(inc_path),
-		src_path = GetPlatConfArray(src_path),
-		res_path = GetPlatConfArray(res_path)
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	language "C++"
-
-	if (_ACTION == "codelite") then
-		cppdialect "C++2a"
-	else
-		cppdialect "C++20"
-	end
-	
-		--staticruntime "on"
-
-	-- change entry point for windows (avoid WinMain to main)
-	if (os.target() == "windows") then
-		if (proj_type == ProjectType.EXECUTABLE) then
-			entrypoint "mainCRTStartup"
-		end
-	end
-
-	-- some files including *.cpp, *.c, *.hpp, *.h, *.ixx
-	local src_h = path.join(self.project_src_path, "**.h")
-	local src_hpp = path.join(self.project_src_path, "**.hpp")
-	local src_c = path.join(self.project_src_path, "**.c")
-	local src_cpp = path.join(self.project_src_path, "**.cpp")
-	local src_ixx = path.join(self.project_src_path, "**.ixx")
-	files {src_h, src_hpp, src_c, src_cpp, src_ixx}
-
-	-- handle C++ modules
-	filter {"files:**.ixx" }
-		buildaction "ClCompile"
-		compileas "Module"
-	filter { }
-
-	-- release/debug settings
-	AllTargets(
-		function(plat, conf)
-			if (conf == DEBUG) then
-				self:DebugConf(plat)
-			else
-				self:ReleaseConf(plat)
-			end
-			self:OnConfig(in_kind, proj_type, plat, conf, result)
-		end
-	)
-
-	-- special filter for copying dll
-	filter 'files:**.dll'
-	  buildmessage 'DLL HANDLING %{file.basename}.dll'
-		build_command_str = Utility:QuotationMarks(COPY_SCRIPT, '%{file.abspath}', '%{cfg.targetdir}/%{file.basename}.dll')
-		buildcommands(build_command_str)
-		buildoutputs '%{cfg.targetdir}/%{file.basename}.dll'
-
-
-
 	return result
+
 end
+
 
 --------------------------------------------------------------------
 -- function to create any kind of library
 --------------------------------------------------------------------
-function BuildSystem:LibraryHelper(kind_library, type)
-	local result = selft:CppProject(kind_library, type)
+function BuildSystem:LibraryHelper(proj_type)
+	local result = selft:CppProject(proj_type)
 	result.libname = GetPlatConfArray(result.name)
 	result:AddFileToCopy("resources")
-	if (type == ProjectType.SHARED_LIBRARY) then
+	if (proj_type == ProjectType.SHARED_LIBRARY) then
 		result:AddFileToCopy("@" .. path.join(result.targetdir[x64][DEBUG], result.name .. ".dll"))
 	end
 	result:GenDoxygen()
@@ -376,25 +182,104 @@ end
 -- declare a static library
 --------------------------------------------------------------------
 function BuildSystem:StaticLibrary()
-	return self:LibraryHelper("StaticLib", ProjectType.STATIC_LIBRARY)
+	return self:LibraryHelper(ProjectType.STATIC_LIBRARY)
 end
 
 --------------------------------------------------------------------
 -- declare a shared library
 --------------------------------------------------------------------
 function BuildSystem:SharedLibrary()
-	local result = self:LibraryHelper("SharedLib", ProjectType.SHARED_LIBRARY)
-	filter {}
-	defines('CHAOS_IS_BUILDING_DLL')
-	allmodulespublic "on" -- required for DLL+modules (requires at least premake 5.0.0-beta2)
-	return result
+	return self:LibraryHelper(ProjectType.SHARED_LIBRARY)
 end
 
 --------------------------------------------------------------------
 -- Create a windowed application
 --------------------------------------------------------------------
 function BuildSystem:WindowedApp()
-	local result = self:CppProject("WindowedApp", ProjectType.EXECUTABLE)
-	result:GenZIP()
-	return result
+	return self:CppProject(ProjectType.EXECUTABLE)
+end
+
+--------------------------------------------------------------------
+-- Collect all dependencies
+--------------------------------------------------------------------
+function BuildSystem:CollectDependencies()
+	local changes = true
+	while changes do
+		changes = false
+		for k, proj in ipairs(self.projects) do
+		
+			for k, v in ipairs(proj.dependencies) do
+		
+			end				
+		
+		end		
+	end
+
+
+end
+
+--[[
+function CollectDependencies(proj, collect_dependencies)
+	if (IsTable(proj.dependencies)) then
+		for k, v in pairs(proj.dependencies) do
+			local other_proj = FindProject(v)
+			if (not IsNil(other_proj)) then
+				if (not SearchElementInTable(collect_dependencies, other_proj)) then
+					table.insert(collect_dependencies, other_proj)
+					CollectDependencies(other_proj, collect_dependencies)
+				end
+			else
+				assert(false, "Dependency not found :" .. v .. " (for project " .. proj.name ..")")
+			end
+		end
+	end
+	return collect_dependencies
+end
+
+for k, proj in pairs(MYPROJECTS) do
+
+	proj.full_dependencies = CollectDependencies(proj, {}) -- replace the array of string with an array of projects
+
+	if (DISPLAY_DEPENDENCIES) then
+		Output("=======================================================================")
+		Output("[" .. proj.name .. "] Dependencies")
+		Output("=======================================================================")
+		for k, d in pairs(proj.full_dependencies) do
+			Output("  " .. d.name)
+		end
+	end
+end
+]]--
+
+--------------------------------------------------------------------
+-- Make the solution
+--------------------------------------------------------------------
+
+function BuildSystem:MakeSolution()
+
+	solution "Chaos"
+
+	platforms {table.unpack(PLATFORMS)}
+	configurations {table.unpack(CONFIGS)}
+	
+	local arch = _OPTIONS['arch']
+	if arch then
+		Log:Output("Architecture: " .. arch)
+		architecture(arch)
+	end
+
+	location(SOLUTION_PATH) -- where the visual studio project file is been created
+
+	if os.target() == "windows" then
+		defines {"WINDOWS"}
+	end
+
+	if os.target() == "linux" then
+		defines {"LINUX"}
+	end
+	
+	for k, proj in ipairs(self.projects) do
+		proj.AddProjectToSolution()
+	end
+
 end
--- premake_scripts/Project.lua
+++ premake_scripts/Project.lua
@@ -125,3 +125,254 @@ function Project:GetResourceProjectName()
 	return "Resources_of_" .. self.proj_name
 end
 
+--------------------------------------------------------------------
+-- function to initialize project for DEBUG
+--------------------------------------------------------------------
+
+-- note on staticruntime -> dll or static
+--         runtime       -> debug or rekease
+-- /MT static multithread
+-- /MTd static debug multithread
+-- /MD  DLL
+-- /MDd DLL debug
+--
+-- LinkTimeOptimization -> see /LTCG
+-- staticruntime "on"
+-- runtime "debug"
+-- flags {"LinkTimeOptimization"}
+
+function Project:DebugConf(plat)
+	filter {"configurations:" .. DEBUG, "platforms:" .. plat}
+	defines {DEBUG}
+	defines {"_DEBUG"}
+	symbols "On"
+	flags {"MultiProcessorCompile"}
+end
+
+
+--------------------------------------------------------------------
+-- function to initialize project for RELEASE
+--------------------------------------------------------------------
+
+-- staticruntime "on"
+
+function Project:ReleaseConf(plat)
+	filter {"configurations:" .. RELEASE, "platforms:" .. plat}
+	runtime "release"
+	defines {"NDEBUG"}
+	defines {RELEASE}
+	defines {"_RELEASE"}
+	optimize "On"
+	symbols "Off"
+	flags {"MultiProcessorCompile"}
+end
+
+--------------------------------------------------------------------
+-- generic initializations
+--------------------------------------------------------------------
+
+function Project:OnConfig(in_kind, proj_type, plat, conf, proj)
+
+	-- where the result EXE/LIB is been saved
+	local targ = path.join(self.project_build_path, plat, conf)
+	targetdir(targ)
+	proj.targetdir[plat][conf] = targ
+
+	-- by default, the editor start the exe in the source path. We prefere to start where it has been build
+	debugdir("$(TargetDir)")
+
+	-- where the includes are for the project
+	local inc = path.join(self.project_src_path, "include")
+	includedirs(inc)
+	proj.includedirs[plat][conf] = inc
+
+	-- some definition for FILE REDIRECTION
+	if (proj_type == ProjectType.EXECUTABLE) then
+		defines('CHAOS_PROJECT_PATH="' .. Utility:Base64Encode(self.project_path) .. '"')
+		defines('CHAOS_PROJECT_SRC_PATH="' .. Utility:Base64Encode(self.project_src_path) .. '"')
+		defines('CHAOS_PROJECT_BUILD_PATH="' .. Utility:Base64Encode(targ) .. '"')
+		prebuildcommands('{ECHO} CHAOS_PROJECT_PATH       = "' .. self.project_path .. '"')
+		prebuildcommands('{ECHO} CHAOS_PROJECT_SRC_PATH   = "' .. self.project_src_path .. '"')
+		prebuildcommands('{ECHO} CHAOS_PROJECT_BUILD_PATH = "' .. targ .. '"')
+	end
+
+	characterset("ASCII")
+end
+
+
+function Project:AddProjectToSolution()
+
+	-- ignore external project
+	if (self.proj_type == ProjectType.EXTERNAL_LIBRARY) then
+		return
+	end
+	
+	-- declare project
+	project(self.proj_name)
+		
+	-- type
+	if (self.proj_type == ProjectType.EXECUTABLE) then
+		kind("WindowedApp")
+	elseif (self.proj_type == ProjectType.STATIC_LIBRARY) then
+		kind("StaticLib")
+	elseif (self.proj_type == ProjectType.SHARED_LIBRARY) then
+		kind("SharedLib")
+		defines('CHAOS_IS_BUILDING_DLL')
+		allmodulespublic "on" -- required for DLL+modules (requires at least premake 5.0.0-beta2)		
+	elseif (self.proj_type == ProjectType.RESOURCES) then	
+		kind("Makefile")
+	end
+	
+	-- language/dialect
+	language "C++"
+
+	if (_ACTION == "codelite") then
+		cppdialect "C++2a"
+	else
+		cppdialect "C++20"
+	end
+	
+		--staticruntime "on"
+
+	-- entry point (avoid WinMain to main)
+	if (os.target() == "windows") then
+		if (proj_type == ProjectType.EXECUTABLE) then
+			entrypoint "mainCRTStartup"
+		end
+	end
+
+	-- files to handle: *.cpp, *.c, *.hpp, *.h, *.ixx
+	local src_h = path.join(self.project_src_path, "**.h")
+	local src_hpp = path.join(self.project_src_path, "**.hpp")
+	local src_c = path.join(self.project_src_path, "**.c")
+	local src_cpp = path.join(self.project_src_path, "**.cpp")
+	local src_ixx = path.join(self.project_src_path, "**.ixx")
+	files {src_h, src_hpp, src_c, src_cpp, src_ixx}
+
+	-- handle C++ modules
+	filter {"files:**.ixx" }
+		buildaction "ClCompile"
+		compileas "Module"
+	filter { }
+
+	-- release/debug settings
+	AllTargets(
+		function(plat, conf)
+			if (conf == DEBUG) then
+				self:DebugConf(plat)
+			else
+				self:ReleaseConf(plat)
+			end
+			self:OnConfig(plat, conf)
+		end
+	)
+
+	-- special filter for copying dll
+	filter 'files:**.dll'
+	  buildmessage 'DLL HANDLING %{file.basename}.dll'
+		build_command_str = Utility:QuotationMarks(COPY_SCRIPT, '%{file.abspath}', '%{cfg.targetdir}/%{file.basename}.dll')
+		buildcommands(build_command_str)
+		buildoutputs '%{cfg.targetdir}/%{file.basename}.dll'
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+--[[
+
+
+
+
+
+	-- the name of the group
+	
+	local group_name = nil
+	if (self.current_group ~= nil) then
+		group_name = path.join(self.current_group, self.project_name)
+		group(group_name)
+	end
+
+	-- create a project for the resources
+	local proj_location = path.join(SOLUTION_PATH, self.project_path)
+	local res_path = path.join(self.project_src_path, "resources")
+
+	local resource_proj_name = result:GetResourceProjectName()
+	project(resource_proj_name)
+	kind(SPECIAL_PROJECT)
+	location(proj_location)
+	files {path.join(res_path, "**")}
+
+	local resource_project = project()
+
+	-- create the project itself
+	local proj_location = path.join(SOLUTION_PATH, self.project_path)
+	local inc_path = path.join(self.project_src_path, "include")
+	local src_path = path.join(self.project_src_path, "src")
+	local res_path = path.join(self.project_src_path, "resources")
+
+	project(PROJ_NAME)
+	kind(in_kind)
+	location(proj_location)
+	
+	local lua_project = project()
+
+	---------------------------------
+	-- IMPORTANT
+	---------------------------------	
+	-- path = self.project_path,
+	-- root_path = self.project_src_path,
+	-- build_path = self.project_build_path,
+	---------------------------------	
+
+
+
+	local result = {
+		proj_type = proj_type,
+		path = self.project_path,
+		root_path = self.project_src_path,
+		build_path = self.project_build_path,
+		
+		
+
+		targetdir = GetPlatConfArray({}),
+		includedirs = GetPlatConfArray({}),
+		group_name = group_name,
+		proj_location = proj_location,
+		inc_path = GetPlatConfArray(inc_path),
+		src_path = GetPlatConfArray(src_path),
+		res_path = GetPlatConfArray(res_path)
+	}
+
+
+		lua_project = lua_project,
+		resource_project = resource_project,
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return result
+end
+
+
+]]--

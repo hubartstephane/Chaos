423f9ab3356da8754063dec1be88126e9b9c6a0b
Stéphane Hubart <s.hubart@dont-nod.lan>
2022-08-20 01:00:27 +0200

    add TESTDLL to solution

--- libraries/subpremake5.lua
+++ libraries/subpremake5.lua
@@ -3,5 +3,4 @@
 -- =============================================================================
 
 build:ProcessSubPremake("chaos")
---build:ProcessSubPremake("chaos2")
---build:ProcessSubPremake("testdll")
+build:ProcessSubPremake("testdll")

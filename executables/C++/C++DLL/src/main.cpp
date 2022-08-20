d4345ac8cde86787a143887df440b7875ee8e5d2
Stéphane Hubart <s.hubart@dont-nod.lan>
2022-08-20 10:46:26 +0200

    create a dedicated project to test for DLL import

--- executables/C++/C++DLL/src/main.cpp
+++ executables/C++/C++DLL/src/main.cpp
@@ -1,7 +1,16 @@
+//#define _SILENCE_CLANG_CONCEPTS_MESSAGE
 #include <iostream>
+
+#include "MyOtherClass.h"
+
+import MyClass;
 
 int main(int argc, char ** argv, char ** env)
 {
+
+	MyClass().f();
+	MyOtherClass().f();
+
 	std::cout << "Hello World Again" << std::endl;
 
 	return 0;
--- executables/C++/subpremake5.lua
+++ executables/C++/subpremake5.lua
@@ -2,6 +2,7 @@
 -- ROOT_PATH/executables/C++
 -- =============================================================================
 
+build:ProcessSubPremake("C++DLL")
 build:ProcessSubPremake("C++11")
 build:ProcessSubPremake("C++17")
 build:ProcessSubPremake("C++20_Concepts")

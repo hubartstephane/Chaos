e55c6d45a2b1e4c6c71e77b574d0869bb2e3561d
Stéphane Hubart <s.hubart@dont-nod.lan>
2022-08-29 00:55:57 +0200

    Config file automatic generation

--- libraries/chaos2/include/chaos2/Chaos2PCH.h
+++ libraries/chaos2/include/chaos2/Chaos2PCH.h
@@ -56,63 +56,7 @@
 #include <forward_list>
 #include <type_traits>
 
-#define BOOST_ALL_NO_LIB
-#define BOOST_LIB_DIAGNOSTIC
-#include <boost/static_assert.hpp>
-#include <boost/thread.hpp>
-#include <boost/atomic.hpp>
-#include <boost/filesystem.hpp>
-#include <boost/filesystem/path.hpp>
-#include <boost/timer/timer.hpp>
-#include <boost/program_options/option.hpp>
-#include <boost/program_options/options_description.hpp>
-#include <boost/program_options/variables_map.hpp>
-#include <boost/program_options/parsers.hpp>
-#include <boost/call_traits.hpp>
-#include <boost/type_traits.hpp>
-#include <boost/algorithm/string.hpp>
-#include <boost/range/irange.hpp>
-#include <boost/shared_ptr.hpp>
-#include <boost/weak_ptr.hpp>
-#include <boost/intrusive_ptr.hpp>
-#include <boost/log/utility/functional/nop.hpp>
-#include <boost/tti/has_member_data.hpp>
-#include <boost/tti/has_member_function.hpp>
-#include <boost/convert/detail/has_member.hpp>
-#include <boost/type_traits/add_const.hpp>
 
-#include <boost/mpl/if.hpp>
-#include <boost/mpl/eval_if.hpp>
-#include <boost/mpl/identity.hpp>
-#include <boost/mpl/vector.hpp>
-#include <boost/mpl/find.hpp>
-#include <boost/mpl/find_if.hpp>
-#include <boost/mpl/placeholders.hpp>
-#include <boost/mpl/apply.hpp>
-#include <boost/mpl/begin.hpp>
-#include <boost/mpl/end.hpp>
-#include <boost/mpl/not_equal_to.hpp>
-#include <boost/mpl/comparison.hpp>
-#include <boost/mpl/inherit_linearly.hpp>
-#include <boost/mpl/inherit.hpp>
-#include <boost/mpl/empty_base.hpp>
-#include <boost/mpl/apply.hpp>
-#include <boost/mpl/fold.hpp>
-#include <boost/mpl/lambda.hpp>
-#include <boost/mpl/next.hpp>
-#include <boost/mpl/deref.hpp>
-#include <boost/mpl/for_each.hpp>
-#include <boost/mpl/apply_wrap.hpp>
-#include <boost/mpl/has_xxx.hpp>
-#include <boost/mpl/aux_/has_type.hpp>
-
-#include <boost/preprocessor/repetition/enum_params.hpp>
-#include <boost/preprocessor/repetition/enum_binary_params.hpp>
-#include <boost/preprocessor/repetition/repeat.hpp>
-#include <boost/preprocessor/punctuation/comma_if.hpp>
-#include <boost/preprocessor/cat.hpp>
-#include <boost/preprocessor/seq/for_each.hpp>
-#include <boost/preprocessor/facilities/empty.hpp>
 
 #define GLM_ENABLE_EXPERIMENTAL
 #define GLM_FORCE_SIZE_T_LENGTH
@@ -170,11 +114,76 @@
 #include <assimp/scene.h>
 #include <assimp/postprocess.h>
 
+// boost is full of #pragma comment(lib, ...)
+// ignore theses link directive for STATIC_LIBRARIES that would use this header
+#if defined CHAOS_BUILDING_STATIC_LIBRARY
+#define BOOST_ALL_NO_LIB
+#endif
+#define BOOST_LIB_DIAGNOSTIC // display all #pragma comment(lib, ...) directives
+#include <boost/static_assert.hpp>
+#include <boost/thread.hpp>
+#include <boost/atomic.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/timer/timer.hpp>
+#include <boost/program_options/option.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/call_traits.hpp>
+#include <boost/type_traits.hpp>
+#include <boost/algorithm/string.hpp>
+#include <boost/range/irange.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/weak_ptr.hpp>
+#include <boost/intrusive_ptr.hpp>
+#include <boost/log/utility/functional/nop.hpp>
+#include <boost/tti/has_member_data.hpp>
+#include <boost/tti/has_member_function.hpp>
+#include <boost/convert/detail/has_member.hpp>
+#include <boost/type_traits/add_const.hpp>
+
+#include <boost/mpl/if.hpp>
+#include <boost/mpl/eval_if.hpp>
+#include <boost/mpl/identity.hpp>
+#include <boost/mpl/vector.hpp>
+#include <boost/mpl/find.hpp>
+#include <boost/mpl/find_if.hpp>
+#include <boost/mpl/placeholders.hpp>
+#include <boost/mpl/apply.hpp>
+#include <boost/mpl/begin.hpp>
+#include <boost/mpl/end.hpp>
+#include <boost/mpl/not_equal_to.hpp>
+#include <boost/mpl/comparison.hpp>
+#include <boost/mpl/inherit_linearly.hpp>
+#include <boost/mpl/inherit.hpp>
+#include <boost/mpl/empty_base.hpp>
+#include <boost/mpl/apply.hpp>
+#include <boost/mpl/fold.hpp>
+#include <boost/mpl/lambda.hpp>
+#include <boost/mpl/next.hpp>
+#include <boost/mpl/deref.hpp>
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpl/apply_wrap.hpp>
+#include <boost/mpl/has_xxx.hpp>
+#include <boost/mpl/aux_/has_type.hpp>
+
+#include <boost/preprocessor/repetition/enum_params.hpp>
+#include <boost/preprocessor/repetition/enum_binary_params.hpp>
+#include <boost/preprocessor/repetition/repeat.hpp>
+#include <boost/preprocessor/punctuation/comma_if.hpp>
+#include <boost/preprocessor/cat.hpp>
+#include <boost/preprocessor/seq/for_each.hpp>
+#include <boost/preprocessor/facilities/empty.hpp>
+
+
 #if 0
+#define FBXSDK_SHARED
 #include <fbxsdk.h>
 #include <fbxsdk/fileio/fbxiosettings.h>
 #include <fbxsdk/scene/fbxscene.h>
 #endif
+//#undef snprintf
 
 #include <ranges>
 
--- libraries/testdll/include/testdll.h
+++ libraries/testdll/include/testdll.h
@@ -4,7 +4,7 @@
 // not __declspec(dllimport)
 
 #ifndef TESTDLL_API
-	#if defined CHAOS_IS_BUILDING_DLL
+	#if defined CHAOS_BUILDING_CHAOS2
 		#define TESTDLL_API __declspec(dllexport)
 	#else
 		#define TESTDLL_API __declspec(dllimport)

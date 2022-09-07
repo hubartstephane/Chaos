949dbbdb75d09f5f75115356ca0fa8c3d90b2929
Stéphane Hubart <hubart.stephane@gmail.com>
2022-09-07 23:37:36 +0200

    clean C+11 main

--- executables/C++/C++11/src/main.cpp
+++ executables/C++/C++11/src/main.cpp
@@ -1,56 +1,16 @@
 #include <iostream>
 #include <memory>
 #include <vector>
+#include <ranges>
+#include <numeric>
 
 
 
-class A
-{
-public:
-
-	std::string GetName1() const { return name; }
-
-	std::string const & GetName2() const { return name; }
-
-	std::string name;
-};
-
-class B
-{
-public:
-	std::vector<std::unique_ptr<A>> myA;
-};
-
-bool operator == (A const&, A const&)
-{
-	return true;
-}
 
 
 int main(int argc, char ** argv, char ** env)
 {
-	std::vector<std::unique_ptr<A>> myA;
 
-	myA.emplace_back(new A);
-
-
-	A a;
-
-	bool b = (a != a);
-
-	auto s1 = a.GetName1();
-	s1 = "t";
-	auto s2 = a.GetName2();
-	s2 = "t";
-
-	decltype(auto) s3 = a.GetName1();
-	s3 = "t";
-
-	decltype(auto) s4 = a.GetName2();
-	//s4 = "t";
-
-
-	std::cout << "Hello World Again" << std::endl;
 
 	return 0;
 }

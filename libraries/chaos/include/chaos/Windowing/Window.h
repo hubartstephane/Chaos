0d79d56385f86abb648b666cb565cbad390023a8
Stephane Hubart <hubart.mail@gmail.com>
2022-11-13 11:25:44 +0100

    WindowInterface has its own file

--- libraries/chaos/include/chaos/Windowing/Window.h
+++ libraries/chaos/include/chaos/Windowing/Window.h
@@ -32,52 +32,6 @@ namespace chaos
 
 	CHAOS_API bool LoadFromJSON(nlohmann::json const& json, WindowParams& dst);
 
-	// ========================================================
-	// WindowDrawParams : some data for the window rendering
-	// ========================================================
-
-	class CHAOS_API WindowDrawParams
-	{
-	public:
-		/** the viewport */
-		ViewportPlacement viewport;
-		/** the rendering full size */
-		glm::ivec2 full_size = { 0, 0 };
-	};
-
-
-
-
-	class CHAOS_API WindowInterface : public TickableInterface, public InputEventReceiverInterface, public NamedInterface, public GPUProgramProviderInterface
-	{
-	protected:
-
-		/** called whenever the user try to close window */
-		virtual bool OnWindowClosed() { return true; }
-		/** called whenever the window is resized */
-		virtual void OnWindowResize(glm::ivec2 size) {}
-		/** called whenever a file is dropped */
-		virtual void OnDropFile(int count, char const** paths) {}
-		/** called whenever the window becomes iconified or is restored */
-		virtual void OnIconifiedStateChange(bool iconified) {}
-		/** called whenever the window gain or loose focus */
-		virtual void OnFocusStateChange(bool gain_focus) {}
-		/** the drawing specialization method */
-		virtual bool OnDraw(GPURenderer* renderer, WindowDrawParams const& DrawParams, GPUProgramProviderInterface const* uniform_provider) { return true; }
-	};
-
-
-
-
-
-
-
-
-
-
-
-
-
 	/**
 	* Window : a binding class between chaos and GLFW to handle window (beware the prefix "My")
 	*/
---
+++ libraries/chaos/include/chaos/Windowing/WindowInterface.h
@@ -0,0 +1,46 @@
+namespace chaos
+{
+#ifdef CHAOS_FORWARD_DECLARATION
+
+	class WindowInterface;
+
+#elif !defined CHAOS_TEMPLATE_IMPLEMENTATION
+
+	// ========================================================
+	// WindowDrawParams : some data for the window rendering
+	// ========================================================
+
+	class CHAOS_API WindowDrawParams
+	{
+	public:
+		/** the viewport */
+		ViewportPlacement viewport;
+		/** the rendering full size */
+		glm::ivec2 full_size = { 0, 0 };
+	};
+
+	// ========================================================
+	// WindowInterface : a common interface for both window and window client
+	// ========================================================
+
+	class CHAOS_API WindowInterface : public TickableInterface, public InputEventReceiverInterface, public NamedInterface, public GPUProgramProviderInterface
+	{
+	protected:
+
+		/** called whenever the user try to close window */
+		virtual bool OnWindowClosed() { return true; }
+		/** called whenever the window is resized */
+		virtual void OnWindowResize(glm::ivec2 size) {}
+		/** called whenever a file is dropped */
+		virtual void OnDropFile(int count, char const** paths) {}
+		/** called whenever the window becomes iconified or is restored */
+		virtual void OnIconifiedStateChange(bool iconified) {}
+		/** called whenever the window gain or loose focus */
+		virtual void OnFocusStateChange(bool gain_focus) {}
+		/** the drawing specialization method */
+		virtual bool OnDraw(GPURenderer* renderer, WindowDrawParams const& DrawParams, GPUProgramProviderInterface const* uniform_provider) { return true; }
+	};
+
+#endif
+
+}; // namespace chaos

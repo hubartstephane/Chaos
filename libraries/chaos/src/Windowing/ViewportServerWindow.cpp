6c0b420dc822ef5f8c498e2b30f3f0ced51135d9
Stephane Hubart <hubart.mail@gmail.com>
2022-11-27 20:56:16 +0100

    Viewport refactor in progress

--- libraries/chaos/src/Windowing/ViewportServerWindow.cpp
+++ libraries/chaos/src/Windowing/ViewportServerWindow.cpp
@@ -5,39 +5,6 @@ namespace chaos
 {
 
 	/**
-	* ViewportServerInterface implementation
-	*/
-
-	AutoCastable<Viewport> ViewportServerInterface::GetViewport()
-	{
-		return viewport.get();
-	}
-	AutoConstCastable<Viewport> ViewportServerInterface::GetViewport() const
-	{
-		return viewport.get();
-	}
-
-	void ViewportServerInterface::SetViewport(Viewport* in_viewport)
-	{
-		if (in_viewport != viewport.get())
-		{
-			// detach previous client
-			if (viewport != nullptr)
-			{
-				viewport->viewport_server = nullptr;
-				viewport->OnDetachedFromServer(this);
-			}
-			// attach new client
-			viewport = in_viewport;
-			if (viewport != nullptr)
-			{
-				viewport->viewport_server = this;
-				viewport->OnAttachedToServer(this);
-			}
-		}
-	}
-
-	/**
 	* ViewportServerWindow implementation
 	*/
 

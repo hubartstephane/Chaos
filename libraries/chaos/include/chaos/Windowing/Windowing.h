06e417431ee4c011a78479ebca1c85cad22bfaa0
Stephane Hubart <hubart.stephane@gmail.com>
2022-12-12 00:06:13 +0100

    remove outdated files (ViewportGridLayout and so)

--- libraries/chaos/include/chaos/Windowing/Windowing.h
+++ libraries/chaos/include/chaos/Windowing/Windowing.h
@@ -7,8 +7,6 @@
 #include "chaos/Windowing/LinearComposerLayout.h"
 #include "chaos/Windowing/WidgetComposer.h"
 #include "chaos/Windowing/WindowRootWidget.h"
-#include "chaos/Windowing/ViewportLayout.h"
-#include "chaos/Windowing/ViewportGridLayout.h"
 #include "chaos/Windowing/GamepadManager.h"
 #include "chaos/Windowing/WindowApplication.h"
 #include "chaos/Windowing/GLFWTools.h"

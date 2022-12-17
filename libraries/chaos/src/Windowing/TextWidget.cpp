48d27ebcae159c662177efddc261a5c87fd0e007
Stephane Hubart <hubart.stephane@gmail.com>
2022-12-17 16:01:11 +0100

    New (yet empty) TextWidget

---
+++ libraries/chaos/src/Windowing/TextWidget.cpp
@@ -0,0 +1,13 @@
+#include "chaos/ChaosPCH.h"
+#include "chaos/ChaosInternals.h"
+
+namespace chaos
+{
+	bool TextWidget::OnDraw(GPURenderer* renderer, WindowDrawParams const& draw_params, GPUProgramProviderInterface const* uniform_provider)
+	{
+
+
+		return true;
+	}
+
+}; // namespace chaos

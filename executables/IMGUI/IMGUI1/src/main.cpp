5f8c744ceb0d04068c82e33f95f1dcd1892d58bd
Stephane Hubart <hubart.stephane@gmail.com>
2023-04-22 22:31:04 +0200

    update IMGUI1: Add window

--- executables/IMGUI/IMGUI1/src/main.cpp
+++ executables/IMGUI/IMGUI1/src/main.cpp
@@ -1,9 +1,47 @@
 #include <chaos/chaos.h>
 #include "Imgui.h"
 
-int main(int argc, char ** argv, char ** env)
-{
-	ImGui::Render();
 
-	return 0;
+
+
+
+class WindowOpenGLTest : public chaos::Window
+{
+	friend class MyEvent;
+
+	CHAOS_DECLARE_OBJECT_CLASS(WindowOpenGLTest, chaos::Window);
+
+protected:
+
+	virtual bool OnDraw(chaos::GPURenderer* renderer, chaos::GPUProgramProviderInterface const* uniform_provider, chaos::WindowDrawParams const& draw_params) override
+	{
+		glm::vec4 clear_color(0.5f, 0.3f, 0.0f, 0.0f);
+		glClearBufferfv(GL_COLOR, 0, (GLfloat*)&clear_color);
+
+
+
+		return true;
+	}
+
+	virtual bool OnKeyEventImpl(chaos::KeyEvent const& event) override
+	{
+		if (event.IsKeyReleased(GLFW_KEY_ESCAPE))
+		{
+			RequireWindowClosure();
+			return true;
+		}
+		return chaos::Window::OnKeyEventImpl(event);
+	}
+};
+
+
+int main(int argc, char** argv, char** env)
+{
+	chaos::WindowCreateParams create_params;
+	create_params.monitor = nullptr;
+	create_params.width = 1200;
+	create_params.height = 500;
+	create_params.monitor_index = 0;
+
+	return chaos::RunWindowApplication<WindowOpenGLTest>(argc, argv, env, create_params);
 }

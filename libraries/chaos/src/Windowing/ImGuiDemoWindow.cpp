87131ca83cc58323d24a3e9737c98fdc3370de9d
Stephane Hubart <hubart.stephane@gmail.com>
2023-05-20 23:04:00 +0200

    ImGuiDemoWindow

--- libraries/chaos/src/Windowing/ImGuiDemoWindow.cpp
+++ libraries/chaos/src/Windowing/ImGuiDemoWindow.cpp
@@ -5,28 +5,20 @@ namespace chaos
 {
 	void ImGuiDemoWindow::OnDrawWindowImGuiContent()
 	{
-		int window_flags = ImGuiDrawableInterface::AddWindowMainMenuFlag(ImGuiDrawMenuMode::ImGuiWindow, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground);
-
-		ImGui::SetNextWindowPos({ 0, 0 });
-		ImGui::SetNextWindowSize({ ImGui::GetMainViewport()->Size.x, ImGui::GetMainViewport()->Size.y });
-		if (ImGui::Begin("##imgui_demos", nullptr, window_flags))
-		{
-			if (show_demo)
-				ImGui::ShowDemoWindow(&show_demo);
-			if (show_metrics)
-				ImGui::ShowMetricsWindow(&show_metrics);
-			if (show_debug_log)
-				ImGui::ShowDebugLogWindow(&show_debug_log);
-			if (show_stack_tool)
-				ImGui::ShowStackToolWindow(&show_stack_tool);
-			if (show_about)
-				ImGui::ShowAboutWindow(&show_about);
-			if (show_style_editor)
-				ImGui::ShowStyleEditor();
-			if (show_user_guide)
-				ImGui::ShowUserGuide();
-			ImGui::End();
-		}
+		if (show_demo)
+			ImGui::ShowDemoWindow(&show_demo);
+		if (show_metrics)
+			ImGui::ShowMetricsWindow(&show_metrics);
+		if (show_debug_log)
+			ImGui::ShowDebugLogWindow(&show_debug_log);
+		if (show_stack_tool)
+			ImGui::ShowStackToolWindow(&show_stack_tool);
+		if (show_about)
+			ImGui::ShowAboutWindow(&show_about);
+		if (show_style_editor)
+			ImGui::ShowStyleEditor();
+		if (show_user_guide)
+			ImGui::ShowUserGuide();
 	}
 
 	void ImGuiDemoWindow::OnDrawWindowImGuiMenu()
@@ -36,7 +28,7 @@ namespace chaos
 
 		chaos::ImGuiDrawableInterface::MenuBar(chaos::ImGuiDrawMenuMode::FullWindow, [this]()
 		{
-			if (ImGui::BeginMenu("ImGui Windows"))
+			if (ImGui::BeginMenu("ImGui"))
 			{
 	#define CHAOS_IMGUI_MENUITEM(X) ImGui::MenuItem(#X, nullptr, &X, true);
 				CHAOS_IMGUI_MENUITEM(show_demo);

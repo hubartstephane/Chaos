51aba23e2a91e44976d1a0935eb3fe8c38afa0b7
Stephane Hubart <hubart.mail@gmail.com>
2023-09-07 00:44:19 +0200

    Tree27 in progress

--- libraries/chaos/src/Gameplay/Tree27.cpp
+++ libraries/chaos/src/Gameplay/Tree27.cpp
@@ -3,5 +3,17 @@
 
 namespace chaos
 {
+	namespace details
+	{
+		float log3(float value)
+		{
+			return (std::log(value) / std::log(3.0f));
+		};
+
+		float pow3(float level)
+		{
+			return std::pow(3.0f, level);
+		};
+	};
 
 }; // namespace chaos

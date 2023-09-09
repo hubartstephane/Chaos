f004e8140f9c368f7de8f51b702aec7cdbc953ab
Stephane Hubart <hubart.mail@gmail.com>
2023-09-09 19:20:39 +0200

    tree27 almost finished

--- libraries/chaos/src/Gameplay/Tree27.cpp
+++ libraries/chaos/src/Gameplay/Tree27.cpp
@@ -5,14 +5,24 @@ namespace chaos
 {
 	namespace details
 	{
-		float log3(float value)
+		float log3(float src)
 		{
-			return (std::log(value) / std::log(3.0f));
+			return (std::log(src) / std::log(3.0f));
 		};
 
-		float pow3(float level)
+		float pow3(float src)
 		{
-			return std::pow(3.0f, level);
+			return std::pow(3.0f, src);
+		};
+
+		int log3i(int src)
+		{
+			return int(log3(float(src)));
+		};
+
+		int pow3i(int src)
+		{
+			return int(pow3(float(src)));
 		};
 	};
 

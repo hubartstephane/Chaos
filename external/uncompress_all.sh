11f8b5855cbc4804a2cb4ddc8b50e4a6a9b5870b
shubart <hubart.stephane@gmail.com>
2023-10-12 22:58:28 +0200

    new uncompress_all.sh script to prepare the compilation workspace

---
+++ external/uncompress_all.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/bash
+
+unzip external_zip/FreeImage3180Win32Win64.zip -d FreeImage-3.18
+unzip external_zip/glew-2.2.0-win32.zip -d glew-2.2.0
+unzip external_zip/irrKlang-64bit-1.6.0.zip -d irrKlang-64bit-1.6.0
+unzip external_zip/lua-5.4.2_Win64_vc16_lib -d lua-5.4.2
+unzip external_zip/ImGui -d ImGui
+
+#unzip external_zip/MSVC_Redist.zip -d

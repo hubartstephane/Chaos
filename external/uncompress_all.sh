b45f26903b53b39df5ac936b0b6a38cb94456380
shubart <hubart.stephane@gmail.com>
2023-10-12 23:30:18 +0200

    update uncompress_all.sh

--- external/uncompress_all.sh
+++ external/uncompress_all.sh
@@ -1,9 +1,11 @@
 #! /usr/bin/bash
 
-unzip external_zip/FreeImage3180Win32Win64.zip -d FreeImage-3.18
-unzip external_zip/glew-2.2.0-win32.zip -d glew-2.2.0
-unzip external_zip/irrKlang-64bit-1.6.0.zip -d irrKlang-64bit-1.6.0
-unzip external_zip/lua-5.4.2_Win64_vc16_lib -d lua-5.4.2
-unzip external_zip/ImGui -d ImGui
+SCRIPT_PATH=$(pwd)/$0
+SCRIPT_PATH=$(dirname $SCRIPT_PATH)
 
-#unzip external_zip/MSVC_Redist.zip -d
+unzip $SCRIPT_PATH/external_zip/FreeImage3180Win32Win64.zip -d $SCRIPT_PATH
+unzip $SCRIPT_PATH/external_zip/glew-2.2.0-win32.zip -d $SCRIPT_PATH
+unzip $SCRIPT_PATH/external_zip/irrKlang-64bit-1.6.0.zip -d $SCRIPT_PATH
+unzip $SCRIPT_PATH/external_zip/lua-5.4.2_Win64_vc16_lib -d $SCRIPT_PATH/lua-5.4.2
+unzip $SCRIPT_PATH/external_zip/ImGui -d $SCRIPT_PATH
+unzip $SCRIPT_PATH/external_zip/MSVC_Redist.zip -d $SCRIPT_PATH

a22e46e6b96bdccf384b19631fa77f599a84ff06
shubart <hubart.stephane@gmail.com>
2023-10-12 17:24:07 +0200

    remove NANA executables

--- executables/NANA/Nana_1/subpremake5.lua
+++
@@ -1,7 +0,0 @@
--- =============================================================================
--- ROOT_PATH/executables/NANA/Nana_1
--- =============================================================================
-
-local project = build:ConsoleApp()
-project:DependOnLib("CHAOS")
-project:DependOnLib("NANA")
--- executables/NANA/Nana_9/subpremake5.lua
+++
@@ -1,7 +0,0 @@
--- =============================================================================
--- ROOT_PATH/executables/NANA/Nana_9
--- =============================================================================
-
-local project = build:ConsoleApp()
-project:DependOnLib("CHAOS")
-project:DependOnLib("NANA")

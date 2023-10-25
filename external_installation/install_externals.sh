5b8d49d714ba7187089d09326d2c1000c0b2e007
stephane hubart <hubart.stephane@gmail.com>
2023-10-25 02:47:43 +0200

    update install_externals.sh

--- external_installation/install_externals.sh
+++ external_installation/install_externals.sh
@@ -1,40 +1,60 @@
 #! /usr/bin/bash
 
+# This script compiles and installs most libraries into "externals" directory
+
 SCRIPT_PATH=$(pwd)/$0
 SCRIPT_PATH=$(dirname $SCRIPT_PATH)
 
-unzip $SCRIPT_PATH/external_zip/FreeImage3180Win32Win64.zip -d $SCRIPT_PATH
-unzip $SCRIPT_PATH/external_zip/glew-2.2.0-win32.zip -d $SCRIPT_PATH
-unzip $SCRIPT_PATH/external_zip/irrKlang-64bit-1.6.0.zip -d $SCRIPT_PATH
-unzip $SCRIPT_PATH/external_zip/lua-5.4.2_Win64_vc16_lib -d $SCRIPT_PATH/lua-5.4.2
-unzip $SCRIPT_PATH/external_zip/ImGui -d $SCRIPT_PATH
-unzip $SCRIPT_PATH/external_zip/MSVC_Redist.zip -d $SCRIPT_PATH
+BASE_INSTALL_PATH=$SCRIPT_PATH/../externals/
+
+###########################################################################
+# Unzip libraries
+###########################################################################
+
+unzip $SCRIPT_PATH/external_zip/FreeImage3180Win32Win64.zip -d $BASE_INSTALL_PATH
+unzip $SCRIPT_PATH/external_zip/glew-2.2.0-win32.zip -d $BASE_INSTALL_PATH
+unzip $SCRIPT_PATH/external_zip/irrKlang-64bit-1.6.0.zip -d $BASE_INSTALL_PATH
+unzip $SCRIPT_PATH/external_zip/lua-5.4.2_Win64_vc16_lib -d $BASE_INSTALL_PATH/lua-5.4.2
+unzip $SCRIPT_PATH/external_zip/ImGui -d $BASE_INSTALL_PATH
+unzip $SCRIPT_PATH/external_zip/MSVC_Redist.zip -d $BASE_INSTALL_PATH
 
 cd $SCRIPT_PATH/external_zip
 gunzip --stdout $SCRIPT_PATH/external_zip/zlib-1.3.tar.gz | tar -x
 mv $SCRIPT_PATH/external_zip/zlib-1.3 $SCRIPT_PATH
 
-cd $SCRIPT_PATH/glfw
-cmake -S . -B build/
-cmake --build build/ --config Release
-cmake --build build/ --config Debug
+###########################################################################
+# Compile libraries (except zlib)
+###########################################################################
 
-cd $SCRIPT_PATH/freetype2
-cmake -S . -B build/
-cmake --build build/ --config Release
-cmake --build build/ --config Debug
+function BeginBuildLibrary()
+{
+	LIBRARY=$1
+	CONFIG_TYPE=Release
+	SRC_PATH=$SCRIPT_PATH/$LIBRARY
+	BUILD_PATH=$SRC_PATH/build/$CONFIG_TYPE
+	INSTALL_PATH=$BASE_INSTALL_PATH/$LIBRARY
+	
+	echo "========================================================="
+	echo "  -> Building: " $LIBRARY
+	echo "       SRC_PATH:     " $SRC_PATH
+	echo "       BUILD_PATH:   " $BUILD_PATH
+	echo "       INSTALL_PATH: " $INSTALL_PATH
+	echo "========================================================="
+}
 
-cd $SCRIPT_PATH/zlib-1.3
-cmake -S . -B .
-cmake --build . --config Release
-cmake --build . --config Debug
+for LIBRARY in assimp freetype2 glm json tinyxml2 glfw
+do
+	BeginBuildLibrary $LIBRARY
+	cmake -S $SRC_PATH -B $BUILD_PATH
+	cmake --build $BUILD_PATH --config $CONFIG_TYPE
+	cmake --install $BUILD_PATH --prefix $INSTALL_PATH
+done
 
-cd $SCRIPT_PATH/assimp
-cmake -S . -B .
-cmake --build . --config Release
-cmake --build . --config Debug
+###########################################################################
+# zlib-1.3: the --prefix is not work working for this lib. Use -DCMAKE_INSTALL_PREFIX instead
+###########################################################################
 
-cd $SCRIPT_PATH/tinyxml2
-cmake -S . -B build/
-cmake --build build/ --config Release
-cmake --build build/ --config Debug
+BeginBuildLibrary "zlib-1.3"
+cmake -S $SRC_PATH -B $BUILD_PATH -DCMAKE_INSTALL_PREFIX=$INSTALL_PATH
+cmake --build $BUILD_PATH --config $CONFIG_TYPE
+cmake --install $BUILD_PATH

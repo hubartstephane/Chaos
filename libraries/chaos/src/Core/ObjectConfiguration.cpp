7e031a052a43f56ffb87537614e54d87133ea4f4
stephane hubart <hubart.stephane@gmail.com>
2023-11-10 14:40:24 +0100

    Prepapre to have partial reloading of configurations

--- libraries/chaos/src/Core/ObjectConfiguration.cpp
+++ libraries/chaos/src/Core/ObjectConfiguration.cpp
@@ -55,6 +55,88 @@ namespace chaos
 		return result;
 	}
 
+	void ObjectConfigurationBase::TriggerChangeNotifications()
+	{
+		// propagate the update to sub hierarchy
+		for (shared_ptr<ChildObjectConfiguration>& child : child_configurations)
+			child->PropagateUpdates();
+		// send notifications
+		PropagateNotifications();
+	}
+
+	RootObjectConfiguration* ObjectConfigurationBase::GetRootConfiguration()
+	{
+		if (RootObjectConfiguration* root = auto_cast(this))
+			return root;
+		if (ChildObjectConfiguration* child = auto_cast(this))
+			return child->GetRootConfiguration();
+		return nullptr;
+	}
+
+	RootObjectConfiguration const* ObjectConfigurationBase::GetRootConfiguration() const
+	{
+		if (RootObjectConfiguration const* root = auto_cast(this))
+			return root;
+		if (ChildObjectConfiguration const* child = auto_cast(this))
+			return child->GetRootConfiguration();
+		return nullptr;
+	}
+
+	bool ObjectConfigurationBase::Reload(bool trigger_notifications)
+	{
+		if (RootObjectConfiguration* root = auto_cast(this))
+			return root->LoadConfigurations(true, false, trigger_notifications);
+		
+
+		return false;
+	}
+
+
+#if 0
+
+
+	bool ObjectConfigurationBase::ReloadPartial()
+	{
+		nlohmann::json new_root;
+
+		if (nlohmann::json const* parent_content = ReloadPartialHelper(new_root, this))
+		{
+			storage_read_config = *parent_content;
+			read_config = &storage_read_config;
+			return true;
+		}
+		return false;
+	}
+
+	nlohmann::json const * ObjectConfigurationBase::ReloadPartialHelper(nlohmann::json & new_root, ObjectConfigurationBase * src)
+	{
+		if (RootObjectConfiguration* root = auto_cast(this))
+		{
+			if (JSONTools::LoadJSONFile(read_config_path, new_root, LoadFileFlag::RECURSIVE))
+				return &new_root;
+			return nullptr;
+		}
+		if (ChildObjectConfiguration* child = auto_cast(this))
+		{
+			if (nlohmann::json const* parent_node = ReloadPartialHelper(new_root, child->parent_configuration.get()))
+			{
+				return JSONTools::GetStructure(*parent_node, child->key);
+			}
+			return nullptr;
+		}
+
+
+		return ReloadPartialHelper(this);
+	}
+
+
+
+
+
+#endif
+
+
+
 	// ---------------------------------------------------------------------
 	// ChildObjectConfiguration implementation
 	// ---------------------------------------------------------------------
@@ -91,6 +173,8 @@ namespace chaos
 		write_config = (parent_configuration != nullptr && parent_configuration->write_config != nullptr) ?
 			JSONTools::GetOrCreateStructure(*parent_configuration->write_config, key) :
 			nullptr;
+
+		storage_read_config = {}; // empty self storage
 	}
 
 	void ChildObjectConfiguration::PropagateUpdates()
@@ -108,39 +192,58 @@ namespace chaos
 
 	RootObjectConfiguration::RootObjectConfiguration()
 	{
-		read_config = &root_read_config;
-		write_config = &root_write_config;
+		read_config = &storage_read_config;
+		write_config = &storage_write_config;
 	}
 
-	void RootObjectConfiguration::TriggerConfigurationChanged()
+	bool RootObjectConfiguration::LoadConfigurations(bool load_read, bool load_write, bool trigger_notifications)
 	{
-		// propagate the update to sub hierarchy
-		for (shared_ptr<ChildObjectConfiguration>& child : child_configurations)
-			child->PropagateUpdates();
-		// send notifications
-		PropagateNotifications();
-	}
+		bool changed = false;
 
-	bool RootObjectConfiguration::LoadConfigurations(FilePathParam const& read_config_path, FilePathParam const& write_config_path, bool trigger_notifications)
-	{
 		// update the read json
-		root_read_config = nlohmann::json();
-		JSONTools::LoadJSONFile(read_config_path, root_read_config, LoadFileFlag::RECURSIVE);
+		if (load_read)
+		{
+			if (!read_config_path.empty())
+			{
+				storage_read_config = nlohmann::json();
+				JSONTools::LoadJSONFile(read_config_path, storage_read_config, LoadFileFlag::RECURSIVE);
+				changed = true;
+			}
+		}
 
 		// update the write json
-		root_write_config = nlohmann::json();
-		JSONTools::LoadJSONFile(write_config_path, root_write_config, LoadFileFlag::RECURSIVE);
+		if (load_write)
+		{
+			if (!write_config_path.empty())
+			{
+				storage_write_config = nlohmann::json();
+				JSONTools::LoadJSONFile(write_config_path, storage_write_config, LoadFileFlag::RECURSIVE);
+				changed = true;
+			}
+		}
 
 		// notify the changes
-		if (trigger_notifications)
-			TriggerConfigurationChanged();
+		if (changed && trigger_notifications)
+			TriggerChangeNotifications();
 
 		return true;
 	}
 
-	bool RootObjectConfiguration::SaveWriteConfiguration(FilePathParam const& write_config_path)
+	bool RootObjectConfiguration::SaveWriteConfiguration()
 	{
-		return JSONTools::SaveJSONToFile(root_write_config, write_config_path);
+		if (write_config_path.empty())
+			return false;
+		return JSONTools::SaveJSONToFile(storage_write_config, write_config_path);
+	}
+	
+	void RootObjectConfiguration::SetReadConfigPath(FilePathParam const& in_read_config_path)
+	{
+		read_config_path = in_read_config_path.GetResolvedPath();
+	}
+	
+	void RootObjectConfiguration::SetWriteConfigPath(FilePathParam const& in_write_config_path)
+	{
+		write_config_path = in_write_config_path.GetResolvedPath();
 	}
 
 }; // namespace chaos

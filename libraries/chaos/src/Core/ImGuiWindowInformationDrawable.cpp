28be95656b7b92bb061e40ebda218af26fcc8586
stephane hubart <hubart.stephane@gmail.com>
2023-12-08 00:09:15 +0100

    Add more information in debug window

--- libraries/chaos/src/Core/ImGuiWindowInformationDrawable.cpp
+++ libraries/chaos/src/Core/ImGuiWindowInformationDrawable.cpp
@@ -15,15 +15,24 @@ namespace chaos
 		if (window != nullptr)
 		{
 			// collect window information
-			glm::ivec2 window_size = window->GetWindowSize(false); // only client size
-			aabox2 viewport = window->GetRequiredViewport(window_size);
+
+			glm::ivec2 window_position = window->GetWindowPosition(true);   // include decorators
+			glm::ivec2 client_position = window->GetWindowPosition(false);  // client area only
+
+			glm::ivec2 window_size = window->GetWindowSize(true);  // include decorators
+			glm::ivec2 client_size = window->GetWindowSize(false); // client area only
+
+			aabox2 viewport = window->GetRequiredViewport(client_size);
 			double x = 0.0;
 			double y = 0.0;
 			glfwGetCursorPos(window->GetGLFWHandler(), &x, &y);
 
 			// trace debugging information
 			ImGui::Text("cursor              : (%0.3f, %0.3f)", (float)x, (float)y);
+			ImGui::Text("window   position   : (%0.3f, %0.3f)", (float)window_position.x, (float)window_position.y);
 			ImGui::Text("window   size       : (%0.3f, %0.3f)", (float)window_size.x, (float)window_size.y);
+			ImGui::Text("client   position   : (%0.3f, %0.3f)", (float)client_position.x, (float)client_position.y);
+			ImGui::Text("client   size       : (%0.3f, %0.3f)", (float)client_size.x, (float)client_size.y);
 			ImGui::Text("viewport position   : (%0.3f, %0.3f)", (float)viewport.position.x, (float)viewport.position.y);
 			ImGui::Text("viewport size       : (%0.3f, %0.3f)", (float)viewport.size.x, (float)viewport.size.y);
 

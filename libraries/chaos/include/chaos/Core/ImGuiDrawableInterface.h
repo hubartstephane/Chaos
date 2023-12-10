7e0343054cae9a0d45bb1db30bdea5758cda62ca
stephane hubart <hubart.stephane@gmail.com>
2023-12-11 00:27:31 +0100

    Use more LightweightFunction

--- libraries/chaos/include/chaos/Core/ImGuiDrawableInterface.h
+++ libraries/chaos/include/chaos/Core/ImGuiDrawableInterface.h
@@ -28,57 +28,10 @@ namespace chaos
 		static int GetConditionalMainMenuFlag(ImGuiDrawMenuMode menu_mode);
 
 		/** start a fullscreen window */
-		template<typename FUNC>
-		static void FullscreenWindow(char const* name, bool menu_bar, FUNC const & content_func)
-		{
-			int window_flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground;
-			if (menu_bar)
-				window_flags |= GetConditionalMainMenuFlag(ImGuiDrawMenuMode::ImGuiWindow);
+		static void FullscreenWindow(char const* name, bool menu_bar, LightweightFunction<void()> content_func);
 
-			ImGui::SetNextWindowPos({ ImGui::GetMainViewport()->WorkPos.x, ImGui::GetMainViewport()->WorkPos.y });
-			ImGui::SetNextWindowSize({ ImGui::GetMainViewport()->Size.x, ImGui::GetMainViewport()->Size.y });
-			if (ImGui::Begin(name, nullptr, window_flags))
-			{
-				content_func();
-				ImGui::End();
-			}
-		}
-
-		/** start a menu */
-		template<typename FUNC>
-		static bool MenuBar(ImGuiDrawMenuMode menu_mode, FUNC const& func)
-		{
-			bool show_menu = true;
-			if (WindowApplication* window_application = Application::GetInstance())
-				show_menu = window_application->GetImGuiMenuMode();
-
-			if (show_menu)
-			{
-				switch (menu_mode)
-				{
-				case ImGuiDrawMenuMode::FullWindow:
-					if (ImGui::BeginMainMenuBar())
-					{
-						func();
-						ImGui::EndMainMenuBar();
-						return true;
-					}
-					break;
-				case ImGuiDrawMenuMode::ImGuiWindow:
-					if (ImGui::BeginMenuBar())
-					{
-						func();
-						ImGui::EndMenuBar();
-						return true;
-					}
-					break;
-				default:
-					assert(0);
-					break;
-				}
-			}
-			return false;
-		}
+		/** try start a menu (maybe FullWindow, maybe ImGuiWindow, maybe failure if ImGUI mode is not active) */
+		static bool MenuBar(ImGuiDrawMenuMode menu_mode, LightweightFunction<void()> func);
 
 		/** request to close this interface */
 		void RequestClosing()

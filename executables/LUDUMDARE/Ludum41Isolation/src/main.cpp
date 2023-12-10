909bd52fd5839e18f006bddadc9133c8b50b4548
stephane hubart <hubart.stephane@gmail.com>
2023-12-10 11:16:16 +0100

    clean useless placement_info arguments in RunWindowApplication<...> calls

--- executables/LUDUMDARE/Ludum41Isolation/src/main.cpp
+++ executables/LUDUMDARE/Ludum41Isolation/src/main.cpp
@@ -18,7 +18,5 @@ public:
 
 int main(int argc, char** argv, char** env)
 {
-	chaos::WindowPlacementInfo placement_info;
-
-	return chaos::RunGame<LudumGame>(argc, argv, env, placement_info);
+	return chaos::RunGame<LudumGame>(argc, argv, env);
 }
--- executables/LUDUMDARE/Ludum44/src/main.cpp
+++ executables/LUDUMDARE/Ludum44/src/main.cpp
@@ -3,7 +3,5 @@
 
 int main(int argc, char ** argv, char ** env)
 {
-	chaos::WindowPlacementInfo placement_info;
-
-	return chaos::RunGame<LudumGame>(argc, argv, env, placement_info);
+	return chaos::RunGame<LudumGame>(argc, argv, env);
 }

d2c7d225550690ca2498006524ba1fc4521b5023
stephane hubart <hubart.stephane@gmail.com>
2023-12-11 09:39:59 +0100

    Rename some ImGui's classes

--- libraries/chaos/include/chaos/Core/ImGuiHelpObject.h
+++ libraries/chaos/include/chaos/Core/ImGuiHelpObject.h
@@ -2,19 +2,19 @@ namespace chaos
 {
 #ifdef CHAOS_FORWARD_DECLARATION
 
-	class ImGuiHelpDrawable;
+	class ImGuiHelpObject;
 
 #elif !defined CHAOS_TEMPLATE_IMPLEMENTATION
 
 	/**
-	* ImGuiHelpDrawable: a drawable that displays some help for keys
+	* ImGuiHelpObject: a drawable that displays some help for keys
 	*/
 
-	class ImGuiHelpDrawable : public ImGuiObject
+	class ImGuiHelpObject : public ImGuiObject
 	{
 	public:
 
-		CHAOS_DECLARE_OBJECT_CLASS(ImGuiHelpDrawable, ImGuiObject);
+		CHAOS_DECLARE_OBJECT_CLASS(ImGuiHelpObject, ImGuiObject);
 
 		/** override */
 		virtual void DrawImGui(ImGuiDrawMenuMode menu_mode) override;

9188e7cd3a4f4fbaa27b1fa714519ad6c8db9ff9
stephane hubart <hubart.stephane@gmail.com>
2023-12-13 01:43:01 +0100

    in progress

--- libraries/chaos/src/Core/ImGuiHelpObject.cpp
+++ libraries/chaos/src/Core/ImGuiHelpObject.cpp
@@ -7,20 +7,21 @@ namespace chaos
 	{
 		bool open_value = true;
 
-		ImGui::Begin("Help", &open_value, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_AlwaysAutoResize);
+		if (ImGui::Begin("Help", &open_value, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_AlwaysAutoResize))
+		{
+			ImGui::Text("F1:  Skip level");
+			ImGui::Text("F2:  Toggle cheat mode");
+			ImGui::Text("F3:  Reload game configuration");
+			ImGui::Text("F4:  Reload current level");
+			ImGui::Text("F5:  Quick save");
+			ImGui::Text("F6:  Toggle free camera mode");
+			ImGui::Text("F7:  Toggle imgui mode");
+			ImGui::Text("F8:  Reload GPU resources");
+			ImGui::Text("F9:  Screen captures");
+			ImGui::Text("F10: Toggle fullscreen");
 
-		ImGui::Text("F1:  Skip level");
-		ImGui::Text("F2:  Toggle cheat mode");
-		ImGui::Text("F3:  Reload game configuration");
-		ImGui::Text("F4:  Reload current level");
-		ImGui::Text("F5:  Quick save");
-		ImGui::Text("F6:  Toggle free camera mode");
-		ImGui::Text("F7:  Toggle imgui mode");
-		ImGui::Text("F8:  Reload GPU resources");
-		ImGui::Text("F9:  Screen captures");
-		ImGui::Text("F10: Toggle fullscreen");
-
-		ImGui::End();
+			ImGui::End();
+		}
 
 		if (!open_value)
 			RequestClosing();

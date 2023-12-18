b87c971d928efc626d81be3311cb0a72bd70fa60
stephane hubart <hubart.stephane@gmail.com>
2023-12-19 00:51:37 +0100

    GlobalVariableViewer

--- libraries/chaos/include/chaos/Core/ImGuiTools.h
+++ libraries/chaos/include/chaos/Core/ImGuiTools.h
@@ -1,19 +1,31 @@
 namespace chaos
 {
+#ifdef CHAOS_FORWARD_DECLARATION
+
 	namespace ImGuiTools
 	{
 
-#ifdef CHAOS_FORWARD_DECLARATION
+		/** implementation of DrawImGui for a bool */
+		CHAOS_API void DrawImGui(bool& value);
+		/** implementation of DrawImGui for a float */
+		CHAOS_API void DrawImGui(float& value);
 
+		/** concept to know whether DrawImGui can be called on a variable */
+		template<typename T>
+		concept HasDrawImGuiFunction = requires (T & t)
+		{
+			{ImGuiTools::DrawImGui(t)};
+		};
+
+		/** ImGUI use 32 bits integers (or strings) for ID's. when using pointers as key, a dedicated function is necessary while pointers are 64 bits */
+		CHAOS_API void PushID(void* ptr);
+
+	}; // namespace ImGuiTools
 
 
 #elif !defined CHAOS_TEMPLATE_IMPLEMENTATION
 
-		/** ImGUI use 32 bits integers (or strings) for ID's. when using pointers as key, a dedicated function is necessary while pointers are 64 bits */
-		void PushID(void* ptr);
 
 #endif
-
-	}; // namespace ImGuiTools
 
 }; // namespace chaos

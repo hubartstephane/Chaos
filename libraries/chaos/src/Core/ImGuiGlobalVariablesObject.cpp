d00261b2a3682f36e68cf370830ca90e143d6fcf
stephane hubart <hubart.stephane@gmail.com>
2024-02-25 20:59:27 +0100

    imgui vector operation in progress

--- libraries/chaos/src/Core/ImGuiGlobalVariablesObject.cpp
+++ libraries/chaos/src/Core/ImGuiGlobalVariablesObject.cpp
@@ -3,170 +3,48 @@
 
 namespace chaos
 {
-	class ImGuiDrawVectorHelper;
-
-	class ImGuiVectorOperationsSolverBase
-	{
-	public:
-
-		virtual void Process(void* in_vector, size_t in_source_index, size_t in_destination_index) = 0;
-	};
-
-	template<typename T>
-	class ImGuiVectorOperationsSolver : public ImGuiVectorOperationsSolverBase
-	{
-	public:
-
-		static ImGuiVectorOperationsSolverBase* GetInstance()
-		{
-			static ImGuiVectorOperationsSolver<T> result;
-			return &result;
-		}
-
-		virtual void Process(void* in_vector, size_t in_source_index, size_t in_destination_index) override
-		{
-			std::vector<T> & typed_vector = *(std::vector<T> *)in_vector;
-
-			// displace elements in the array
-			T source_element = std::move(typed_vector[in_source_index]);
-
-			if (in_source_index < in_destination_index)
-			{
-				for (size_t i = in_source_index; i < in_destination_index; ++i)
-					typed_vector[i] = std::move(typed_vector[i + 1]);
-			}
-			else
-			{
-				for (size_t i = in_source_index; i > in_destination_index; --i)
-					typed_vector[i] = std::move(typed_vector[i - 1]);
-			}
-			typed_vector[in_destination_index] = std::move(source_element);
-
-			// update the preload data
-			if (ImGuiDrawVectorHelper* payload = ImGuiDrawVectorHelper::GetCurrentPlayload(typed_vector))
-			{
-				payload->source_index = in_destination_index;
-				payload->destination_index = in_destination_index;
-			}
-		}
-	};
 
 
-	class ImGuiDrawVectorHelper
-	{
 
-	protected:
 
-		template<typename T>
-		friend class ImGuiVectorOperationsSolver;
 
-		/** constructor */
-		template<typename T>
-		ImGuiDrawVectorHelper(std::vector<T> * in_source_vector, size_t in_source_index):
-			source_vector(in_source_vector),
-			source_index(in_source_index),
-			solver(ImGuiVectorOperationsSolver<T>::GetInstance())
-		{
-		}
 
-		/** try to get the payload. only success if target_vector is the same than source_vector */
-		template<typename T>
-		static class ImGuiDrawVectorHelper* GetCurrentPlayload(std::vector<T>& in_target_vector)
-		{
-			if (ImGuiPayload const* payload = ImGui::GetDragDropPayload())
-				if (StringTools::Strcmp("CHAOS_VECTOR_OPERATIONS", payload->DataType) == 0)
-					if (ImGuiDrawVectorHelper* result = (ImGuiDrawVectorHelper*)payload->Data)
-						if (result->source_vector == &in_target_vector)
-							return result;
-			return nullptr;
-		}
 
-	public:
 
-		/** start a drag-and-drop */
-		template<typename T>
-		static bool CheckDragSource(std::vector<T> & in_source_vector, size_t in_source_index, ImGuiDragDropFlags source_flags, LightweightFunction<void()> draw_tooltip_func = {})
-		{
-			if (ImGui::BeginDragDropSource(source_flags))
-			{
-				ImGuiDrawVectorHelper data(&in_source_vector, in_source_index);
-				ImGui::SetDragDropPayload("CHAOS_VECTOR_OPERATIONS", &data, sizeof(ImGuiDrawVectorHelper), ImGuiCond_Once);
-
-				if (!(source_flags & ImGuiDragDropFlags_SourceNoPreviewTooltip))
-				{
-					ImGui::PushID((int)in_source_index); // ensure that whatever draw_tooltip_func does, there is not ID collision
-					draw_tooltip_func();
-					ImGui::PopID();
-				}
-
-				ImGui::EndDragDropSource();
-				return true;
-			}
-			return false;
-		}
-
-		/** end a drag-and-drop */
-		template<typename T>
-		static bool CheckDropOnTarget(std::vector<T> & in_target_vector, size_t in_destination_index, ImGuiDragDropFlags target_flags)
-		{
-			if (ImGui::BeginDragDropTarget())
-			{
-				if (ImGuiPayload const * payload = ImGui::AcceptDragDropPayload("CHAOS_VECTOR_OPERATIONS", target_flags))
-				{
-					if (ImGuiDrawVectorHelper* helper = (ImGuiDrawVectorHelper*)payload->Data)
-					{
-						if (helper->source_vector == &in_target_vector)
-						{
-							helper->destination_index = in_destination_index;
-							return true;
-						}
-					}
-				}
-				ImGui::EndDragDropTarget();
-			}
-			return false;
-		}
-
-		/** finalize pending operations */
-		template<typename T>
-		static bool SolveVectorOperations(std::vector<T> & in_vector)
-		{
-			if (ImGuiDrawVectorHelper const * payload = GetCurrentPlayload(in_vector))
-			{
-				if (payload->source_index.has_value() && payload->destination_index.has_value())
-				{
-					size_t source_index = payload->source_index.value();
-					size_t destination_index = payload->destination_index.value();
-					if (source_index != destination_index)
-						payload->solver->Process(payload->source_vector, source_index, destination_index);
-					return true;
-				}
-			}
-			return false;
-		}
-
-	public:
-
-		/** the vector which is concerned by operations */
-		void * source_vector = nullptr;
-		/** the index in the vector of the element as destination for drag-and-drop */
-		std::optional<size_t> source_index;
-		/** the index in the vector of the element as source for drag-and-drop */
-		std::optional<size_t> destination_index;
-		/** the operator for solving drag-and-drop */
-		ImGuiVectorOperationsSolverBase* solver = nullptr;
-	};
 
 
 	template<typename T>
+
 	class ImGuiDrawVectorOperationProcessor
 	{
+		/** possible operations to do on a vector */
 		enum class VectorOperation
 		{
 			Delete,
 			Duplicate,
 			InsertBefore,
 			InsertAfter
+		};
+
+		/** tool class for processing drag-and-drop */
+		class DragAndDropPayload
+		{
+
+		public:
+
+			/** constructor */
+			DragAndDropPayload(std::vector<T>* in_target, size_t in_source_index) :
+				target(in_target),
+				source_index(in_source_index){}
+
+		public:
+
+			/** the vector which is concerned by operations */
+			void* target = nullptr;
+			/** the index in the vector of the element as destination for drag-and-drop */
+			std::optional<size_t> source_index;
+			/** the index in the vector of the element as source for drag-and-drop */
+			std::optional<size_t> destination_index;
 		};
 
 	public:
@@ -185,27 +63,68 @@ namespace chaos
 		/** request for a duplication */
 		void Duplicate(size_t index)
 		{
-			pending_operation = VectorOperation::Duplicate;
-			target_index = index;
+			if constexpr (std::copy_constructible<T>)
+			{
+				pending_operation = VectorOperation::Duplicate;
+				target_index = index;
+			}
 		}
 
 		/** request for insertion (before) */
 		void InsertBefore(size_t index)
 		{
-			pending_operation = VectorOperation::InsertBefore;
-			target_index = index;
+			if constexpr (std::is_default_constructible_v<T>)
+			{
+				pending_operation = VectorOperation::InsertBefore;
+				target_index = index;
+			}
 		}
 
 		/** request for insertion (after) */
 		void InsertAfter(size_t index)
 		{
-			pending_operation = VectorOperation::InsertAfter;
-			target_index = index;
+			if constexpr (std::is_default_constructible_v<T>)
+			{
+				pending_operation = VectorOperation::InsertAfter;
+				target_index = index;
+			}
 		}
 
 		/** finalize the pending operation */
 		void ResolvePendingOperation()
 		{
+			/** resolve drag and drop */
+			if (DragAndDropPayload* drag_and_drop_payload = GetCurrentPlayload())
+			{
+				if (drag_and_drop_payload->source_index.has_value() && drag_and_drop_payload->destination_index.has_value())
+				{
+					size_t source_index = drag_and_drop_payload->source_index.value();
+					size_t destination_index = drag_and_drop_payload->destination_index.value();
+					if (source_index != destination_index)
+					{
+						T first_element = std::move(target[source_index]);
+
+						if (source_index < destination_index)
+						{
+							for (size_t i = source_index; i < destination_index; ++i)
+								target[i] = std::move(target[i + 1]);
+						}
+						else
+						{
+							for (size_t i = source_index; i > destination_index; --i)
+								target[i] = std::move(target[i - 1]);
+						}
+						target[destination_index] = std::move(first_element);
+
+						// update the preload data
+						drag_and_drop_payload->source_index = destination_index;
+						//drag_and_drop_payload->destination_index = destination_index;
+					}
+				}
+				return;
+			}
+
+			/* resolve other operations */
 			if (pending_operation.has_value())
 			{
 				switch (pending_operation.value())
@@ -224,30 +143,90 @@ namespace chaos
 			}
 		}
 
+		/** start a drag-and-drop */
+		bool CheckDragSource(size_t in_source_index, ImGuiDragDropFlags source_flags, LightweightFunction<void()> draw_tooltip_func = {})
+		{
+			if (ImGui::BeginDragDropSource(source_flags))
+			{
+				DragAndDropPayload drag_and_drop_payload(&target, in_source_index);
+				ImGui::SetDragDropPayload("CHAOS_VECTOR_OPERATIONS", &drag_and_drop_payload, sizeof(DragAndDropPayload), ImGuiCond_Once);
+
+				if (!(source_flags & ImGuiDragDropFlags_SourceNoPreviewTooltip))
+				{
+					ImGui::PushID((int)in_source_index); // ensure that whatever draw_tooltip_func does, there is not ID collision
+					draw_tooltip_func();
+					ImGui::PopID();
+				}
+
+				ImGui::EndDragDropSource();
+				return true;
+			}
+			return false;
+		}
+
+		/** end a drag-and-drop */
+		bool CheckDropOnTarget(size_t in_destination_index, ImGuiDragDropFlags target_flags)
+		{
+			if (ImGui::BeginDragDropTarget())
+			{
+				if (ImGuiPayload const* payload = ImGui::AcceptDragDropPayload("CHAOS_VECTOR_OPERATIONS", target_flags))
+				{
+					if (DragAndDropPayload* drag_and_drop_payload = (DragAndDropPayload*)payload->Data)
+					{
+						if (drag_and_drop_payload->target == &target)
+						{
+							drag_and_drop_payload->destination_index = in_destination_index;
+							return true;
+						}
+					}
+				}
+				ImGui::EndDragDropTarget();
+			}
+			return false;
+		}
+
 	protected:
+
+		/** try to get the payload. only success if target_vector is the same than source_vector */
+		DragAndDropPayload* GetCurrentPlayload()
+		{
+			if (ImGuiPayload const* payload = ImGui::GetDragDropPayload())
+				if (StringTools::Strcmp("CHAOS_VECTOR_OPERATIONS", payload->DataType) == 0)
+					if (DragAndDropPayload* result = (DragAndDropPayload*)payload->Data)
+						if (result->target == &target)
+							return result;
+			return nullptr;
+		}
 
 		/** process delete operation */
 		void DoDelete()
 		{
-
+			if (target_index < target.size())
+				target.erase(target.begin() + target_index);
 		}
 
 		/** process duplicate operation */
 		void DoDuplicate()
 		{
-
+			if constexpr (std::copy_constructible<T>)
+				if (target_index < target.size())
+					target.insert(target.begin() + target_index, target[target_index]);
 		}
 
 		/** process insert before operation */
 		void DoInsertBefore()
 		{
-
+			if constexpr (std::is_default_constructible_v<T>)
+				if (target_index < target.size())
+					target.insert(target.begin() + target_index, std::move(T()));
 		}
 
 		/** process insert after operation */
 		void DoInsertAfter()
 		{
-
+			if constexpr (std::is_default_constructible_v<T>)
+				if (target_index < target.size())
+					target.insert(std::next(target.begin() + target_index), std::move(T()));
 		}
 
 	protected:
@@ -266,7 +245,29 @@ namespace chaos
 
 
 
+	class AAA
+	{
+	public:
 
+		//AAA():
+		//	name("vide"){}
+
+		AAA(char const * in_name) :
+			name(in_name) {};
+
+
+		void DrawImGuiVariable(chaos::DrawImGuiVariableFlags flags)
+		{
+			ImGui::Text("A: %s", name.c_str());
+			ImGui::Text("B: %s", name.c_str());
+			ImGui::Text("C: %s", name.c_str());
+			ImGui::Text("D: %s", name.c_str());
+		}
+
+	protected:
+
+		std::string name;
+	};
 
 
 	template<typename T>
@@ -279,19 +280,16 @@ namespace chaos
 			ImGui::PushID((int)i);
 
 			// render the drag and drop handler
-			ImGui::Text("[%02d]", (int)i);
+			ImGui::Button(StringTools::Printf("[%02d]", i).c_str());
 
 			// source for drag and drop
 			ImGuiDragDropFlags source_flags = 0;
 			source_flags |= ImGuiDragDropFlags_SourceNoDisableHover;     // Keep the source displayed as hovered
 			source_flags |= ImGuiDragDropFlags_SourceNoHoldToOpenOthers; // Because our dragging is local, we disable the feature of opening foreign treenodes/tabs while dragging
 
-			ImGuiDrawVectorHelper::CheckDragSource(values, i, source_flags, [&values, i]()
+			vector_operation_processor.CheckDragSource(i, source_flags, [&values, i]()
 			{
 				ImGui::BeginGroup();
-				ImGui::Dummy({ 200.0f, 1.0f }); ImGui::SameLine();
-				ImGui::Text("toto");
-				ImGui::Dummy({ 200.0f, 1.0f });
 				DrawImGuiVariable(values[i]);
 				ImGui::EndGroup();
 			});
@@ -299,24 +297,20 @@ namespace chaos
 			// target for drag and drop
 			ImGuiDragDropFlags target_flags = 0;
 			target_flags |= ImGuiDragDropFlags_AcceptBeforeDelivery;    // Don't wait until the delivery (release mouse button on a target) to do something
-			ImGuiDrawVectorHelper::CheckDropOnTarget(values, i, target_flags);
+
+			vector_operation_processor.CheckDropOnTarget(i, target_flags);
 
 			// render the object itself
 			ImGui::SameLine();
 
 			ImGui::BeginGroup();
-			ImGui::Dummy({ 200.0f, 1.0f }); ImGui::SameLine();
-			ImGui::Text("toto");
-			ImGui::Dummy({ 200.0f, 1.0f });
 			DrawImGuiVariable(values[i]);
 			ImGui::EndGroup();
 
 			// drop down popup for row operations
 			ImGui::SameLine();
 			if (ImGui::ArrowButton("##down", ImGuiDir_Down))
-			{
 				ImGui::OpenPopup("row operation popup", ImGuiPopupFlags_MouseButtonRight);
-			}
 
 			// operation popup
 			if (ImGui::BeginPopup("row operation popup"))
@@ -335,18 +329,10 @@ namespace chaos
 				ImGui::EndPopup();
 			}
 
-
-
-
-
-
-
 			ImGui::PopID();
 		}
 
 		vector_operation_processor.ResolvePendingOperation();
-
-		ImGuiDrawVectorHelper::SolveVectorOperations(values);
 	}
 
 
@@ -426,18 +412,12 @@ namespace chaos
 
 		ImGui::Separator();
 
-		static std::vector<std::string> names1 = { "name1" , "name1", "name1", "name4", "name5", "name6", "name7", "name8", "name9", "name10" };
+		
+
+		static std::vector<AAA> names1 = { "name1" , "name2", "name3", "name4", "name5", "name6", "name7", "name8", "name9", "name10" };
 		ImGui::PushID(1);
 		DrawArray(names1);
 		ImGui::PopID();
-
-		ImGui::Separator();
-		return;
-		static std::vector<std::string> names2 = { "xxxxxxx1" , "xxxxxxx2", "xxxxxxx3", "xxxxxxx4", "xxxxxxx5", "xxxxxxx6" };
-		ImGui::PushID(1);
-		DrawArray(names2);
-		ImGui::PopID();
-
 
 	}
 

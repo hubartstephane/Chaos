9f17a357b648912871bf93a240c0058495bb5502
stephane hubart <hubart.stephane@gmail.com>
2024-05-08 15:31:10 +0200

    add forgotten files for ImGuiObject

---
+++ libraries/chaos/include/chaos/Core/ImGuiObject.h
@@ -0,0 +1,71 @@
+namespace chaos
+{
+#ifdef CHAOS_FORWARD_DECLARATION
+
+	class ImGuiObject;
+
+	enum class ImGuiDrawFlags;
+
+#elif !defined CHAOS_TEMPLATE_IMPLEMENTATION
+
+	/**
+	 * ImGuiDrawFlags: some flags concerning ImGui rendering
+	 */
+
+	enum class ImGuiDrawFlags : int
+	{
+		NONE = 0,
+		FULL_WINDOW = 1,           // the ImGui (pseudo) window fills the whole GLFW window
+		FLOATING_IMGUI_WINDOW = 2, // this is a standart ImGui (pseudo) window
+		USE_FULL_WINDOW_MENU = 4   // menu items are stored inside the full GLFW window's menu instead of the ImGui (pseudo) window's menu
+	};
+
+	CHAOS_DECLARE_ENUM_BITMASK_METHOD(ImGuiDrawFlags, CHAOS_API);
+
+	using BeginImGuiMenuFunc = LightweightFunction<void(LightweightFunction<void()>)>;
+
+	/**
+	 * ImGuiObject: an interface to display an ImGui window
+	 */
+	class CHAOS_API ImGuiObject : public Object
+	{
+	public:
+
+		CHAOS_DECLARE_OBJECT_CLASS(ImGuiObject, Object);
+
+		/** main method to display both menu and content */
+		virtual void DrawImGui(char const * title, ImGuiDrawFlags flags);
+
+		/** request to close this interface */
+		void RequestClosing();
+		/** check whether closing is requested */
+		bool IsClosingRequested() const;
+
+		/** create a fullscreen window and fill with content (returns true if the window must be kept alive) */
+		static bool FullscreenWindow(char const* title, int imgui_window_flags, LightweightFunction<void()> content_func);
+		/** create a floating window and fill with content (returns true if the window must be kept alive) */
+		static bool FloatingWindow(char const* title, int imgui_window_flags, LightweightFunction<void()> content_func);
+
+	protected:
+
+		/** get flags */
+		virtual int GetImGuiWindowFlags() const;
+		/** method to override to display content */
+		virtual void OnDrawImGuiContent();
+		/** method to override to display menu */
+		virtual void OnDrawImGuiMenu(BeginImGuiMenuFunc begin_menu_func);
+
+		/** check whether a context is valid for having a menubar and call FullScreen or Floating window menu bar method */
+		void OnDrawImGuiMenuConditional(ImGuiDrawFlags flags, int imgui_window_flags);
+		/** update the imgui window flags according to use flags */
+		int UpdateWindowFlagsForMenu(ImGuiDrawFlags flags, int imgui_window_flags);
+
+	protected:
+
+		/** indicates whether closing this UI is requested */
+		bool closing_request = false;
+	};
+
+#endif
+
+}; // namespace chaos
---
+++ libraries/chaos/src/Core/ImGuiObject.cpp
@@ -0,0 +1,143 @@
+#include "chaos/ChaosPCH.h"
+#include "chaos/ChaosInternals.h"
+
+namespace chaos
+{
+	static chaos::EnumTools::EnumBitmaskMetaData<ImGuiDrawFlags> const ImGuiDrawFlags_bitmask_metadata =
+	{
+		{ImGuiDrawFlags::FULL_WINDOW, ImGuiDrawFlags::FLOATING_IMGUI_WINDOW}
+	};
+
+	CHAOS_IMPLEMENT_ENUM_BITMASK_METHOD(ImGuiDrawFlags, &ImGuiDrawFlags_bitmask_metadata, CHAOS_API);
+
+	void ImGuiObject::OnDrawImGuiMenuConditional(ImGuiDrawFlags flags, int imgui_window_flags)
+	{
+		// check wether the application accepts the menu bar
+		if (WindowApplication* window_application = Application::GetInstance())
+			if (!window_application->GetImGuiMenuMode())
+				return;
+
+		// display full window menu bar
+		if (HasAnyFlags(flags, ImGuiDrawFlags::USE_FULL_WINDOW_MENU))
+		{
+			OnDrawImGuiMenu(ImGuiTools::BeginMainMenuBar());
+		}
+		// display floating window menu bar (default)
+		else
+		{
+			if ((imgui_window_flags & ImGuiWindowFlags_MenuBar) != 0)
+			{
+				OnDrawImGuiMenu(ImGuiTools::BeginMenuBar());
+			}
+		}
+	}
+
+	int ImGuiObject::UpdateWindowFlagsForMenu(ImGuiDrawFlags flags, int imgui_window_flags)
+	{
+		// check whether application accepts menu
+		if (WindowApplication* window_application = Application::GetInstance())
+			if (!window_application->GetImGuiMenuMode())
+				return imgui_window_flags & ~ImGuiWindowFlags_MenuBar;
+
+		// check whether the menu is to be plugged into full window
+		if (HasAnyFlags(flags, ImGuiDrawFlags::USE_FULL_WINDOW_MENU))
+			return imgui_window_flags & ~ImGuiWindowFlags_MenuBar;
+
+		// request if there is really some menu content
+		bool menu_required = false;
+		auto fake_begin_menu_func = [&menu_required](LightweightFunction<void()> func)
+		{
+			menu_required = true; // this implementation fully ignore the "func" content. we just check whether OnDrawImGuiMenu calls "fake_begin_menu_func"
+		};
+		OnDrawImGuiMenu(fake_begin_menu_func);
+
+		// add or remove menu bar flag
+		if (menu_required)
+			return imgui_window_flags | ImGuiWindowFlags_MenuBar;
+		else
+			return imgui_window_flags & ~ImGuiWindowFlags_MenuBar;
+	}
+
+	void ImGuiObject::DrawImGui(char const * title, ImGuiDrawFlags flags)
+	{
+		// check flags coherency
+		assert(
+			(!HasAnyFlags(flags, ImGuiDrawFlags::FULL_WINDOW)) ||
+			(!HasAnyFlags(flags, ImGuiDrawFlags::FLOATING_IMGUI_WINDOW))
+		);
+
+		// get ImGui window flags
+		int imgui_window_flags = GetImGuiWindowFlags();
+		imgui_window_flags = UpdateWindowFlagsForMenu(flags, imgui_window_flags);
+
+		// display content + menu delegate
+		auto display_func = [this, flags, imgui_window_flags]()
+		{
+			OnDrawImGuiContent();
+			OnDrawImGuiMenuConditional(flags, imgui_window_flags);
+		};
+
+		// display fullscreen window
+		if (HasAnyFlags(flags, ImGuiDrawFlags::FULL_WINDOW))
+		{
+			closing_request = !FullscreenWindow(title, imgui_window_flags, display_func);
+		}
+		// display floating window (default case)
+		else
+		{
+			closing_request = !FloatingWindow(title, imgui_window_flags, display_func);
+		}
+	}
+
+	bool ImGuiObject::FullscreenWindow(char const* title, int imgui_window_flags, LightweightFunction<void()> content_func)
+	{
+		// update flags
+		imgui_window_flags |= ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoBackground | ImGuiWindowFlags_NoBringToFrontOnFocus;
+
+		// prepare ImGui window size and position
+		ImGui::SetNextWindowPos({ ImGui::GetMainViewport()->WorkPos.x, ImGui::GetMainViewport()->WorkPos.y });
+		ImGui::SetNextWindowSize({ ImGui::GetMainViewport()->Size.x, ImGui::GetMainViewport()->Size.y });
+		// display then window
+		if (ImGui::Begin(title, nullptr, imgui_window_flags))
+		{
+			content_func();
+			ImGui::End();
+		}
+		return true; // window must be kept alive
+	}
+
+	bool ImGuiObject::FloatingWindow(char const* title, int imgui_window_flags, LightweightFunction<void()> content_func)
+	{
+		bool open_value = true;
+		if (ImGui::Begin(title, &open_value, imgui_window_flags))
+		{
+			content_func();
+			ImGui::End();
+		}
+		return open_value;
+	}
+
+	int ImGuiObject::GetImGuiWindowFlags() const
+	{
+		return ImGuiWindowFlags_NoCollapse;
+	}
+
+	void ImGuiObject::OnDrawImGuiContent()
+	{
+	}
+
+	void ImGuiObject::OnDrawImGuiMenu(BeginImGuiMenuFunc begin_menu_func)
+	{
+	}
+
+	void ImGuiObject::RequestClosing()
+	{
+		closing_request = true;
+	}
+
+	bool ImGuiObject::IsClosingRequested() const
+	{
+		return closing_request;
+	}
+
+}; // namespace chaos

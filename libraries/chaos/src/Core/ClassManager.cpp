beea9474424d5e0bccd282dfa3e307ac8f9f29cf
stephane hubart <hubart.stephane@gmail.com>
2024-06-17 07:35:49 +0200

    Refactor Class. Prepare to remove functors and have inheriting classes instead

--- libraries/chaos/src/Core/ClassManager.cpp
+++ libraries/chaos/src/Core/ClassManager.cpp
@@ -3,6 +3,8 @@
 
 namespace chaos
 {
+	CHAOS_IMPLEMENT_ENUM_BITMASK_METHOD(FindClassFlags, nullptr, CHAOS_API);
+
 	ClassManager::ClassManager(ClassManager* in_parent_manager):
 		parent_manager(in_parent_manager)
 	{
@@ -14,30 +16,10 @@ namespace chaos
 		return &default_instance;
 	}
 
-	Class* ClassManager::CreateClass(std::string name, Class * in_parent_class)
-	{
-		// search in current manager (but not parents) for a class that have same name
-		auto it = std::ranges::find_if(classes, [&name](Class const* cls)
-		{
-			if (StringTools::Stricmp(cls->GetClassName(), name) == 0)
-			{
-				return true;
-			}
-			return false;
-		});
-		// cannot have a class with same name in the same manager (but its possible in parent manager)
-		if (it != classes.end())
-		{
-			Log::Error("ClassManager::CreateClass(...): class already existing [%s]", name.c_str());
-			return nullptr;
-		}
-		// create the class
-		return new Class(this, std::move(name), in_parent_class);
-	}
-
-	ClassFindResult ClassManager::FindClass(char const* name, bool search_manager_hierarchy)
+	ClassFindResult ClassManager::FindClass(char const* name, FindClassFlags flags)
 	{
 		assert(name != nullptr);
+		assert(HasAnyFlags(flags, FindClassFlags::NAME | FindClassFlags::SHORTNAME)); // at least one search criteria
 
 		// early exit
 		if (StringTools::IsEmpty(name))
@@ -48,23 +30,29 @@ namespace chaos
 		while (manager != nullptr)
 		{
 			ClassMatchType match_type = ClassMatchType::MATCH_NAME;
-			auto it = std::ranges::find_if(manager->classes, [name, &match_type](Class const* cls)
+			auto it = std::ranges::find_if(manager->classes, [name, &match_type, flags](Class const* cls)
 			{
-				if (StringTools::Stricmp(cls->GetClassName(), name) == 0)
+				if (HasAnyFlags(flags, FindClassFlags::NAME))
 				{
-					match_type = ClassMatchType::MATCH_NAME;
-					return true;
-				}
-				else if (StringTools::Stricmp(cls->GetShortName(), name) == 0)
+					if (StringTools::Stricmp(cls->GetClassName(), name) == 0)
+					{
+						match_type = ClassMatchType::MATCH_NAME;
+						return true;
+					}
+				}				
+				if (HasAnyFlags(flags, FindClassFlags::SHORTNAME))
 				{
-					match_type = ClassMatchType::MATCH_SHORTNAME;
-					return true;
+					if (StringTools::Stricmp(cls->GetShortName(), name) == 0)
+					{
+						match_type = ClassMatchType::MATCH_SHORTNAME;
+						return true;
+					}
 				}
 				return false;
 			});
 			if (it != manager->classes.end())
 				return { this, it, match_type };
-			if (!search_manager_hierarchy)
+			if (!HasAnyFlags(flags, FindClassFlags::PARENT_MANAGER))
 				break;
 			manager = manager->parent_manager.get();
 		}

3a2d828100c3afcac281609798ab882d7253a5e9
shubart <hubart.stephane@gmail.com>
2024-07-21 10:14:32 +0200

    update some premake.lua files to ignore some directories (for linux tests)

--- premake_scripts/premake5.lua
+++ premake_scripts/premake5.lua
@@ -128,7 +128,8 @@ build:DisplayInformation()
 
 require 'external_premake5' -- declare external libraries
 
-local sub_directories = {'libraries', 'executables','shared_resources'}
+--local sub_directories = {'libraries', 'executables','shared_resources'}
+local sub_directories = {'executables'}
 build:ProcessSubPremake(sub_directories, true) -- create sub groups
 
 build:CollectDependencies()

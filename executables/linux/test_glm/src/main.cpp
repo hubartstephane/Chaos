cd98f58df65adeb30f4779124bbc2d5c970637a0
shubart <hubart.stephane@gmail.com>
2024-07-21 18:11:41 +0200

    new test_glm executable for linux

---
+++ executables/linux/test_glm/src/main.cpp
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+
+#include <glm/glm.hpp>
+
+int main(int argc, char ** argv)
+{
+	glm::vec3 v = {1.0f, 2.0f, 3.0f};
+	v = v + v * 3.0f;
+	
+	std::cout << "[" << v.x << ", " << v.y << ", " << v.z << "]" << std::endl;
+	return 0;
+}
+ 

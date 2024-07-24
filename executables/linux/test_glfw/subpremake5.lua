5ee83a29bc8041a9415fc93af85f4228f7c52c92
shubart <hubart.stephane@gmail.com>
2024-07-24 07:41:18 +0200

    new test_glfw executable to test for opengl+glfw with linux

---
+++ executables/linux/test_glfw/subpremake5.lua
@@ -0,0 +1,7 @@
+-- =============================================================================
+-- ROOT_PATH/executables/linux/test_glfw
+-- =============================================================================
+
+local project = build:WindowedApp()
+project:DependOnLib("GLFW")
+project:DependOnLib("OPENGL")

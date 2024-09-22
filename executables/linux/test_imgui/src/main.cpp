65fe8dbc16b0317e546569bb22492f382ee3b547
stephane.hubart <hubart.stephane@gmail.com>
2024-09-22 15:46:14 +0200

    test_imgui is working on linux

--- executables/linux/test_imgui/src/main.cpp
+++ executables/linux/test_imgui/src/main.cpp
@@ -2,8 +2,6 @@
 #include <stdio.h>
 #include <iostream>
 
-
-
 //#define GLEW_STATIC
 #include <GL/glew.h>
 #if _WIN32
@@ -33,19 +31,24 @@
 #include <GLFW/glfw3native.h>
 #endif
 
-
-
-
-
-
-
-
-
+#include "imgui.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "backends/imgui_impl_glfw.h"
 
 void DrawWindow(GLFWwindow * window)
 {
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui_ImplGlfw_NewFrame();
+	ImGui::NewFrame();
+	
+	ImGui::Begin("Hello world");
+	ImGui::End();	
+	
 	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 	
 	glfwSwapBuffers(window);
 }
@@ -61,15 +64,30 @@ int main(int argc, char ** argv)
 		glfwMakeContextCurrent(window);
 		
 		GLenum err = glewInit();
+
+		IMGUI_CHECKVERSION();
+		ImGui::CreateContext();
+		ImGuiIO& io = ImGui::GetIO(); (void)io;
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+		io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+		ImGui::StyleColorsDark();		
+				
+		char const * glsl_version = "#version 130";
+		
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init(glsl_version);
 		
 		while (!glfwWindowShouldClose(window))
 		{
 			DrawWindow(window);
 			glfwPollEvents();
 		}
+		
+		ImGui_ImplOpenGL3_Shutdown();
+		ImGui_ImplGlfw_Shutdown();
+		ImGui::DestroyContext();
+		
 		glfwDestroyWindow(window);
-		
-		
 	}
 	glfwTerminate();
 

f08961f37c3137880530e85fb2912739436f22ee
stephane.hubart <hubart.stephane@gmail.com>
2024-10-06 22:48:58 +0200

    enable back old 'libraries', 'executables' and 'shared_resources' back into solution

--- HIDDEN_executables/subpremake5.lua
+++
@@ -1,7 +0,0 @@
--- =============================================================================
--- ROOT_PATH/executables
--- =============================================================================
-
-for _, v in ipairs({"LUDUMDARE", "GLFW", "MISC", "TOOLS", "LUA", "SOUND", "MODEL3D", "VULKAN", "C++" })   do
-	build:ProcessSubPremake(v, true) -- create a sub groups
-end
--- executables/subpremake5.lua
+++ executables/subpremake5.lua
@@ -2,6 +2,6 @@
 -- ROOT_PATH/executables
 -- =============================================================================
 
-for _, v in ipairs({"test_external_libraries" })   do
+for _, v in ipairs({"LUDUMDARE", "GLFW", "MISC", "TOOLS", "LUA", "SOUND", "MODEL3D", "VULKAN", "C++", "TEST_EXTERNAL_LIBRARIES" })   do
 	build:ProcessSubPremake(v, true) -- create a sub groups
 end

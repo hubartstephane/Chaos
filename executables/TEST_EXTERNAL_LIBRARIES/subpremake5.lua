1b8c95d9c0e3088b121ff37dcbd65ee2bc2b3da4
stephane hubart <hubart.stephane@gmail.com>
2024-10-06 23:04:14 +0200

    rewrite test_external_libraries/subpremake5.lua

--- executables/TEST_EXTERNAL_LIBRARIES/subpremake5.lua
+++ executables/TEST_EXTERNAL_LIBRARIES/subpremake5.lua
@@ -2,6 +2,19 @@
 -- ROOT_PATH/executables/test_external_libraries
 -- =============================================================================
 
-for _, v in ipairs({"test_empty", "test_glm", "test_tinyxml2", "test_nlohmann", "test_glfw", "test_freeimage", "test_classes", "test_lua", "test_freetype2", "test_assimp", "test_glslang", "test_vulkan", "test_zlib", "test_boost", "test_irrklang", "test_imgui" })   do
-	build:ProcessSubPremake(v, true) -- create a sub groups
-end
+build:ProcessSubPremake("test_empty")
+build:ProcessSubPremake("test_glm")
+build:ProcessSubPremake("test_tinyxml2")
+build:ProcessSubPremake("test_nlohmann")
+build:ProcessSubPremake("test_glfw")
+build:ProcessSubPremake("test_freeimage")
+build:ProcessSubPremake("test_classes")
+build:ProcessSubPremake("test_lua")
+build:ProcessSubPremake("test_freetype2")
+build:ProcessSubPremake("test_assimp")
+build:ProcessSubPremake("test_glslang")
+build:ProcessSubPremake("test_vulkan")
+build:ProcessSubPremake("test_zlib")
+build:ProcessSubPremake("test_boost")
+build:ProcessSubPremake("test_irrklang")
+build:ProcessSubPremake("test_imgui")

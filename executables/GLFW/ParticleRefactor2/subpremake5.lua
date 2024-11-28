3d2d259a3bc553fd4ab16d1311585a2945e288b1
Stephane Hubart <stephane.hubart@buildarocketboy.com>
2024-11-28 22:40:18 +0100

    remove ParticleRefactor2

--- executables/GLFW/ParticleRefactor2/subpremake5.lua
+++
@@ -1,6 +0,0 @@
--- =============================================================================
--- ROOT_PATH/executables/GLFW/ParticleRefactor2
--- =============================================================================
-
-local project = build:WindowedApp()
-project:DependOnLib("CHAOS")

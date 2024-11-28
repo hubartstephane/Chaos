ecaccf075aedd62eb0f242eca2286844bcfc793b
Stephane Hubart <hubart.stephane@gmail.com>
2024-11-28 22:27:40 +0100

    move some files from Gameplay to 3D directory

--- libraries/chaos/include/chaos/3D/3D.h
+++ libraries/chaos/include/chaos/3D/3D.h
@@ -2,3 +2,7 @@
 #include "chaos/3D/FPSViewController.h"
 #include "chaos/3D/MyAssimpImporter.h"
 #include "chaos/3D/MyFBxImporter.h"
+#include "chaos/3D/LooseTree27.h"
+#include "chaos/3D/SceneTransform.h"
+#include "chaos/3D/SceneNode.h"
+#include "chaos/3D/SceneRenderer.h"

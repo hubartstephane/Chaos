485f34cf304cfc067eb8b73dd42e109b2144b552
Stephane Hubart <hubart.stephane@gmail.com>
2024-11-30 11:00:44 +0100

    Node LooseTree27 uses templated allocator. Add a new StandardAllocator

--- libraries/chaos/include/chaos/Core/Allocators.h
+++ libraries/chaos/include/chaos/Core/Allocators.h
@@ -3,8 +3,36 @@ namespace chaos
 #ifdef CHAOS_FORWARD_DECLARATION
 
 	template<typename T> class ArrayAllocator;
+	template<typename T> class StandardAllocator;
 
 #elif !defined CHAOS_TEMPLATE_IMPLEMENTATION
+
+	/**
+	* a class for default allocation
+	*/
+
+	template<typename T>
+	class StandardAllocator
+	{
+	public:
+
+		/** the type of object to allocate */
+		using type = T;
+
+		/** release an object */
+		void Free(type* object)
+		{
+			if (object != nullptr)
+				delete (object);
+		}
+
+		/** allocate a new object */
+		template<typename ...PARAMS>
+		type* Allocate(PARAMS ...params)
+		{
+			return new type(std::forward<PARAMS>(params)...);
+		}
+	};
 
 	/**
 	* a default class for Allocation of Array

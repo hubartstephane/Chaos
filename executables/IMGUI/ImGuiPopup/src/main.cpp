3dd1f84585dedd6b8a3e86e1f1bbd6c53262270f
Stephane Hubart <hubart.stephane@gmail.com>
2025-01-03 00:19:36 +0100

    minor changes inside ImGuiPopupTest

--- executables/IMGUI/ImGuiPopup/src/main.cpp
+++ executables/IMGUI/ImGuiPopup/src/main.cpp
@@ -39,6 +39,12 @@ protected:
 
 
 
+
+
+
+
+
+
 class MyApplicationTest : public chaos::WindowApplication
 {
 public:
@@ -48,35 +54,6 @@ public:
 
 
 
-class MyImGuiPopup : public chaos::ImGuiObject
-{
-public:
-
-
-#if 0
-	MyImGuiPopup(std::function<void()> in_func) :
-		func(std::move(in_func))
-	{
-	}
-#endif
-
-	virtual void OnDrawImGuiContent(chaos::Window* window) override
-	{
-		if (ImGui::Button("Close"))
-		{
-			RequestClosing();
-		}
-	}
-
-	virtual void OnDrawImGuiMenu(chaos::Window* window, chaos::BeginImGuiMenuFunc begin_menu_func) override
-	{
-
-
-	}
-	
-
-	std::function<void()> func;
-};
 
 
 class WindowOpenGLTest : public chaos::Window
@@ -85,27 +62,6 @@ class WindowOpenGLTest : public chaos::Window
 
 protected:
 
-#if 1
-	virtual bool OnKeyEventImpl(chaos::KeyEvent const& key_event) override
-	{
-		if (key_event.IsKeyDown(chaos::KeyboardButton::ESCAPE))
-		{
-		//	return true;
-		}
-		
-
-
-		return chaos::Window::OnKeyEventImpl(key_event);
-	}
-
-#endif
-
-	void AddPopup(char const * name, std::function<void()> func)
-	{
-		//MyImGuiPopup * popup = new MyImGuiPopup(std::move(func));
-		//popup->SetName(name);
-		//AddImGuiObject(popup);
-	}
 
 	virtual bool OnDraw(chaos::GPURenderer* renderer, chaos::GPUProgramProviderInterface const* uniform_provider, chaos::WindowDrawParams const& draw_params) override
 	{
@@ -123,24 +79,20 @@ protected:
 		{
 			if (ImGui::Button("show popup", { 200.0f , 200.0f }))
 			{
-				chaos::ImGuiWindowPlacement placement = chaos::ImGuiWindowPlacement::GetCenterOnScreenPlacement();
 
-				MyImGuiPopup* p = new MyImGuiPopup;
-				p->SetName("titi");
-				p->SetImGuiObjectFlags(chaos::ImGuiObjectFlags::PopupModalWindow);
-				AddImGuiObject(p);
-
-
-				
-
-
-
+				AddNewImGuiPopup<chaos::ImGuiYesNoObject>("hello", "do you really want ?", [this](bool validate)
+				{
+					if (validate)
+						AddNewImGuiPopup<chaos::ImGuiMessageObject>("Hello", "Hello World true");
+					else
+						AddNewImGuiPopup<chaos::ImGuiMessageObject>("Hello", "Hello World false");
+				});
 
 
 
 
 				//placement.movable = true;
-				message_popup.Open("error", "this is the error message", placement);
+				//message_popup.Open("error", "this is the error message", placement);
 			}
 			ImGui::End();
 		}

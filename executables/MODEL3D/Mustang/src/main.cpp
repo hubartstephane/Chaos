77dd1a5c7d798e10391e9e3af52aa401c709ab13
Stephane Hubart <hubart.stephane@gmail.com>
2025-01-10 16:00:48 +0100

    update Mustang application with some assimp import

--- executables/MODEL3D/Mustang/src/main.cpp
+++ executables/MODEL3D/Mustang/src/main.cpp
@@ -19,7 +19,8 @@ protected:
 		glClearBufferfi(GL_DEPTH_STENCIL, 0, far_plane, 0);
 
 		glEnable(GL_DEPTH_TEST);
-		glEnable(GL_CULL_FACE);
+		glDisable(GL_CULL_FACE);
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 		// compute matrices
 		glm::mat4x4 projection = glm::perspectiveFov(fov * (float)M_PI / 180.0f, float(draw_params.viewport.size.x), float(draw_params.viewport.size.y), near_plane, far_plane);
@@ -35,8 +36,18 @@ protected:
 		chaos::GPURenderParams render_params;
 		mesh_sphere->DisplayWithProgram(program.get(), renderer, &main_uniform_provider, render_params);
 
+		// display other meshes
+		for (chaos::shared_ptr<chaos::GPUMesh> const & mesh : meshes)
+			mesh->DisplayWithProgram(program.get(), renderer, &main_uniform_provider, render_params);
+
 
 		return true;
+	}
+
+	boost::filesystem::path const & GetResourcesPath() const
+	{
+		chaos::WindowApplication* application = chaos::Application::GetInstance();
+		return application->GetResourcesPath();
 	}
 
 	virtual bool InitializeFromConfiguration(nlohmann::json const* config) override
@@ -45,18 +56,18 @@ protected:
 		if (!chaos::Window::InitializeFromConfiguration(config))
 			return false;
 
-		chaos::WindowApplication* application = chaos::Application::GetInstance();
-		if (application == nullptr)
-			return false;
-
 		// compute resource path
-		boost::filesystem::path resources_path = application->GetResourcesPath();
+		boost::filesystem::path const & resources_path = GetResourcesPath();
 
 		// generate the mesh
 		chaos::sphere3   s = chaos::sphere3(glm::vec3(0.0f, 0.0f, 0.0f), 100.0f);
 		chaos::GPUMultiMeshGenerator generators;
-		generators.AddGenerator(new chaos::GPUSphereMeshGenerator(s, glm::mat4x4(1.0f), 30), mesh_sphere);
+		generators.AddGenerator(new chaos::GPUSphereMeshGenerator(s, glm::mat4x4(1.0f), 10), mesh_sphere);
 		if (!generators.GenerateMeshes())
+			return false;
+
+		// load the mesh model
+		if (!LoadMeshes())
 			return false;
 
 		// generate the program
@@ -75,6 +86,136 @@ protected:
 		fps_view_controller.config.strafe_speed = CAMERA_SPEED;
 
 		fps_view_controller.fps_view.position.z = 500.0f;
+
+		return true;
+	}
+
+	bool LoadMeshes()
+	{
+		// compute resource path
+		boost::filesystem::path const & resources_path = GetResourcesPath();
+
+		// load the file
+		chaos::Buffer<char> buffer = chaos::FileTools::LoadFile(resources_path / "scene.obj");
+		if (buffer == nullptr)
+			return false;
+
+		// get the extensions
+		Assimp::Importer importer;
+
+		std::string supported_extension;
+		importer.GetExtensionList(supported_extension);
+
+		// load the scene
+		unsigned int load_flags =
+			aiProcess_CalcTangentSpace |
+			aiProcess_GenNormals |
+			aiProcess_Triangulate |
+			aiProcess_JoinIdenticalVertices |
+			aiProcess_SortByPType;
+
+		const aiScene* scene = importer.ReadFileFromMemory(buffer.data, buffer.bufsize, load_flags);
+
+		if (scene == nullptr || scene->mFlags == AI_SCENE_FLAGS_INCOMPLETE || scene->mRootNode == nullptr)
+		{
+			char const* error = importer.GetErrorString();
+			if (error != nullptr)
+				chaos::Log::Message("Assimp::Importer::ReadFileFromMemory failure [%s]", error);
+			return false;
+		}
+
+		// get all meshes
+		if (scene->HasMeshes())
+		{
+			// create a vertex declaration
+			chaos::shared_ptr<chaos::GPUVertexDeclaration> declaration = new chaos::GPUVertexDeclaration();
+			declaration->Push(chaos::VertexAttributeSemantic::POSITION, 0, chaos::VertexAttributeType::FLOAT3);
+			declaration->Push(chaos::VertexAttributeSemantic::NORMAL, 0, chaos::VertexAttributeType::FLOAT3);
+
+			for (unsigned int i = 0; i < scene->mNumMeshes; ++i)
+			{
+				chaos::shared_ptr<chaos::GPUMesh> gpu_mesh = new chaos::GPUMesh;
+
+				aiMesh const* mesh = scene->mMeshes[i];
+				if (mesh == nullptr)
+					continue;
+
+				if (!mesh->HasNormals())
+					continue;
+				if (!mesh->HasPositions())
+					continue;
+
+				struct MeshVertex
+				{
+					glm::vec3 position;
+					glm::vec3 normal;
+				};
+
+				// vertices			
+				chaos::shared_ptr<chaos::GPUBuffer> vertex_buffer = new chaos::GPUBuffer(false);
+				if (vertex_buffer == nullptr)
+					break;
+			
+				unsigned int vertices_count = mesh->mNumVertices;
+				size_t vertex_bufsize = vertices_count * sizeof(MeshVertex);
+				vertex_buffer->SetBufferData(nullptr, vertex_bufsize); // set buffer size
+
+				char * vertex_buf = vertex_buffer->MapBuffer(0, 0, false, true);
+				if (vertex_buf == nullptr)
+					break;
+
+				chaos::MemoryBufferWriter vertices_writer(vertex_buf, vertex_bufsize);
+
+				for (unsigned int i = 0 ; i < vertices_count ; ++i)
+				{
+					vertices_writer << mesh->mVertices[i] * 100.0f;
+					vertices_writer << mesh->mNormals[i];
+				}
+
+				vertex_buffer->UnMapBuffer();
+
+				// indices
+				chaos::shared_ptr<chaos::GPUBuffer> index_buffer = new chaos::GPUBuffer(false);
+				if (vertex_buffer == nullptr)
+					break;
+
+				unsigned int faces_count = mesh->mNumFaces;
+				size_t index_bufsize = faces_count * 3 * sizeof(int32_t);
+				index_buffer->SetBufferData(nullptr, index_bufsize); // set buffer size
+
+
+				char* index_buf = index_buffer->MapBuffer(0, 0, false, true);
+				if (index_buf == nullptr)
+					break;
+
+				chaos::MemoryBufferWriter indices_writer(index_buf, index_bufsize);
+
+				for (unsigned int i = 0; i < faces_count; ++i)
+				{
+					if (mesh->mFaces[i].mNumIndices != 3)
+						i = i;
+
+					for (unsigned j = 0; j < mesh->mFaces[i].mNumIndices; ++j)
+					{
+						indices_writer << int32_t(mesh->mFaces[i].mIndices[j]);
+					}
+				}
+
+				index_buffer->UnMapBuffer();
+
+				// create the element
+				chaos::GPUMeshElement & element = gpu_mesh->AddMeshElement(vertex_buffer.get(), index_buffer.get());
+				element.vertex_declaration = declaration;
+
+				chaos::GPUDrawPrimitive primitive; 
+				primitive.indexed = true;
+				primitive.primitive_type = GL_TRIANGLES;
+				primitive.count = faces_count * 3;
+				element.primitives.push_back(primitive);
+
+				meshes.push_back(gpu_mesh.get());
+			}
+		}
 
 		return true;
 	}
@@ -110,6 +251,8 @@ protected:
 
 	chaos::shared_ptr<chaos::GPUMesh> mesh_sphere;
 
+	std::vector<chaos::shared_ptr<chaos::GPUMesh>> meshes;
+
 	chaos::shared_ptr<chaos::GPUProgram> program;
 
 	chaos::FPSViewController fps_view_controller;
@@ -119,232 +262,3 @@ int main(int argc, char ** argv, char ** env)
 {
     return chaos::RunWindowApplication<WindowOpenGLTest>(argc, argv, env);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#if 0
-virtual void Finalize() override
-{
-    debug_display.Finalize();
-    chaos::Window::Finalize();
-}
-
-bool InitializeScene(boost::filesystem::path const& model_path)
-{
-    chaos::Buffer<char> buffer = chaos::FileTools::LoadFile(model_path);
-    if (buffer == nullptr)
-        return false;
-
-
-
-    Assimp::Importer importer;
-
-    std::string supported_extension;
-    importer.GetExtensionList(supported_extension);
-
-
-    importer.SetExtraVerbose(true);
-
-
-    unsigned int load_flags =
-        aiProcess_CalcTangentSpace |
-        aiProcess_Triangulate |
-        aiProcess_JoinIdenticalVertices |
-        aiProcess_SortByPType;
-
-    const aiScene* scene = importer.ReadFileFromMemory(buffer.data, buffer.bufsize, load_flags);
-
-
-    if (scene == nullptr || scene->mFlags == AI_SCENE_FLAGS_INCOMPLETE || scene->mRootNode == nullptr)
-    {
-        char const* error = importer.GetErrorString();
-        if (error != nullptr)
-            chaos::Log::Message("Assimp::Importer::ReadFileFromMemory failure [%s]", error);
-        return false;
-    }
-
-    if (scene->HasMeshes())
-    {
-        for (unsigned int i = 0; i < scene->mNumMeshes; ++i)
-        {
-            aiMesh const* mesh = scene->mMeshes[i];
-            if (mesh == nullptr)
-                continue;
-
-            i = i;
-        }
-    }
-
-
-    if (scene->HasAnimations())
-    {
-        for (unsigned int i = 0; i < scene->mNumAnimations; ++i)
-        {
-            aiAnimation const* animation = scene->mAnimations[i];
-            if (animation == nullptr)
-                continue;
-
-            i = i;
-
-        }
-    }
-
-    if (scene->HasTextures())
-    {
-        for (unsigned int i = 0; i < scene->mNumTextures; ++i)
-        {
-            aiTexture const* texture = scene->mTextures[i];
-            if (texture == nullptr)
-                continue;
-            i = i;
-        }
-    }
-
-    if (scene->HasMaterials())
-    {
-        for (unsigned int i = 0; i < scene->mNumMaterials; ++i)
-        {
-            aiMaterial const* material = scene->mMaterials[i];
-            if (material == nullptr)
-                continue;
-
-            for (unsigned int j = 0; j < material->mNumProperties; ++j)
-            {
-                aiMaterialProperty* property = material->mProperties[j];
-                if (property == nullptr)
-                    continue;
-                j = j;
-            }
-        }
-    }
-
-    if (scene->HasCameras())
-    {
-        for (unsigned int i = 0; i < scene->mNumCameras; ++i)
-        {
-            aiCamera const* camera = scene->mCameras[i];
-            if (camera == nullptr)
-                continue;
-            i = i;
-        }
-    }
-
-    if (scene->HasLights())
-    {
-        for (unsigned int i = 0; i < scene->mNumLights; ++i)
-        {
-            aiLight const* light = scene->mLights[i];
-            if (light == nullptr)
-                continue;
-            i = i;
-        }
-    }
-
-    return ImportSceneNodes(scene, scene->mRootNode);
-}
-
-bool ImportSceneNodes(aiScene const* scene, aiNode const* node)
-{
-    if (node == nullptr)
-        return true;
-
-    aiString const& name = node->mName;
-
-    for (unsigned int i = 0; i < node->mNumMeshes; ++i)
-    {
-        unsigned int mesh_index = node->mMeshes[i];
-
-        if (mesh_index >= scene->mNumMeshes)
-            continue;
-
-        aiMesh* mesh = scene->mMeshes[mesh_index];
-        if (mesh == nullptr)
-            continue;
-    }
-
-    if (node->mMetaData != nullptr)
-    {
-        for (unsigned int i = 0; i < node->mMetaData->mNumProperties; ++i)
-        {
-            aiString        const& key = node->mMetaData->mKeys[i];
-            aiMetadataEntry const& value = node->mMetaData->mValues[i];
-
-
-            i = i;
-        }
-    }
-
-    bool result = true;
-    for (unsigned int i = 0; i < node->mNumChildren; ++i)
-    {
-        result &= ImportSceneNodes(scene, node->mChildren[i]);
-        if (!result)
-            break;
-    }
-    return result;
-}
-
-
-
-
-
-virtual bool InitializeFromConfiguration(nlohmann::json const* config) override
-{
-    if (!chaos::Window::InitializeFromConfiguration(config))
-        return false;
-
-    chaos::Application* application = chaos::Application::GetInstance();
-    if (application == nullptr)
-        return false;
-
-    boost::filesystem::path resources_path = application->GetResourcesPath();
-
-    // load the FONT texture
-    boost::filesystem::path image_path = resources_path / "font.png";
-
-    chaos::GLDebugOnScreenDisplay::Params debug_params;
-    debug_params.texture_path = image_path;
-    debug_params.font_characters = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
-    debug_params.font_characters_per_line = 10;
-    debug_params.font_characters_line_count = 10;
-    debug_params.character_width = 20;
-    debug_params.spacing = glm::ivec2(0, 0);
-    debug_params.crop_texture = glm::ivec2(15, 7);
-
-    if (!debug_display.Initialize(debug_params))
-        return false;
-
-    debug_display.AddLine("HelloWorld");
-
-    // Load the model
-    static int MODEL_INDEX = 1;
-
-    boost::filesystem::path model_path;
-    if (MODEL_INDEX % 3 == 0)
-        model_path = resources_path / "AG_CIM_Act_Stand_Drink_1_2.fbx";
-    else if (MODEL_INDEX % 3 == 1)
-        model_path = resources_path / "house.fbx";
-    else if (MODEL_INDEX % 3 == 2)
-        model_path = resources_path / "watcher.fbx";
-
-    if (!InitializeScene(model_path))
-        return false;
-
-    return true;
-}
-
-#endif

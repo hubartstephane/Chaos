092170d97370327a43c074beef29b4dcdd7e8a33
Stephane Hubart <hubart.stephane@gmail.com>
2025-02-27 18:29:08 +0100

    Update Stats rendering

--- libraries/chaos/src/ImGuiCore/ImGuiRenderingStatsObject.cpp
+++ libraries/chaos/src/ImGuiCore/ImGuiRenderingStatsObject.cpp
@@ -21,49 +21,54 @@ namespace chaos
 			ImGui::Text("Draw calls / sec: %d",   average_drawcall);
 			ImGui::Text("Vertices   / sec: %d",   average_vertices);
 
-			boost::circular_buffer<GPURenderContextFrameStats> const stats = render_context->GetStats();
+			boost::circular_buffer<GPURenderContextFrameStats> const & stats = render_context->GetStats();
 
-			auto DrawStat = [stats](char const * title, int GPURenderContextFrameStats::*data_ptr)
+			float current_time = (float)glfwGetTime();
+
+			auto DrawStat = [&stats, current_time](char const * title, int GPURenderContextFrameStats::*data_ptr)
 			{
 				constexpr float  HISTORY_DURATION   = 20.0f; // 20 seconds history on screen
-				constexpr float  SAMPLES_PER_SECOND = 100.0f;
-				constexpr size_t BUFFER_SIZE        = size_t(HISTORY_DURATION * SAMPLES_PER_SECOND);
+				constexpr float  SAMPLES_PER_SECOND = 1.0f;
+				constexpr float  SAMPLE_DURATION    = 1.0f / SAMPLES_PER_SECOND;
+				constexpr size_t BUFFER_SIZE        = size_t(HISTORY_DURATION * SAMPLES_PER_SECOND) + 1;
 
-				float current_time = (float)glfwGetTime();
+				float reference_time = float(int((current_time - HISTORY_DURATION) * SAMPLES_PER_SECOND)) / SAMPLES_PER_SECOND;
+
+				constexpr float UNSET_VALUE = -std::numeric_limits<float>::max(); // this 'special' value works well with the MAX operation
 
 				float values[BUFFER_SIZE];
-				memset(values, 0, sizeof(values));
+				for (size_t i = 0 ; i < BUFFER_SIZE ; ++i)
+					values[i] = UNSET_VALUE;
 
 				float times [BUFFER_SIZE];
-				memset(times, 0, sizeof(times));
+				for (size_t i = 0; i < BUFFER_SIZE; ++i)
+					times[i] = reference_time + float(i) * SAMPLE_DURATION;
 
 				float max_value = 0.0f; // values are always positive
 
-				int last_index_in_buffer = BUFFER_SIZE - 1;
+				int min_index_in_buffer = BUFFER_SIZE - 1;
 				for (int i = (int)stats.size() - 1 ; i >= 0 ; --i)
 				{
 					GPURenderContextFrameStats const & st = stats[i];
 
-					float dt = current_time - st.frame_time;
-					if (dt > HISTORY_DURATION)
+					if (st.frame_time < reference_time)
 						break;
 
-					int index_in_buffer = (BUFFER_SIZE - 1) - int(dt * SAMPLES_PER_SECOND);
+					int index_in_buffer = int((st.frame_time - reference_time) / SAMPLE_DURATION);
 					if (index_in_buffer < 0)
 						break;
+					if (index_in_buffer > BUFFER_SIZE - 1)
+						index_in_buffer = BUFFER_SIZE - 1;
 
 					values[index_in_buffer] = std::max(values[index_in_buffer], float(st.*data_ptr));
-					times[index_in_buffer]  = std::max(times[index_in_buffer], st.frame_time);
-
-					max_value = std::max(max_value, values[index_in_buffer]);
-
-					last_index_in_buffer = index_in_buffer;
+					max_value               = std::max(max_value, values[index_in_buffer]);
+					min_index_in_buffer     = std::min(min_index_in_buffer, index_in_buffer);
 				}
 
 				size_t write_index = 0;
-				for (size_t read_index = last_index_in_buffer; read_index < BUFFER_SIZE ; ++read_index)
+				for (size_t read_index = min_index_in_buffer ; read_index < BUFFER_SIZE ; ++read_index)
 				{
-					if (times[read_index] == 0)
+					if (values[read_index] == UNSET_VALUE)
 						continue;
 
 					values[write_index] = values[read_index];
@@ -91,9 +96,9 @@ namespace chaos
 						ImPlotAxisFlags_NoMenus;
 
 					ImPlot::SetupAxes("Time", title, axis_x_style, axis_y_style);
-					ImPlot::SetupAxisLimits(ImAxis_X1, current_time - HISTORY_DURATION, current_time, ImPlotCond_Always);
+					ImPlot::SetupAxisLimits(ImAxis_X1, current_time - HISTORY_DURATION, current_time - SAMPLE_DURATION, ImPlotCond_Always);
 					ImPlot::SetupAxisLimits(ImAxis_Y1, 0.0f, max_value * 1.2f, ImPlotCond_Always);
-										
+
 					ImPlot::PlotShaded(title, times, values, int(write_index));
 					ImPlot::PlotLine(title, times, values, int(write_index));
 

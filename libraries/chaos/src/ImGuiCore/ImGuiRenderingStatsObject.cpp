01e213dee933c38151ec06e2c9464a29b6d8a1e6
Stephane Hubart <hubart.stephane@gmail.com>
2025-02-27 11:59:42 +0100

    stat viewer in progress

--- libraries/chaos/src/ImGuiCore/ImGuiRenderingStatsObject.cpp
+++ libraries/chaos/src/ImGuiCore/ImGuiRenderingStatsObject.cpp
@@ -38,28 +38,47 @@ namespace chaos
 			auto DrawStat = [stats](char const * title, int GPURenderContextFrameStats::*data_ptr)
 			{
 				constexpr size_t GRAPH_WIDTH = 500;
+				constexpr float  SLICE_DURATION = 1.0f;
 
-				int count = (int)std::min(GRAPH_WIDTH, stats.size());
+				float time[GRAPH_WIDTH];
+				float min_value[GRAPH_WIDTH];
+				float max_value[GRAPH_WIDTH];
+				float y_max = 0.0f; // values are always positive
 
-				float x[GRAPH_WIDTH];
-				float y[GRAPH_WIDTH];
-				float y_max = -1.0f; // values are always positive
-				for (int i = 0; i < count; ++i)
+				int stat_index  = (int)stats.size() - 1;
+				int graph_index = GRAPH_WIDTH - 1;
+				for (; graph_index >= 0 && stat_index >= 0; --graph_index)
 				{
-					GPURenderContextFrameStats const& frame_stat = stats[i];
-					x[i] = (float)frame_stat.frame_time;
-					y[i] = (float)(&frame_stat->*data_ptr);
-					y_max = std::max(y_max, y[i]);
+					float base_time = (float)stats[stat_index].frame_time;
+					base_time = float(int(base_time * 100.0f)) / 100.0f;
+
+					time[graph_index] = base_time;
+
+					int min_v = stats[stat_index].*data_ptr;
+					int max_v = stats[stat_index].*data_ptr;
+					do
+					{
+						float stat_time = (float)stats[stat_index].frame_time;
+						if (stat_time < base_time)
+							break;
+
+						int value = stats[stat_index].*data_ptr;
+						min_v = std::min(min_v, value);
+						max_v = std::max(max_v, value);
+
+					} while (--stat_index >= 0);
+
+					min_value[graph_index] = std::min(min_value[graph_index], (float)min_v);
+					max_value[graph_index] = std::max(max_value[graph_index], (float)max_v);
+					y_max = std::max(y_max, max_value[graph_index]);
 				}
 
 				int plot_style =
 					ImPlotFlags_NoMenus |
 					ImPlotFlags_NoLegend |
+					ImPlotFlags_NoInputs |
 					ImPlotFlags_NoMouseText |
-					ImPlotFlags_NoBoxSelect |
-					ImPlotAxisFlags_LockMin |
-					ImPlotAxisFlags_LockMax |
-					ImPlotAxisFlags_NoSideSwitch;
+					ImPlotFlags_NoBoxSelect;
 
 				if (ImPlot::BeginPlot(title, ImVec2(-1, 0), plot_style))
 				{
@@ -75,7 +94,13 @@ namespace chaos
 					ImPlot::SetupAxisLimits(ImAxis_Y1, 0.0f, y_max * 1.2f);
 
 					ImPlot::PushStyleVar(ImPlotStyleVar_FillAlpha, 0.25f);
-					ImPlot::PlotShaded(title, x, y, count);
+
+					int count = (GRAPH_WIDTH - 1) - graph_index;
+					float const * mn = &min_value[graph_index + 1];
+					float const * mx = &max_value[graph_index + 1];
+					float const * t  = &time[graph_index + 1];
+
+					ImPlot::PlotShaded(title, t, mx, count);
 					ImPlot::PopStyleVar();
 					ImPlot::EndPlot();
 				}

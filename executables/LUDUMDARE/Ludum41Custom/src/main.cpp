bb71120f7b3d12cf1396ce33c3b15d387833d474
Stephane Hubart <hubart.stephane@gmail.com>
2025-03-13 21:24:32 +0100

    In Ludum41xxx projects, remove useless LudumGameApplication

--- executables/LUDUMDARE/Ludum41Custom/src/main.cpp
+++ executables/LUDUMDARE/Ludum41Custom/src/main.cpp
@@ -1,21 +1,6 @@
 #include "Ludum41CustomPCH.h"
 #include "Ludum41CustomGame.h"
 
-class LudumGameApplication : public chaos::GameApplication
-{
-public:
-
-	using chaos::GameApplication::GameApplication;
-
-	virtual void OnWindowCreated(chaos::Window* window) override
-	{
-		// super
-		chaos::GameApplication::OnWindowCreated(window);
-		// hidden cursor
-		window->SetCursorMode(chaos::CursorMode::DISABLED);
-	}
-};
-
 int main(int argc, char** argv, char** env)
 {
 	return chaos::RunGame<LudumGame>(argc, argv, env);
--- executables/LUDUMDARE/Ludum41Isolation/src/main.cpp
+++ executables/LUDUMDARE/Ludum41Isolation/src/main.cpp
@@ -1,21 +1,6 @@
 #include "Ludum41IsolationPCH.h"
 #include "Ludum41IsolationGame.h"
 
-class LudumGameApplication : public chaos::GameApplication
-{
-public:
-
-	using chaos::GameApplication::GameApplication;
-
-	virtual void OnWindowCreated(chaos::Window* window) override
-	{
-		// super
-		chaos::GameApplication::OnWindowCreated(window);
-		// hidden cursor
-		window->SetCursorMode(chaos::CursorMode::DISABLED);
-	}
-};
-
 int main(int argc, char** argv, char** env)
 {
 	return chaos::RunGame<LudumGame>(argc, argv, env);

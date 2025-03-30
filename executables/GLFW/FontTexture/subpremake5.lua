1b1bb8dee1c22a493acbda9cbd8457b6629f6d08
Stephane Hubart <hubart.stephane@gmail.com>
2025-03-30 01:51:44 +0100

    remove useless FontTexture application

--- executables/GLFW/FontTexture/subpremake5.lua
+++
@@ -1,6 +0,0 @@
--- =============================================================================
--- ROOT_PATH/executables/GLFW/FontTexture
--- =============================================================================
-
-local project = build:WindowedApp()
-project:DependOnLib("CHAOS")

6e94661069715299fe247fee602de9dd930747bd
Stephane Hubart <hubart.stephane@gmail.com>
2025-07-19 17:02:32 +0200

    GamepadState deserves its own files

--- libraries/chaos/include/chaos/Windowing/GamepadManager.h
+++ libraries/chaos/include/chaos/Windowing/GamepadManager.h
@@ -8,7 +8,6 @@ namespace chaos
 
 	// all classes in this file
 #define CHAOS_GAMEPAD_CLASSES \
-(GamepadState) \
 (PhysicalGamepad) \
 (Gamepad) \
 (GamepadCallbacks) \
@@ -23,7 +22,6 @@ namespace chaos
 #define CHAOS_GAMEPAD_FRIEND_DECL(r, data, elem) friend class elem;
 #define CHAOS_GAMEPAD_ALL_FRIENDS BOOST_PP_SEQ_FOR_EACH(CHAOS_GAMEPAD_FRIEND_DECL, _, CHAOS_GAMEPAD_CLASSES)
 
-
 BOOST_PP_SEQ_FOR_EACH(CHAOS_GAMEPAD_FORWARD_DECL, _, CHAOS_GAMEPAD_CLASSES);
 
 #elif !defined CHAOS_TEMPLATE_IMPLEMENTATION
@@ -34,57 +32,6 @@ BOOST_PP_SEQ_FOR_EACH(CHAOS_GAMEPAD_FORWARD_DECL, _, CHAOS_GAMEPAD_CLASSES);
 
 	/** maximum number of supported physical gamepads */
 	static constexpr int MAX_SUPPORTED_GAMEPAD_COUNT = GLFW_JOYSTICK_LAST + 1;
-
-	/**
-	* GamepadState : the states contained in the device
-	*/
-
-	class CHAOS_API GamepadState
-	{
-		CHAOS_GAMEPAD_ALL_FRIENDS
-
-		/** number of axis in a gamepad */
-		static constexpr size_t AXIS_COUNT = sizeof(meta::FakeInstance<GLFWgamepadstate>().axes) / sizeof(meta::FakeInstance<GLFWgamepadstate>().axes[0]);
-		/** number of button in a gamepad */
-		static constexpr size_t BUTTON_COUNT = sizeof(meta::FakeInstance<GLFWgamepadstate>().buttons) / sizeof(meta::FakeInstance<GLFWgamepadstate>().buttons[0]);
-
-	public:
-
-		/* returns a status giving the change of button relative to previous frame */
-		ButtonStateChange GetButtonStateChange(GamepadButton button) const;
-		/** returns the button state */
-		bool IsButtonPressed(GamepadButton button, InputStateFrame frame = InputStateFrame::CURRENT) const;
-		/** returns the button state */
-		float GetAxisValue(GamepadAxis axis, InputStateFrame frame = InputStateFrame::CURRENT) const;
-		/** returns true whether there is a button that just has become pressed */
-		bool IsAnyButtonJustPressed() const;
-		/** returns true whether there is any pressed button */
-		bool IsAnyButtonPressed(InputStateFrame frame = InputStateFrame::CURRENT) const;
-		/** returns true whether there is any axis in use */
-		bool IsAnyAxisAction(InputStateFrame frame = InputStateFrame::CURRENT) const;
-		/** returns true whenever a buttons is pressed or an axis is in action */
-		bool IsAnyAction(InputStateFrame frame = InputStateFrame::CURRENT) const;
-		/** returns the direction of one stick (a combinaison of 2 axis) */
-		glm::vec2 GetStickValue(GamepadStick stick, InputStateFrame frame = InputStateFrame::CURRENT) const;
-		/** returns the number of buttons */
-		size_t GetButtonCount() const;
-		/** returns the number of axis */
-		size_t GetAxisCount() const;
-
-	protected:
-
-		/** update all the values for the axis and buttons */
-		void UpdateAxisAndButtons(int stick_index, float delta_time, float dead_zone);
-		/** reset the content of the object */
-		void Clear();
-
-	protected:
-
-		/** the value for axis */
-		std::array<AxisState, AXIS_COUNT> axes;
-		/** the value for buttons */
-		std::array<ButtonState, BUTTON_COUNT> buttons;
-	};
 
 	/**
 	* GamepadCallbacks : some callbacks that may be plugged into a gamepad
---
+++ libraries/chaos/include/chaos/Windowing/GamepadState.h
@@ -0,0 +1,62 @@
+namespace chaos
+{
+#ifdef CHAOS_FORWARD_DECLARATION
+
+	class GamepadState;
+
+#elif !defined CHAOS_TEMPLATE_IMPLEMENTATION
+
+	/**
+	* GamepadState : the states contained in the device
+	*/
+
+	class CHAOS_API GamepadState
+	{
+		CHAOS_GAMEPAD_ALL_FRIENDS
+
+		/** number of axis in a gamepad */
+		static constexpr size_t AXIS_COUNT = sizeof(meta::FakeInstance<GLFWgamepadstate>().axes) / sizeof(meta::FakeInstance<GLFWgamepadstate>().axes[0]);
+		/** number of button in a gamepad */
+		static constexpr size_t BUTTON_COUNT = sizeof(meta::FakeInstance<GLFWgamepadstate>().buttons) / sizeof(meta::FakeInstance<GLFWgamepadstate>().buttons[0]);
+
+	public:
+
+		/* returns a status giving the change of button relative to previous frame */
+		ButtonStateChange GetButtonStateChange(GamepadButton button) const;
+		/** returns the button state */
+		bool IsButtonPressed(GamepadButton button, InputStateFrame frame = InputStateFrame::CURRENT) const;
+		/** returns the button state */
+		float GetAxisValue(GamepadAxis axis, InputStateFrame frame = InputStateFrame::CURRENT) const;
+		/** returns true whether there is a button that just has become pressed */
+		bool IsAnyButtonJustPressed() const;
+		/** returns true whether there is any pressed button */
+		bool IsAnyButtonPressed(InputStateFrame frame = InputStateFrame::CURRENT) const;
+		/** returns true whether there is any axis in use */
+		bool IsAnyAxisAction(InputStateFrame frame = InputStateFrame::CURRENT) const;
+		/** returns true whenever a buttons is pressed or an axis is in action */
+		bool IsAnyAction(InputStateFrame frame = InputStateFrame::CURRENT) const;
+		/** returns the direction of one stick (a combinaison of 2 axis) */
+		glm::vec2 GetStickValue(GamepadStick stick, InputStateFrame frame = InputStateFrame::CURRENT) const;
+		/** returns the number of buttons */
+		size_t GetButtonCount() const;
+		/** returns the number of axis */
+		size_t GetAxisCount() const;
+
+	protected:
+
+		/** update all the values for the axis and buttons */
+		void UpdateAxisAndButtons(int stick_index, float delta_time, float dead_zone);
+		/** reset the content of the object */
+		void Clear();
+
+	protected:
+
+		/** the value for axis */
+		std::array<AxisState, AXIS_COUNT> axes;
+		/** the value for buttons */
+		std::array<ButtonState, BUTTON_COUNT> buttons;
+	};
+
+#endif
+
+}; // namespace chaos

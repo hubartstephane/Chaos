20c45659debdf778b202689c9c819ad91cc63d51
stephane.hubart <hubart.stephane@gmail.com>
2025-11-15 23:42:33 +0100

    change Direction values

--- libraries/chaos/include/chaos/Misc/Direction.h
+++ libraries/chaos/include/chaos/Misc/Direction.h
@@ -7,14 +7,14 @@ namespace chaos
 #elif !defined CHAOS_TEMPLATE_IMPLEMENTATION
 
 	/** the possible direction */
-	enum class CHAOS_API Direction : int
+	enum class CHAOS_API Direction : int // order is important
 	{
-		POSITIVE_X = (1 << 0),
-		NEGATIVE_X = (1 << 1),
-		POSITIVE_Y = (1 << 2),
-		NEGATIVE_Y = (1 << 3),
-		POSITIVE_Z = (1 << 4),
-		NEGATIVE_Z = (1 << 5)
+		NEGATIVE_X = 0,
+		POSITIVE_X = 1,
+		NEGATIVE_Y = 2,
+		POSITIVE_Y = 3,
+		NEGATIVE_Z = 4,
+		POSITIVE_Z = 5		
 	};
 
 	CHAOS_DECLARE_ENUM_METHOD(Direction, CHAOS_API);
--- libraries/chaos/src/Misc/Direction.cpp
+++ libraries/chaos/src/Misc/Direction.cpp
@@ -5,12 +5,12 @@ namespace chaos
 {
 	static EnumMetaData<Direction> const Direction_metadata =
 	{
-		{ Direction::POSITIVE_X, "positive_x" },
 		{ Direction::NEGATIVE_X, "negative_y" },
-		{ Direction::POSITIVE_Y, "positive_x" },
+		{ Direction::POSITIVE_X, "positive_x" },
 		{ Direction::NEGATIVE_Y, "negative_y" },
+		{ Direction::POSITIVE_Y, "positive_x" },
+		{ Direction::NEGATIVE_Z, "negative_z" },
 		{ Direction::POSITIVE_Z, "positive_y" },
-		{ Direction::NEGATIVE_Z, "negative_z" }
 	};
 
 	CHAOS_IMPLEMENT_ENUM_METHOD(Direction, &Direction_metadata, CHAOS_API);

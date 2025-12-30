3cc190a85daeb15913de09bae368855b1c58f69e
stephane.hubart <hubart.stephane@gmail.com>
2025-12-30 17:49:52 +0100

    update CHAOS_PP_GENERATE_CLASS_MAPPING

--- libraries/chaos/include/chaos/Core/ClassMapping.h
+++ libraries/chaos/include/chaos/Core/ClassMapping.h
@@ -2,33 +2,37 @@
 
 // Class mapping is a meta programming feature that is used to map on class into another one
 //
-//   MyMapping<A> ---> C
-//   MyMapping<B> ---> D
+//   MyMapping_t<A> ---> C
+//   MyMapping_t<B> ---> D
 // 
 // You first need to declare the mapping itself
 //
-//     CHAOS_GENERATE_DECLARE_CLASS_MAPPING(MyMapping)
+//     CHAOS_PP_GENERATE_DECLARE_CLASS_MAPPING(MyMapping)
+// 
+//        or 
+// 
+//     CHAOS_PP_GENERATE_DECLARE_CLASS_MAPPING(MyMapping, default_class)
 //
 // Then the specializations
 //
-//     CHAOS_GENERATE_SPECIALIZE_CLASS_MAPPING(MyMapping, A, C)
-//     CHAOS_GENERATE_SPECIALIZE_CLASS_MAPPING(MyMapping, B, D)
+//     CHAOS_PP_GENERATE_DECLARE_CLASS_MAPPING(MyMapping, A, C)
+//     CHAOS_PP_GENERATE_DECLARE_CLASS_MAPPING(MyMapping, B, D)
 //
 
-template <template<typename> class T>
-struct is_strict_mapping : public boost::mpl::bool_<false> {};
-template <template<typename> class T>
-constexpr bool is_strict_mapping_v = is_strict_mapping<T>::value;
-
-#define CHAOS_GENERATE_DECLARE_CLASS_MAPPING(mapping_name, strict_mapping)\
+#define CHAOS_PP_GENERATE_DECLARE_CLASS_MAPPING(mapping_name, ...)\
 template<typename T>\
-struct mapping_name;\
+struct mapping_name BOOST_PP_IIF(\
+	BOOST_PP_IS_EMPTY(__VA_ARGS__),\
+	CHAOS_PP_GENERATE_DECLARE_CLASS_MAPPING_NODEFAULT,\
+	CHAOS_PP_GENERATE_DECLARE_CLASS_MAPPING_WITHDEFAULT\
+)(__VA_ARGS__);\
 template<typename T>\
-using mapping_name##_t = mapping_name<T>::type;\
-template<>\
-struct is_strict_mapping<mapping_name> : public boost::mpl::bool_<strict_mapping> {};
+using mapping_name##_t = mapping_name<T>::type;
 
-#define CHAOS_GENERATE_SPECIALIZE_CLASS_MAPPING(mapping_name, from_class, to_class)\
+#define CHAOS_PP_GENERATE_DECLARE_CLASS_MAPPING_NODEFAULT(...);
+#define CHAOS_PP_GENERATE_DECLARE_CLASS_MAPPING_WITHDEFAULT(default_value, ...) : public boost::mpl::identity<default_value> {};
+
+#define CHAOS_PP_GENERATE_SPECIALIZE_CLASS_MAPPING(mapping_name, from_class, to_class)\
 template<> struct mapping_name<from_class> : public boost::mpl::identity<to_class>{};
 
 #endif

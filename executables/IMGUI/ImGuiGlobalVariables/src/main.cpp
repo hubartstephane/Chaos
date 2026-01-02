d88e5f0eb28330eeb8eccca39059f3e9d6bc68d6
stephane.hubart <hubart.stephane@gmail.com>
2026-01-02 16:21:03 +0100

    Fix ImGuiGlobalVariables test executable. Still some fix to do in ImGui code

--- executables/IMGUI/ImGuiGlobalVariables/src/main.cpp
+++ executables/IMGUI/ImGuiGlobalVariables/src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "chaos/Chaos.h"
 
+
 enum class Truc : int
 {
 	None = 0,
@@ -29,7 +30,7 @@ public:
 	std::string name;
 };
 
-static EnumMetaData<Truc> const Truc_metadata =
+static chaos::EnumMetaData<Truc> const Truc_metadata =
 {
 	{ Truc::Truc1, "Truc1" },
 	{ Truc::Truc2, "Truc2" },
@@ -50,47 +51,6 @@ CHAOS_GLOBAL_VARIABLE(chaos::Hotpoint, hot);
 
 CHAOS_GLOBAL_VARIABLE(AAA, aaa);
 
-#if 0
-
-bool RuntimeCheck(chaos::LightweightFunction<bool()> func)
-{
-	bool result = func();
-	assert(result);
-	return result;
-}
-
-#define RUNTIME_CHECK(FUNC) static bool value = RuntimeCheck(FUNC);
-
-static bool value = RuntimeCheck([]()
-{
-	std::vector<std::pair<char const*, char const*>> checks =
-	{
-		{CHAOS_PP_UNICODE_TO_UTF8(0, 0, 0, 2, 0), "\x20"},
-		{CHAOS_PP_UNICODE_TO_UTF8(0, 0, 0, 4, 1), "\x41"},
-		{CHAOS_PP_UNICODE_TO_UTF8(0, 0, 0, 7, F), "\x7F"},
-
-		{CHAOS_PP_UNICODE_TO_UTF8(0, 0, 0, A, 9), "\xC2\xA9"},
-		{CHAOS_PP_UNICODE_TO_UTF8(0, 0, 3, A, 9), "\xCE\xA9"},
-		{CHAOS_PP_UNICODE_TO_UTF8(0, 0, 7, F, F), "\xDF\xBF"},
-
-		{CHAOS_PP_UNICODE_TO_UTF8(0, 2, 0, A, C), "\xE2\x82\xAC"},
-		{CHAOS_PP_UNICODE_TO_UTF8(0, 4, F, 6, 0), "\xE4\xBD\xA0"},
-		{CHAOS_PP_UNICODE_TO_UTF8(0, F, F, F, F), "\xEF\xBF\xBF"},
-
-		{CHAOS_PP_UNICODE_TO_UTF8(1, F, 6, 0, 0), "\xF0\x9F\x98\x80"},
-		{CHAOS_PP_UNICODE_TO_UTF8(1, F, 6, 8, 0), "\xF0\x9F\x9A\x80"},
-		{CHAOS_PP_UNICODE_TO_UTF8(1, 0, F, F, F, F), "\xF4\x8F\xBF\xBF"}
-	};
-
-	for (auto const& p : checks)
-	{
-		if (chaos::StringTools::Strcmp(p.first, p.second) != 0)
-			return false;
-	}
-	return true;
-});
-
-#endif
 
 class MyApplicationTest : public chaos::WindowApplication
 {
@@ -102,7 +62,11 @@ public:
 	{
 		CreateNamedWindow("Global Variables", &chaos::ImGuiWindow::CreateImGuiWindow<chaos::ImGuiGlobalVariablesObject>);
 
-		return chaos::WindowApplication::MainBody();
+		RunMessageLoop([this]()
+		{
+			return GetWindowCount() > 0;
+		});
+		return 0;
 	}
 };
 

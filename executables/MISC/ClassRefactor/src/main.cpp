d70bc064e47816dba4b0055958679225c0fe19db
stephane.hubart <hubart.stephane@gmail.com>
2026-05-26 23:05:32 +0200

    ClassRefactor exe in progress

--- executables/MISC/ClassRefactor/src/main.cpp
+++ executables/MISC/ClassRefactor/src/main.cpp
@@ -1,113 +1,231 @@
 #include "chaos/Chaos.h"
+#include <Windows.h>
+#include <iostream>
+#include <streambuf>
+
+using namespace chaos;
+
+//-----------------------------------------------------------
+
+class DebugOutputBuf : public std::streambuf
+{
+protected:
+	virtual int overflow(int c) override
+	{
+		if (c != EOF)
+		{
+			char ch = static_cast<char>(c);
+			OutputDebugStringA(std::string(1, ch).c_str());
+		}
+		return c;
+	}
+};
+
+DebugOutputBuf debugBuf;
 
 
-class A;
-class B;
-class ClassBase;
+//-----------------------------------------------------------
 
-// ------------------------------------------------------
+template<typename CLASS_TYPE>
+class MyClass;
 
-class ClassBase
+//-----------------------------------------------------------
+
+
+
+//-----------------------------------------------------------
+
+
+//-----------------------------------------------------------
+
+#if 0
+
+
+
+template<typename CLASS_TYPE>
+struct MyParentClassOf
+{
+	using type = MyClassBase;
+};
+
+template<typename CLASS_TYPE>
+	requires
+(
+	HasInternalSuperType<CLASS_TYPE> || HasExternalSuperType<CLASS_TYPE>
+	)
+	struct MyParentClassOf<CLASS_TYPE>
+{
+	using type = MyClass<SuperClass_t<CLASS_TYPE>>;
+};
+
+template<typename CLASS_TYPE>
+using MyParentClassOf_t = typename MyParentClassOf<CLASS_TYPE>::type;
+
+
+template<typename CLASS_TYPE>
+class MyClass : public MyParentClassOf_t<CLASS_TYPE>
 {
 public:
 
-    ClassBase()
-    {
-        std::cout << "ClassBase::ClassBase()" << std::endl;
-    }
+	using Super = MyParentClassOf_t<CLASS_TYPE>;
 
-    virtual A* Create() const = 0;
+	MyClass(std::string in_name, std::type_info const& in_info = typeid(CLASS_TYPE)) :
+		Super(std::move(in_name), in_info) {
+	}
+
+	virtual CLASS_TYPE* CreateInstance() const
+	{
+		return new CLASS_TYPE;
+	}
+
+
+protected:
+
+
 };
 
+#else
 
-// ------------------------------------------------------
-
-template<typename T>
-class TClass;
-
-template<>
-class TClass<A> : public ClassBase
+class MyClassBase
 {
 public:
 
-    virtual A* Create() const override;
+	MyClassBase(std::string in_name, std::type_info const& in_info) :
+		name(std::move(in_name)),
+		info(in_info) {}
+
+	virtual ~MyClassBase() = default;
+
+public:
+
+	std::string name;
+	std::type_info const& info;
 };
 
-template<typename T>
-class TClass : public TClass<typename T::Super>
+template<typename CLASS_TYPE>
+class MyClass : public MyClassBase
 {
 public:
 
-    virtual T* Create() const override
-    {
-        return new T;
-    }
+	using Super = MyClassBase;
+
+	MyClass(std::string in_name, std::type_info const& in_info = typeid(CLASS_TYPE)) :
+		Super(std::move(in_name), in_info) {}
+
+	virtual CLASS_TYPE* CreateInstance() const
+	{
+		return new CLASS_TYPE;
+	}
 };
 
-// ------------------------------------------------------
+template<typename CLASS_TYPE>
+requires
+(
+	HasInternalSuperType<CLASS_TYPE> || HasExternalSuperType<CLASS_TYPE>
+)
+class MyClass<CLASS_TYPE> : public MyClass<SuperClass_t<CLASS_TYPE>>
+{
+public:
+
+	using Super = MyClass<SuperClass_t<CLASS_TYPE>>;
+
+	MyClass(std::string in_name, std::type_info const& in_info = typeid(CLASS_TYPE)) :
+		Super(std::move(in_name), in_info){}
+
+	virtual CLASS_TYPE* CreateInstance() const override
+	{
+		return new CLASS_TYPE;
+	}
+};
+
+
+
+
+#endif
+
+
+
+
+
+
+//-----------------------------------------------------------
+
 
 class A
 {
 public:
 
-    static inline TClass<A> static_class_A;
+	A() { std::cout << "A::A()" << std::endl; }
 
-    static TClass<A> const* GetStaticClass()
-    {
-        return &static_class_A;
-    }
+	virtual ~A() = default;
 
-    virtual TClass<A> const* GetClass() const
-    {
-        return &static_class_A;
-    }
+	static MyClass<A> const * GetStaticClass()
+	{
+		static MyClass<A> result("A");
+		return &result;
+	}
 
-    A()
-    {
-        std::cout << "A::A()" << std::endl;
-    }
-
-    virtual ~A() = default;
-
+	virtual MyClass<A> const* GetClass() const
+	{
+		return A::GetStaticClass();
+	}
 };
-
-// ------------------------------------------------------
-
-A* TClass<A>::Create() const
-{
-    return new A;
-}
-
-// ------------------------------------------------------
 
 class B : public A
 {
+
 public:
 
-    using Super = A;
+	using Super = A;
 
-    static inline TClass<B> static_class_B;
+	B() { std::cout << "B::B()" << std::endl; }
 
-    static TClass<B> const* GetStaticClass()
-    {
-        return &static_class_B;
-    }
 
-    virtual TClass<B> const* GetClass() const
-    {
-        return &static_class_B;
-    }
 
-    B()
-    {
-        std::cout << "B::B()" << std::endl;
-    }
+	static MyClass<B> const* GetStaticClass()
+	{
+		static MyClass<B> result("B");
+		return &result;
+	}
+
+	virtual MyClass<B> const* GetClass() const override
+	{
+		return B::GetStaticClass();
+	}
 };
+
+
 
 int main(int argc, char ** argv, char ** env)
 {
-    TClass<A> const * CA = new TClass<B>();
-    A * a = CA->Create();
+	std::cout.rdbuf(&debugBuf);
+
+
+
+	bool internal_a = HasInternalSuperType<A>;
+	bool internal_b = HasInternalSuperType<B>;
+
+	bool external_a = HasExternalSuperType<A>;
+	bool external_b = HasExternalSuperType<B>;
+
+	std::cout << "---------------------------" << std::endl;
+	std::cout << "internal_a: " << internal_a << std::endl;
+	std::cout << "internal_b: " << internal_b << std::endl;
+	std::cout << "---------------------------" << std::endl;
+	std::cout << "external_a: " << external_a << std::endl;
+	std::cout << "external_b: " << external_b << std::endl;
+	std::cout << "---------------------------" << std::endl;
+
+
+	MyClass<B> cb("B");
+
+	cb.CreateInstance();
+
+	//SuperClass_t<B> b;
+
+	MyClass<A> const * cca = nullptr;
+	MyClass<B> const* ccb = nullptr;
+
 
 	return 0;
 }
--- libraries/chaos/include/chaos/Meta/SuperClass.h
+++ libraries/chaos/include/chaos/Meta/SuperClass.h
@@ -8,6 +8,8 @@ namespace chaos
 	// 
 	//		class B : public A
 	//		{
+	//		public: // <----- IMPORTANT
+	// 
 	//			using Super = A;
 	//		};
 	//

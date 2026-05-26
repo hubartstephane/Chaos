43b3bd55ecb20d970f634c8a14ba71ead242f901
stephane.hubart <hubart.stephane@gmail.com>
2026-05-26 23:59:03 +0200

    comment some require clause inside SuperClass that does not fit well with the class<XXX>/XXX object we want to design

--- libraries/chaos/include/chaos/Meta/SuperClass.h
+++ libraries/chaos/include/chaos/Meta/SuperClass.h
@@ -30,7 +30,26 @@ namespace chaos
 	{
 		typename T::Super;
 
-		requires IsBaseOf_v<typename T::Super, T>;
+		// XXX: this makes sense ... BUUUUUUTTT this doesn't fit with Object/Class nested compilation
+		//
+		// class A
+		// {
+		// public:
+		// 
+		//		virtual Class<A> const * GetClass() const;
+		// };
+		//
+		// class B : public A
+		// {
+		// public:
+		// 
+		//		using Super = A;
+		//
+		//		virtual Class<B> const * GetClass() const override;  <----- at this point, the IsBaseOf_v below fails because B is still being compiled. Class<B> is not recognized as a child class of Class<A>
+		//		                                                            'override' is problematic (and not marking 'override' is an error too)
+		// };
+
+		//requires IsBaseOf_v<typename T::Super, T>; 
 	};
 
 	// check whether a class has a ChaosGetSuperClass function defined (and check whether this 'Super' is a parent class of T)
@@ -41,7 +60,9 @@ namespace chaos
 			decltype(ChaosGetSuperClass(std::type_identity_t<T>()))
 		>);
 
-		requires IsBaseOf_v<decltype(ChaosGetSuperClass(std::type_identity_t<T>())), T>;
+		// XXX: same here !!!
+
+		//requires IsBaseOf_v<decltype(ChaosGetSuperClass(std::type_identity_t<T>())), T>;
 	};
 
 	// meta function to get super class 
